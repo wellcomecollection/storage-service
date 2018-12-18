9eccc4e4eee56a74c896dfb2cede2f202280e9b3
Robert Kenny <r.kenny@wellcome.ac.uk>
2018-12-18 16:39:39 +0000

    adds correct provider

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,15 @@
 resource "aws_s3_bucket" "storage_archive" {
   bucket = "${local.storage_archive_bucket_name}"
   acl    = "private"
+
+  lifecycle_rule {
+    enabled = true
+
+    transition {
+      days          = 90
+      storage_class = "GLACIER"
+    }
+  }
 }
 
 resource "aws_s3_bucket" "storage_access" {
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,19 +1,21 @@
-# Provider
-
-provider "aws" {
-  region  = "${var.aws_region}"
-  version = "1.42.0"
-}
-
-# Terraform
-
 terraform {
-  required_version = ">= 0.9"
-
   backend "s3" {
+    role_arn     = "arn:aws:iam::975596993436:role/developer"
+
     bucket         = "wellcomecollection-storage-infra"
     key            = "terraform/storage.tfstate"
     dynamodb_table = "terraform-locktable"
     region         = "eu-west-1"
   }
 }
+
+provider "aws" {
+  assume_role {
+    role_arn     = "arn:aws:iam::975596993436:role/developer"
+  }
+
+  region  = "${var.aws_region}"
+  version = "1.42.0"
+}
+
+data "aws_caller_identity" "current" {}
