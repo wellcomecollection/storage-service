33ed1e4aa28c640518d5ac379d3123f1fc0bfe72
Robert Kenny <r.kenny@wellcome.ac.uk>
2019-01-14 18:40:23 +0000

    on_demand VHS

--- terraform/critical/vhs.tf
+++ terraform/critical/vhs.tf
@@ -1,4 +1,4 @@
 module "vhs_manifests" {
-  source = "git::https://github.com/wellcometrust/terraform-modules.git//vhs/modules/vhs?ref=v18.2.2"
+  source = "git::https://github.com/wellcometrust/terraform-modules.git//vhs/modules/vhs?ref=v19.2.0"
   name   = "${var.namespace}-manifests"
 }
