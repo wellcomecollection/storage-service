3de39553512cf059f70e3685baa148f40095fea7
Alex Chan <alex@alexwlchan.net>
2019-02-12 17:31:08 +0000

    BagInfo make SourceOrganisation optional (#22)
    * SourceOrganisation is optional in bag-info.txt
    
    * add keyword args
    
    * sourceOrganization optional in display models
    
    * Apply auto-formatting rules

--- common/src/test/scala/uk/ac/wellcome/platform/archive/common/fixtures/RandomThings.scala
+++ common/src/test/scala/uk/ac/wellcome/platform/archive/common/fixtures/RandomThings.scala
@@ -11,13 +11,13 @@ import scala.util.Random
 trait RandomThings {
   def randomBagInfo =
     BagInfo(
-      randomExternalIdentifier,
-      randomSourceOrganisation,
-      randomPayloadOxum,
-      randomLocalDate,
-      Some(randomExternalDescription),
-      Some(randomInternalSenderIdentifier),
-      Some(randomInternalSenderDescription)
+      externalIdentifier = randomExternalIdentifier,
+      payloadOxum = randomPayloadOxum,
+      baggingDate = randomLocalDate,
+      sourceOrganisation = Some(randomSourceOrganisation),
+      externalDescription = Some(randomExternalDescription),
+      internalSenderIdentifier = Some(randomInternalSenderIdentifier),
+      internalSenderDescription = Some(randomInternalSenderDescription)
     )
 
   def randomAlphanumeric(length: Int = 8) = {
