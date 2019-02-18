16d8b6902c9e0c9511fcf48e2fd4b61053c69056
Alex Chan <a.chan@wellcome.ac.uk>
2019-02-18 14:25:23 +0000

    And now delete all the Akka-based code

--- ingests/src/test/scala/uk/ac/wellcome/platform/archive/progress_async/fixtures/IngestsFixture.scala
+++ ingests/src/test/scala/uk/ac/wellcome/platform/archive/progress_async/fixtures/IngestsFixture.scala
@@ -1,36 +1,18 @@
 package uk.ac.wellcome.platform.archive.progress_async.fixtures
 
-import akka.NotUsed
-import akka.stream.scaladsl.Flow
 import org.scalatest.concurrent.ScalaFutures
-import uk.ac.wellcome.akka.fixtures.Akka
 import uk.ac.wellcome.fixtures.TestWith
-import uk.ac.wellcome.messaging.fixtures.Messaging
 import uk.ac.wellcome.messaging.fixtures.SNS.Topic
 import uk.ac.wellcome.messaging.fixtures.SQS.Queue
-import uk.ac.wellcome.platform.archive.common.fixtures.{
-  ArchiveMessaging,
-  RandomThings
-}
-import uk.ac.wellcome.platform.archive.common.progress.fixtures.ProgressTrackerFixture
-import uk.ac.wellcome.platform.archive.common.progress.models.{
-  Progress,
-  ProgressUpdate
-}
+import uk.ac.wellcome.platform.archive.common.progress.models.Progress
 import uk.ac.wellcome.platform.archive.common.progress.monitor.ProgressTracker
-import uk.ac.wellcome.platform.archive.progress_async.flows.ProgressUpdateFlow
 import uk.ac.wellcome.storage.fixtures.LocalDynamoDb.Table
 import uk.ac.wellcome.storage.fixtures.{LocalDynamoDb, S3}
 
 trait IngestsFixture
     extends S3
-    with Akka
     with LocalDynamoDb
-    with RandomThings
-    with ArchiveMessaging
-    with ProgressTrackerFixture
     with WorkerServiceFixture
-    with Messaging
     with ScalaFutures {
 
   def withProgress[R](progressTracker: ProgressTracker)(
@@ -41,17 +23,6 @@ trait IngestsFixture
       testWith(storedProgress)
     }
   }
-
-  def withProgressUpdateFlow[R](table: Table)(
-    testWith: TestWith[(
-                         Flow[ProgressUpdate, Progress, NotUsed],
-                         ProgressTracker
-                       ),
-                       R]): R =
-    withProgressTracker(table) { progressTracker =>
-      testWith((ProgressUpdateFlow(progressTracker), progressTracker))
-    }
-
 
   def withConfiguredApp[R](
     testWith: TestWith[(Queue, Topic, Table), R]): R = {
