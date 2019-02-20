1ff4a41d32063487ff418dff34a1d9e6777e225b
Robert Kenny <r.kenny@wellcome.ac.uk>
2019-02-20 10:02:24 +0000

    Update for tooling container

--- bin/cmd_alias.sh
+++ bin/cmd_alias.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Release tooling
-alias release="$(PWD)/docker_run.py --root --aws -- -it wellcome/release_tooling:local.1"
+alias release="$(PWD)/docker_run.py --root --aws -- -it wellcome/release_tooling:65"
 
 # Terraform
 alias tf="$(PWD)/docker_run.py --root --aws -- -it --workdir=$(PWD)/terraform hashicorp/terraform:0.11.11"
