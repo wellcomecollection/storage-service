e61b9197c4a244287ec462efb1478efddf03a5f8
Alex Chan <a.chan@wellcome.ac.uk>
2019-02-20 15:34:38 +0000

    Get the bags API tests passing

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/http/models/ErrorResponse.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/http/models/ErrorResponse.scala
@@ -5,44 +5,38 @@ import java.net.URL
 import akka.http.scaladsl.model.StatusCode
 import io.circe.generic.extras.JsonKey
 
-sealed trait ErrorResponse {
-  val context: String
-  val httpStatus: Int
-  val label: String
-  val `type`: String
-}
-
 case class UserErrorResponse(
   @JsonKey("@context") context: String,
   httpStatus: Int,
   description: String,
   label: String,
   `type`: String = "Error"
-) extends ErrorResponse
+)
+
+case object UserErrorResponse {
+  def apply(context: URL,
+            statusCode: StatusCode,
+            description: String): UserErrorResponse =
+    UserErrorResponse(
+      context = context.toString,
+      httpStatus = statusCode.intValue(),
+      description = description,
+      label = statusCode.reason()
+    )
+}
 
 case class InternalServerErrorResponse(
   @JsonKey("@context") context: String,
   httpStatus: Int,
   label: String,
   `type`: String = "Error"
-) extends ErrorResponse
+)
 
-case object ErrorResponse {
-  def apply(context: URL,
-            statusCode: StatusCode,
-            description: String): ErrorResponse =
-    if (statusCode.intValue() >= 500) {
-      InternalServerErrorResponse(
-        context = context.toString,
-        httpStatus = statusCode.intValue(),
-        label = statusCode.reason()
-      )
-    } else {
-      UserErrorResponse(
-        context = context.toString,
-        httpStatus = statusCode.intValue(),
-        description = description,
-        label = statusCode.reason()
-      )
-    }
+case object InternalServerErrorResponse {
+  def apply(context: URL, statusCode: StatusCode): InternalServerErrorResponse =
+    InternalServerErrorResponse(
+      context = context.toString,
+      httpStatus = statusCode.intValue(),
+      label = statusCode.reason()
+    )
 }
