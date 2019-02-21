92acb3aed574f63876dece3d84149e2b02f09f8a
Robert Kenny <r.kenny@wellcome.ac.uk>
2019-02-21 14:02:08 +0000

    Scalaify ChecksumVerifier

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/models/bagit/ChecksumVerifier.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/models/bagit/ChecksumVerifier.scala
@@ -1,40 +1,104 @@
 package uk.ac.wellcome.platform.archive.common.models.bagit
 
 import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.model.S3ObjectInputStream
+import grizzled.slf4j.Logging
 import org.apache.commons.codec.binary.Hex
 import org.apache.commons.codec.digest.DigestUtils._
 import org.apache.commons.codec.digest.MessageDigestAlgorithms
 import uk.ac.wellcome.storage.ObjectLocation
 
-object ChecksumVerifier {
-  @throws(classOf[IllegalArgumentException])
-  def checksum(objectLocation: ObjectLocation, bagItAlgorithm: String)(
-    implicit s3Client: AmazonS3): String = {
-    checksumAlgorithms.get(bagItAlgorithm) match {
-      case Some(digestAlgorithm) =>
-        checksumObject(objectLocation, digestAlgorithm)
-      case None =>
-        throw new IllegalArgumentException(
-          f"unknown algorithm '$bagItAlgorithm'")
-    }
+import scala.util.Try
+
+object ChecksumVerifier extends Logging {
+
+  def checksum(
+    objectLocation: ObjectLocation,
+    bagItAlgorithm: String
+  )(implicit
+      s3Client: AmazonS3
+  ): Option[String] = {
+
+    for {
+
+      digestAlgorithm <- getChecksum(
+        bagItAlgorithm
+      )
+
+      checksum <- checksumObject(
+        objectLocation,
+        digestAlgorithm
+      )
+
+    } yield checksum
   }
 
   private def checksumObject(
     objectLocation: ObjectLocation,
-    digestAlgorithm: String)(implicit s3Client: AmazonS3): String = {
-    // TODO: handle exceptions thrown in stream construction and proper stream closure
-    val objectStream = s3Client
-      .getObject(objectLocation.namespace, objectLocation.key)
-      .getObjectContent
-    try {
-      Hex.encodeHexString(
-        updateDigest(getDigest(digestAlgorithm), objectStream).digest)
-    } finally {
-      objectStream.close()
-    }
+    digestAlgorithm: String
+  )(implicit s3Client: AmazonS3): Option[String] = {
+
+    val checksumResult: Try[String] = for {
+      objectContent <- tryObjectContent(
+        s3Client,
+        objectLocation
+      )
+
+      checksum <- tryChecksum(
+        objectContent,
+        digestAlgorithm
+      )
+
+    } yield checksum
+
+    val stringOption: Option[String] = checksumResult
+      .map(Some(_))
+      .recover {
+        case t: Throwable => {
+          error("Failed to get checksum!", t)
+          None
+        }
+      }.get
+
+    stringOption
   }
 
-  // Normalised BagIt checksum algorithms to MessageDigestAlgorithms
+  private def tryObjectContent(
+    s3Client: AmazonS3,
+    objectLocation: ObjectLocation
+  ): Try[S3ObjectInputStream] = Try {
+    s3Client
+      .getObject(
+        objectLocation.namespace,
+        objectLocation.key)
+      .getObjectContent
+  }
+
+  private def tryChecksum(
+    objectStream: S3ObjectInputStream,
+    digestAlgorithm: String
+  ): Try[String] = Try {
+    Hex.encodeHexString(
+      updateDigest(
+        getDigest(digestAlgorithm),
+        objectStream
+      ).digest
+    )
+  }
+
+  private def getChecksum(checksumAlgorithm: String) = {
+    val tryAlgorithm: Option[String] = checksumAlgorithms.get(checksumAlgorithm)
+
+    if(tryAlgorithm.isEmpty){
+      error(s"Unknown algorithm: $checksumAlgorithm ")
+    }
+
+    tryAlgorithm
+  }
+
+  // Normalised BagIt checksum algorithms
+  // to MessageDigestAlgorithms
   private val checksumAlgorithms = Map(
-    "sha256" -> MessageDigestAlgorithms.SHA_256)
+    "sha256" -> MessageDigestAlgorithms.SHA_256
+  )
 }
--- common/src/test/scala/uk/ac/wellcome/platform/archive/common/models/bagit/ChecksumVerifierTest.scala
+++ common/src/test/scala/uk/ac/wellcome/platform/archive/common/models/bagit/ChecksumVerifierTest.scala
@@ -1,7 +1,6 @@
 package uk.ac.wellcome.platform.archive.common.models.bagit
 
 import com.amazonaws.services.s3.AmazonS3
-import com.amazonaws.services.s3.model.AmazonS3Exception
 import org.scalatest.{FunSpec, Matchers}
 import uk.ac.wellcome.storage.ObjectLocation
 import uk.ac.wellcome.storage.fixtures.S3
@@ -13,38 +12,54 @@ class ChecksumVerifierTest extends FunSpec with Matchers with S3 {
     withLocalS3Bucket { bucket =>
       val content = "text"
       val key = "key"
+
       s3Client.putObject(bucket.name, key, content)
-      val contentChecksum =
+
+      val expectedChecksum = Some(
         "982d9e3eb996f559e633f4d194def3761d909f5a3b647d1a851fead67c32c9d1"
+      )
 
-      ChecksumVerifier.checksum(ObjectLocation(bucket.name, key), "sha256") shouldBe contentChecksum
+      val actualChecksum = ChecksumVerifier.checksum(
+        ObjectLocation(bucket.name, key),
+        bagItAlgorithm = "sha256"
+      )
+
+      actualChecksum shouldBe expectedChecksum
     }
   }
 
-  it("fails for an unknown algorithm") {
-    val thrown = intercept[IllegalArgumentException] {
-      ChecksumVerifier.checksum(ObjectLocation("bucket", "key"), "unknown")
-    }
-    thrown.getMessage shouldBe "unknown algorithm 'unknown'"
+  it("returns None for an unknown algorithm") {
+    val actualChecksum = ChecksumVerifier.checksum(
+        ObjectLocation("bucket", "key"),
+        bagItAlgorithm = "unknown"
+      )
+
+    val expectedChecksum = None
+
+    actualChecksum shouldBe expectedChecksum
   }
 
-  it("fails if the bucket cannot be found") {
-    val thrown = intercept[AmazonS3Exception] {
-      ChecksumVerifier.checksum(ObjectLocation("bucket", "not-there"), "sha256")
-    }
-    thrown.getMessage should startWith("The specified bucket does not exist.")
+  it("returns None if the bucket cannot be found") {
+    val actualChecksum = ChecksumVerifier.checksum(
+      ObjectLocation("bucket", "not-there"),
+      bagItAlgorithm = "sha256"
+    )
+
+    val expectedChecksum = None
+
+    actualChecksum shouldBe expectedChecksum
   }
 
-  it("fails if the object cannot be found") {
+  it("returns None if the object cannot be found") {
     withLocalS3Bucket { bucket =>
-      val thrown = intercept[AmazonS3Exception] {
-        ChecksumVerifier.checksum(
-          ObjectLocation(bucket.name, "not-there"),
-          "sha256")
-      }
-      thrown.getMessage should startWith("The specified key does not exist.")
+      val actualChecksum = ChecksumVerifier.checksum(
+        ObjectLocation(bucket.name, "not-there"),
+        bagItAlgorithm = "sha256"
+      )
+
+      val expectedChecksum = None
+
+      actualChecksum shouldBe expectedChecksum
     }
   }
-
-  //  TODO: it("fails if the object get/stream fails") {
 }
