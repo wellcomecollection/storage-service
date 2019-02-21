d26afefccd94647c4b6d92aafb4c48397f0c36f3
Alex Chan <a.chan@wellcome.ac.uk>
2019-02-21 13:32:58 +0000

    Revert "go parallel, get faster, do more"
    This reverts commit 01e54c10630350356ed111605d0a946392a536ba.

--- bag_replicator/src/main/scala/uk/ac/wellcome/platform/archive/bagreplicator/storage/S3PrefixCopier.scala
+++ bag_replicator/src/main/scala/uk/ac/wellcome/platform/archive/bagreplicator/storage/S3PrefixCopier.scala
@@ -7,7 +7,6 @@ import grizzled.slf4j.Logging
 import uk.ac.wellcome.storage.ObjectLocation
 
 import scala.collection.JavaConverters._
-import scala.collection.parallel.immutable.ParSeq
 import scala.concurrent.{ExecutionContext, Future}
 
 class S3PrefixCopier(
@@ -33,7 +32,7 @@ class S3PrefixCopier(
     srcLocationPrefix: ObjectLocation,
     dstLocationPrefix: ObjectLocation
   ): Future[Unit] = Future {
-    val objects: ParSeq[S3ObjectSummary] = S3Objects
+    val objects: Iterator[S3ObjectSummary] = S3Objects
       .withPrefix(
         s3Client,
         srcLocationPrefix.namespace,
@@ -42,8 +41,14 @@ class S3PrefixCopier(
       .withBatchSize(batchSize)
       .iterator()
       .asScala
-      .toStream
-      .par
+
+    // Implementation note: this means we're single-threaded within a single bag.
+    // That is, we're copying objects in a bag one at a time.
+    //
+    // We could rewrite this code to process objects in parallel, but it would
+    // make it more complicated and introduces more failure modes.  For now we'll
+    // just use this simple version, and we can revisit it if it's not fast
+    // enough in practice.
 
     objects.foreach { summary: S3ObjectSummary =>
       val srcLocation = ObjectLocation(
--- bag_replicator/src/test/scala/uk/ac/wellcome/platform/archive/bagreplicator/storage/S3PrefixCopierTest.scala
+++ bag_replicator/src/test/scala/uk/ac/wellcome/platform/archive/bagreplicator/storage/S3PrefixCopierTest.scala
@@ -18,10 +18,12 @@ class S3PrefixCopierTest
     with IntegrationPatience
     with S3CopierFixtures {
 
+  val batchSize = 10
   val copier = new S3Copier(s3Client)
   val s3PrefixCopier = new S3PrefixCopier(
     s3Client = s3Client,
-    copier = copier
+    copier = copier,
+    batchSize = batchSize
   )
 
   it("returns a successful Future if there are no files in the prefix") {
@@ -126,7 +128,7 @@ class S3PrefixCopierTest
         val srcPrefix = createObjectLocationWith(srcBucket, key = "src/")
 
         // You can get up to 1000 objects in a single S3 ListObject call.
-        val count = 1001
+        val count = 10
         val srcLocations = (1 to count).par.map { i =>
           val src = srcPrefix.copy(key = srcPrefix.key + s"$i.txt")
           createObject(src)
