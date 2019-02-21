56f2add8443170720637cdde52f2677c99c2caa0
Alex Chan <a.chan@wellcome.ac.uk>
2019-02-21 14:53:23 +0000

    Now add some more tests for UpdateStoredManifestServiceTest

--- bags/src/test/scala/uk/ac/wellcome/platform/archive/bags/async/services/UpdateStoredManifestServiceTest.scala
+++ bags/src/test/scala/uk/ac/wellcome/platform/archive/bags/async/services/UpdateStoredManifestServiceTest.scala
@@ -1,5 +1,6 @@
 package uk.ac.wellcome.platform.archive.bags.async.services
 
+import com.amazonaws.services.sns.model.AmazonSNSException
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{FunSpec, Matchers}
 import uk.ac.wellcome.fixtures.TestWith
@@ -9,6 +10,7 @@ import uk.ac.wellcome.messaging.fixtures.SNS.Topic
 import uk.ac.wellcome.platform.archive.bags.fixtures.StorageManifestVHSFixture
 import uk.ac.wellcome.platform.archive.bags.generators.StorageManifestGenerators
 import uk.ac.wellcome.platform.archive.common.progress.ProgressUpdateAssertions
+import uk.ac.wellcome.platform.archive.common.progress.models.Progress
 import uk.ac.wellcome.storage.fixtures.LocalDynamoDb.Table
 import uk.ac.wellcome.storage.fixtures.S3.Bucket
 
@@ -33,6 +35,42 @@ class UpdateStoredManifestServiceTest extends FunSpec with Matchers with ScalaFu
                 events.head.description shouldBe "Bag registered successfully"
               }
             }
+          }
+        }
+      }
+    }
+  }
+
+  it("sends a failed ProgressUpdate if updating VHS fails") {
+    val archiveRequestId = randomUUID
+    val storageManifest = createStorageManifest
+
+    val table = Table("does-not-exist", index = "does-not-exist")
+    val bucket = Bucket("does-not-exist")
+
+    withLocalSnsTopic { progressTopic =>
+      withUpdateStoredManifestService(table, bucket, progressTopic) { service =>
+        val future = service.updateManifest(archiveRequestId, storageManifest = storageManifest)
+
+        whenReady(future) { _ =>
+          assertTopicReceivesProgressStatusUpdate(archiveRequestId, progressTopic, status = Progress.Failed) { events =>
+            events should have size 1
+            events.head.description shouldBe "Failed to register bag"
+          }
+        }
+      }
+    }
+  }
+
+  it("returns a failed Future if publishing to SNS fails") {
+    withLocalDynamoDbTable { table =>
+      withLocalS3Bucket { bucket =>
+        val progressTopic = Topic("does-not-exist")
+        withUpdateStoredManifestService(table, bucket, progressTopic) { service =>
+          val future = service.updateManifest(randomUUID, storageManifest = createStorageManifest)
+
+          whenReady(future.failed) { err =>
+            err shouldBe a[AmazonSNSException]
           }
         }
       }
