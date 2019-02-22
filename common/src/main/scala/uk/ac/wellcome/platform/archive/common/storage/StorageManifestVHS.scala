1369899b4d37981c3fe68fa8b4cd8ebeec8014c0
Alex Chan <a.chan@wellcome.ac.uk>
2019-02-22 13:36:24 +0000

    Add a basic StorageManifestVHS class

---
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/storage/StorageManifestVHS.scala
@@ -0,0 +1,31 @@
+package uk.ac.wellcome.platform.archive.common.storage
+
+import uk.ac.wellcome.platform.archive.common.models.StorageManifest
+import uk.ac.wellcome.platform.archive.common.models.bagit.BagId
+import uk.ac.wellcome.storage.ObjectStore
+import uk.ac.wellcome.storage.vhs.{EmptyMetadata, VHSIndexEntry, VersionedHybridStore}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class StorageManifestVHS(
+  vhs: VersionedHybridStore[StorageManifest,
+                            EmptyMetadata,
+                            ObjectStore[StorageManifest]]
+)(implicit ec: ExecutionContext) {
+
+  def getRecord(id: BagId): Future[Option[StorageManifest]] =
+    vhs.getRecord(id = id.toString)
+
+  def updateRecord(
+    ifNotExisting: StorageManifest)(
+    ifExisting: StorageManifest => StorageManifest): Future[Unit] =
+    vhs.updateRecord(
+      id = ifNotExisting.id.toString
+    )(
+      ifNotExisting = (ifNotExisting, EmptyMetadata())
+    )(
+      ifExisting = (existingStorageManifest: StorageManifest, existingMetadata: EmptyMetadata) =>
+        (ifExisting(existingStorageManifest: StorageManifest), existingMetadata: EmptyMetadata)
+    )
+      .map { _ => () }
+}
