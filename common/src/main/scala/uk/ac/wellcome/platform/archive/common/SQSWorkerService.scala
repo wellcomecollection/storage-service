aad310ba50dc0b71294220d6dea61384ccc19bf3
Alex Chan <a.chan@wellcome.ac.uk>
2019-02-22 15:43:08 +0000

    Tweak the WorkerService to hide NotificationMessage parsing

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/SQSWorkerService.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/SQSWorkerService.scala
@@ -1,17 +1,24 @@
 package uk.ac.wellcome.platform.archive.common
 
 import akka.Done
+import io.circe.Decoder
 import uk.ac.wellcome.json.JsonUtil._
 import uk.ac.wellcome.messaging.sns.NotificationMessage
 import uk.ac.wellcome.messaging.sqs.SQSStream
 import uk.ac.wellcome.typesafe.Runnable
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
-abstract class SQSWorkerService(sqsStream: SQSStream[NotificationMessage])
+abstract class SQSWorkerService[T](sqsStream: SQSStream[NotificationMessage])(implicit decoder: Decoder[T], ec: ExecutionContext)
     extends Runnable {
   def run(): Future[Done] =
-    sqsStream.foreach(this.getClass.getSimpleName, processMessage)
+    sqsStream.foreach(this.getClass.getSimpleName, processNotification)
 
-  def processMessage(notificationMessage: NotificationMessage): Future[Unit]
+  def processNotification(notificationMessage: NotificationMessage): Future[Unit] =
+    for {
+      message <- Future.fromTry(fromJson[T](notificationMessage.body))
+      _ <- processMessage(message)
+    } yield ()
+
+  def processMessage(message: T): Future[Unit]
 }
