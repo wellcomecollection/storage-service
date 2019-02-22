00919afe79d4e907452379e789d8f0bfbc92eb18
Alex Chan <a.chan@wellcome.ac.uk>
2019-02-22 11:56:33 +0000

    And now swap out the Akka for the WorkerService

--- bags/src/main/scala/uk/ac/wellcome/platform/archive/bags/async/Main.scala
+++ bags/src/main/scala/uk/ac/wellcome/platform/archive/bags/async/Main.scala
@@ -4,29 +4,38 @@ import akka.actor.ActorSystem
 import com.typesafe.config.Config
 import uk.ac.wellcome.json.JsonUtil._
 import uk.ac.wellcome.messaging.sns.NotificationMessage
-import uk.ac.wellcome.messaging.typesafe.SNSBuilder
-import uk.ac.wellcome.platform.archive.common.config.builders._
+import uk.ac.wellcome.messaging.typesafe.{SNSBuilder, SQSBuilder}
+import uk.ac.wellcome.platform.archive.bags.async.services.{
+  BagsWorkerService,
+  StorageManifestService,
+  UpdateStoredManifestService
+}
 import uk.ac.wellcome.platform.archive.bags.common.models.StorageManifest
 import uk.ac.wellcome.storage.typesafe.{S3Builder, VHSBuilder}
 import uk.ac.wellcome.storage.vhs.EmptyMetadata
 import uk.ac.wellcome.typesafe.WellcomeTypesafeApp
 import uk.ac.wellcome.typesafe.config.builders.AkkaBuilder
 
+import scala.concurrent.ExecutionContext
+
 object Main extends WellcomeTypesafeApp {
   runWithConfig { config: Config =>
     implicit val actorSystem: ActorSystem = AkkaBuilder.buildActorSystem()
+    implicit val executionContext: ExecutionContext = AkkaBuilder.buildExecutionContext()
 
-    val messageStream =
-      MessagingBuilder.buildMessageStream[NotificationMessage, Unit](config)
+    val storageManifestService = new StorageManifestService(
+      s3Client = S3Builder.buildS3Client(config)
+    )
 
-    val dataStore = VHSBuilder.buildVHS[StorageManifest, EmptyMetadata](config)
+    val updateStoredManifestService = new UpdateStoredManifestService(
+      vhs = VHSBuilder.buildVHS[StorageManifest, EmptyMetadata](config)
+    )
 
-    new Registrar(
-      snsClient = SNSBuilder.buildSNSClient(config),
-      progressSnsConfig = SNSBuilder.buildSNSConfig(config),
-      s3Client = S3Builder.buildS3Client(config),
-      messageStream = messageStream,
-      dataStore = dataStore
+    new BagsWorkerService(
+      sqsStream = SQSBuilder.buildSQSStream[NotificationMessage](config),
+      storageManifestService = storageManifestService,
+      updateStoredManifestService = updateStoredManifestService,
+      progressSnsWriter = SNSBuilder.buildSNSWriter(config)
     )
   }
 }
