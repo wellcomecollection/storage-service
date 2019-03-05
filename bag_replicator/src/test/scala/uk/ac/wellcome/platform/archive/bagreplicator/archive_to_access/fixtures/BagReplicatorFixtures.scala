72ca66f09377bcb300de3e4c1bd87d40cbca7fdd
Alex Chan <a.chan@wellcome.ac.uk>
2019-03-05 13:35:34 +0000

    Set up a common lib

---
+++ bag_replicator/src/test/scala/uk/ac/wellcome/platform/archive/bagreplicator/archive_to_access/fixtures/BagReplicatorFixtures.scala
@@ -0,0 +1,31 @@
+package uk.ac.wellcome.platform.archive.bagreplicator.fixtures
+
+import com.amazonaws.services.s3.model.S3ObjectSummary
+import org.scalatest.Assertion
+import uk.ac.wellcome.platform.archive.common.fixtures.RandomThings
+import uk.ac.wellcome.platform.archive.common.models.bagit.BagLocation
+import uk.ac.wellcome.storage.fixtures.S3
+
+import scala.collection.JavaConverters._
+
+trait BagReplicatorFixtures extends S3 with RandomThings {
+
+  def verifyBagCopied(src: BagLocation, dst: BagLocation): Assertion = {
+    val sourceItems = getObjectSummaries(src)
+    val sourceKeyEtags = sourceItems.map { _.getETag }
+
+    val destinationItems = getObjectSummaries(dst)
+    val destinationKeyEtags = destinationItems.map { _.getETag }
+
+    destinationKeyEtags should contain theSameElementsAs sourceKeyEtags
+  }
+
+  private def getObjectSummaries(
+    bagLocation: BagLocation): List[S3ObjectSummary] =
+    s3Client
+      .listObjects(bagLocation.storageNamespace, bagLocation.completePath)
+      .getObjectSummaries
+      .asScala
+      .toList
+
+}
