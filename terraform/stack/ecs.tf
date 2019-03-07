a6ac960e192ff58f8b9a02232ede48ba6ed33f52
Alex Chan <alex@alexwlchan.net>
2019-03-07 13:15:20 +0000

    Revert "Delete the archivist cluster hosts"
    8e357707d46748cd11a326370519c127f0f044b0

--- terraform/stack/ecs.tf
+++ terraform/stack/ecs.tf
@@ -6,3 +6,23 @@ resource "aws_service_discovery_private_dns_namespace" "namespace" {
   name = "${var.namespace}"
   vpc  = "${var.vpc_id}"
 }
+
+module "cluster_hosts" {
+  source = "git::https://github.com/wellcometrust/terraform.git//ecs/modules/ec2/prebuilt/nvm?ref=v17.1.0"
+
+  image_id = "ami-0de29b072b458b107"
+
+  vpc_id   = "${var.vpc_id}"
+  key_name = "${var.ssh_key_name}"
+  asg_name = "${var.namespace}"
+
+  subnets = "${var.private_subnets}"
+
+  cluster_name = "${aws_ecs_cluster.cluster.name}"
+
+  asg_min     = "1"
+  asg_desired = "${var.desired_ec2_instances}"
+  asg_max     = "${var.desired_ec2_instances}"
+
+  controlled_access_cidr_ingress = ["${var.vpc_cidr}"]
+}
