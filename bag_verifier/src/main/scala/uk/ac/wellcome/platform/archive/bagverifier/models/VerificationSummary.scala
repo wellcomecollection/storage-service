3c5c177ca27272e46d586186eed7a13fe665ee2e
mklander <mklanderyou@gmail.com>
2019-03-08 14:50:56 +0000

    Summary toString

--- bag_verifier/src/main/scala/uk/ac/wellcome/platform/archive/bagverifier/models/VerificationSummary.scala
+++ bag_verifier/src/main/scala/uk/ac/wellcome/platform/archive/bagverifier/models/VerificationSummary.scala
@@ -3,16 +3,26 @@ package uk.ac.wellcome.platform.archive.bagverifier.models
 import java.time.Instant
 
 import uk.ac.wellcome.platform.archive.common.models.bagit.BagDigestFile
-import uk.ac.wellcome.platform.archive.common.operation.Timed
+import uk.ac.wellcome.platform.archive.common.operation.Summary
 
 case class VerificationSummary(
   successfulVerifications: Seq[BagDigestFile] = List.empty,
   failedVerifications: Seq[FailedVerification] = List.empty,
   startTime: Instant = Instant.now(),
   endTime: Option[Instant] = None)
-    extends Timed {
+    extends Summary {
   def succeeded: Boolean = failedVerifications.isEmpty
   def complete: VerificationSummary = this.copy(endTime = Some(Instant.now()))
+
+  override def toString: String = {
+    f"""|verified in $describeDuration
+        | :
+        |${successfulVerifications.size} succeeded /
+        |${failedVerifications.size} failed
+        |"""
+      .stripMargin
+      .replaceAll("\n", " ")
+  }
 }
 
 case class FailedVerification(
