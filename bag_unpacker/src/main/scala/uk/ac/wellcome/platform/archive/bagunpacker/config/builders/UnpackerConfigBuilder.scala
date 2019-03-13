3bc447fcbf8cb4e599bcb70344646a7b7c3c65a2
mklander <mklanderyou@gmail.com>
2019-03-13 17:52:31 +0000

    parse bufferSize from config even if it is a String.

--- bag_unpacker/src/main/scala/uk/ac/wellcome/platform/archive/bagunpacker/config/builders/UnpackerConfigBuilder.scala
+++ bag_unpacker/src/main/scala/uk/ac/wellcome/platform/archive/bagunpacker/config/builders/UnpackerConfigBuilder.scala
@@ -2,17 +2,21 @@ package uk.ac.wellcome.platform.archive.bagunpacker.config.builders
 
 import com.typesafe.config.Config
 import uk.ac.wellcome.platform.archive.bagunpacker.config.models.UnpackerConfig
-import uk.ac.wellcome.typesafe.config.builders.EnrichConfig._
 
 object UnpackerConfigBuilder {
+  val BUFFER_SIZE_DEFAULT = 8192
+  val BUFFER_SIZE_PATH = "unpacker.buffer.size"
+
   def build(config: Config): UnpackerConfig = {
-    val DEFAULT_BUFFER_SIZE = 8192
-
-    val bufferSize =
-      config.getOrElse[Int]("unpacker.buffer.size")(DEFAULT_BUFFER_SIZE)
-
     UnpackerConfig(
-      bufferSize = bufferSize
+      bufferSize = parseBufferSize(config)
     )
   }
+
+  private def parseBufferSize(config: Config): Int =
+    if (config.hasPath(BUFFER_SIZE_PATH)) {
+      config.getInt(BUFFER_SIZE_PATH)
+    } else {
+      BUFFER_SIZE_DEFAULT
+    }
 }
