c8ec86d7aad90597dda49da46bf3a3cf9d36b40d
mklander <mklanderyou@gmail.com>
2019-03-13 18:38:42 +0000

    set bufferSize to 128Kb

--- bag_unpacker/src/main/scala/uk/ac/wellcome/platform/archive/bagunpacker/config/builders/UnpackerConfigBuilder.scala
+++ bag_unpacker/src/main/scala/uk/ac/wellcome/platform/archive/bagunpacker/config/builders/UnpackerConfigBuilder.scala
@@ -1,10 +1,11 @@
 package uk.ac.wellcome.platform.archive.bagunpacker.config.builders
 
+import com.amazonaws.RequestClientOptions
 import com.typesafe.config.Config
 import uk.ac.wellcome.platform.archive.bagunpacker.config.models.UnpackerConfig
 
 object UnpackerConfigBuilder {
-  val BUFFER_SIZE_DEFAULT = 8192
+  val BUFFER_SIZE_DEFAULT: Int = RequestClientOptions.DEFAULT_STREAM_BUFFER_SIZE
   val BUFFER_SIZE_PATH = "unpacker.buffer.size"
 
   def build(config: Config): UnpackerConfig = {
