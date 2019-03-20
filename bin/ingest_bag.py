92b32062bfba0c8b836cb998ddd0a769e8c292cb
Alex Chan <a.chan@wellcome.ac.uk>
2019-03-20 15:50:00 +0000

    Modify the ingest_bag script to be a progress monitor
    * Keep polling the ingests endpoint to track the progress of the bag
    * Report only when there's an update, so you can easily see changes
    * Add a red/green print on the final result so you can see when it stops

--- bin/ingest_bag.py
+++ bin/ingest_bag.py
@@ -3,11 +3,11 @@
 """
 Create a request to ingest a bag into storage
 
-Usage: ingest_bag.py <BAG>... [--oauth-credentials=<OAUTH_CREDENTIALS>] [--bucket=<BUCKET_NAME>] [--storage-space=<SPACE_NAME>]  [--api=<API>]
+Usage: ingest_bag.py <BAG> [--oauth-credentials=<OAUTH_CREDENTIALS>] [--bucket=<BUCKET_NAME>] [--storage-space=<SPACE_NAME>]  [--api=<API>]
        ingest_bag.py -h | --help
 
 Arguments:
-    BAG                    paths to BagIt locations to ingest
+    BAG                    Path to BagIt locations to ingest
 
 Examples:
     ingest_bag.py b22454408.zip
@@ -27,27 +27,28 @@ OAuth details:
   Credentials are supplied in a file (default ~/.wellcome-storage/oauth-credentials.json) with the following Json
 
   {
-    "token_url": "https://auth.wellcomecollection.org/oauth2/token"
-    "client_id": "YOUR-CLIENT-ID"
+    "token_url": "https://auth.wellcomecollection.org/oauth2/token",
+    "client_id": "YOUR-CLIENT-ID",
     "client_secret": "YOUR-CLIENT-SECRET"
   }
 """
 
-import os
-
-import docopt
+import datetime as dt
 import json
-from requests_oauthlib import OAuth2Session
+import os
+import time
+
+import crayons
+import docopt
 from oauthlib.oauth2 import BackendApplicationClient
+from requests_oauthlib import OAuth2Session
 
 
-def oauth_details_from_file(filepath):
+def oauth_details_from_file(path):
     """
     Obtain OAuth details from a file
     """
-    with open(filepath, "r") as f:
-        oauth_credentials = json.load(f)
-    return oauth_credentials
+    return json.load(open(path))
 
 
 def oauth_session(token_url, client_id, client_secret):
@@ -62,54 +63,79 @@ def oauth_session(token_url, client_id, client_secret):
     return api_session
 
 
-def archive_bag_api_messages(bag_paths, space, ingest_bucket):
-    """
-    Generates bag archive messages.
-    """
-    for bag_path in bag_paths:
-        yield {
-            "type": "Ingest",
-            "ingestType": {"id": "create", "type": "IngestType"},
-            "space": {"id": space, "type": "Space"},
-            "sourceLocation": {
-                "type": "Location",
-                "provider": {"type": "Provider", "id": "aws-s3-standard"},
-                "bucket": ingest_bucket,
-                "path": bag_path,
-            },
-        }
-
-
-def call_ingest_api(ingest_bucket_name, bag_paths, space, ingests_endpoint, session):
+def call_ingest_api(ingest_bucket_name, bag_path, space, ingests_endpoint, session):
     """
     Call the storage ingests api to ingest bags
     """
-    for message in archive_bag_api_messages(bag_paths, space, ingest_bucket_name):
-        response = session.post(ingests_endpoint, json=message)
-        status_code = response.status_code
-        if status_code != 201:
-            print_result(f"ERROR calling {ingests_endpoint} with {message}", response)
-        else:
-            print(f"{message} -> {ingests_endpoint} [{status_code}]")
-            location = response.headers.get("Location")
-            ingest = session.get(location)
-            if location:
-                print_result(location, ingest)
+    message = {
+        "type": "Ingest",
+        "ingestType": {"id": "create", "type": "IngestType"},
+        "space": {"id": space, "type": "Space"},
+        "sourceLocation": {
+            "type": "Location",
+            "provider": {"type": "Provider", "id": "aws-s3-standard"},
+            "bucket": ingest_bucket_name,
+            "path": bag_path,
+        },
+    }
+
+    response = session.post(ingests_endpoint, json=message)
+    status_code = response.status_code
+    if status_code != 201:
+        print_result(f"ERROR calling {ingests_endpoint} with {message}", response)
+    else:
+        print(f"{message} -> {ingests_endpoint} [{status_code}]")
+        location = response.headers.get("Location")
+
+        last_resp = None
+        while True:
+            resp = session.get(location)
+            if (last_resp is None) or (resp.json() != last_resp.json()):
+                print("\nResponse at %s" % dt.datetime.now().strftime("%H:%M:%S"))
+
+                if last_resp is not None:
+                    trimmed_resp = {
+                        "status": resp.json()["status"],
+                        "events": resp.json()["events"],
+                        "createdDate": resp.json()["createdDate"],
+                        "lastModifiedDate": resp.json()["lastModifiedDate"]
+                    }
+                    print_result(location, trimmed_resp)
+                else:
+                    print_result(location, resp)
+
+                last_resp = resp
+
+            if resp.json()["status"]["id"] in ("succeeded", "failed"):
+                break
+
+            time.sleep(2)
 
 
 def print_result(description, result):
     """
     pretty print the result an ingest request
     """
+    try:
+        data = result.json()
+    except AttributeError:
+        data = result
+
     print(description)
-    dumped_json = json.dumps(result.json(), indent=2)
-    print(dumped_json)
+    dumped_json = json.dumps(data, indent=2)
+
+    if data["status"]["id"] == "succeeded":
+        print(crayons.green(dumped_json))
+    elif data["status"]["id"] == "failed":
+        print(crayons.red(dumped_json))
+    else:
+        print(dumped_json)
 
 
 def main():
     args = docopt.docopt(__doc__)
 
-    bag_paths = args["<BAG>"]
+    bag_path = args["<BAG>"]
     space = args["--storage-space"]
 
     oauth_filepath = os.path.expanduser(args["--oauth-credentials"])
@@ -129,7 +155,13 @@ def main():
     if ingests_endpoint in api_lookup:
         ingests_endpoint = api_lookup[ingests_endpoint]
 
-    call_ingest_api(ingest_bucket_name, bag_paths, space, ingests_endpoint, api_session)
+    call_ingest_api(
+        ingest_bucket_name=ingest_bucket_name,
+        bag_path=bag_path,
+        space=space,
+        ingests_endpoint=ingests_endpoint,
+        session=api_session
+    )
 
 
 if __name__ == "__main__":
