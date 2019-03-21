1882df0674aef63c8e115e227c8c902febc8112f
Alex Chan <a.chan@wellcome.ac.uk>
2019-03-21 11:10:38 +0000

    Remove an unused model

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/bagit/models/BagPath.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/bagit/models/BagPath.scala
@@ -3,8 +3,3 @@ package uk.ac.wellcome.platform.archive.common.bagit.models
 case class BagPath(underlying: String) extends AnyVal {
   override def toString: String = underlying
 }
-
-object BagPath {
-  def create(identifier: ExternalIdentifier): BagPath =
-    BagPath(identifier.underlying)
-}
