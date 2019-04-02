1c3055bc08806e1adad95790ad5e50fc20dce37b
Alex Chan <a.chan@wellcome.ac.uk>
2019-04-02 17:07:31 +0100

    Start using the new S3BagLocator class

--- bag_replicator/src/main/scala/uk/ac/wellcome/platform/archive/bagreplicator/services/BagLocator.scala
+++ bag_replicator/src/main/scala/uk/ac/wellcome/platform/archive/bagreplicator/services/BagLocator.scala
@@ -9,14 +9,11 @@ import uk.ac.wellcome.platform.archive.common.bagit.models.{
   ExternalIdentifier
 }
 import uk.ac.wellcome.platform.archive.common.bagit.parsers.BagInfoParser
-import uk.ac.wellcome.platform.archive.common.bagit.services.S3BagFile
 import uk.ac.wellcome.storage.ObjectLocation
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class BagLocator(s3Client: AmazonS3)(implicit ec: ExecutionContext) {
-  val s3BagFile = new S3BagFile(s3Client)
-
   val s3BagLocator = new S3BagLocator(s3Client)
 
   implicit val _ = s3Client
@@ -24,10 +21,10 @@ class BagLocator(s3Client: AmazonS3)(implicit ec: ExecutionContext) {
   def getBagIdentifier(
     objectLocation: ObjectLocation): Future[ExternalIdentifier] =
     for {
-      bagInfoPath: String <- Future.fromTry(
-        s3BagFile.locateBagInfo(objectLocation))
-      inputStream: InputStream <- objectLocation
-        .copy(key = bagInfoPath)
+      bagInfoLocation <- Future.fromTry {
+        s3BagLocator.locateBagInfo(objectLocation)
+      }
+      inputStream: InputStream <- bagInfoLocation
         .toInputStream
       bagInfo: BagInfo <- BagInfoParser.create(inputStream)
     } yield bagInfo.externalIdentifier
