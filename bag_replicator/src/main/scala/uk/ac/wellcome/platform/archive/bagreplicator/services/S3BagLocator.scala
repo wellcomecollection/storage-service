53091c680d22d82b6f9d64b0d746952a698981a5
Alex Chan <a.chan@wellcome.ac.uk>
2019-04-02 16:17:29 +0100

    Okay, now look inside a subdirectory

--- bag_replicator/src/main/scala/uk/ac/wellcome/platform/archive/bagreplicator/services/S3BagLocator.scala
+++ bag_replicator/src/main/scala/uk/ac/wellcome/platform/archive/bagreplicator/services/S3BagLocator.scala
@@ -1,18 +1,30 @@
 package uk.ac.wellcome.platform.archive.bagreplicator.services
 
 import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.model.{ListObjectsV2Request, ListObjectsV2Result}
+import grizzled.slf4j.Logging
 import uk.ac.wellcome.storage.ObjectLocation
 
+import scala.collection.JavaConverters._
 import scala.util.{Failure, Success, Try}
 
-class S3BagLocator(s3Client: AmazonS3) {
-  def locateBagInfo(objectLocation: ObjectLocation): Try[ObjectLocation] =
-    findBagInfoInRoot(objectLocation) match {
-      case Some(key) => Success(objectLocation.copy(key = key))
-      case None => Failure(new IllegalArgumentException("Unable to locate root of bag"))
-    }
+class S3BagLocator(s3Client: AmazonS3) extends Logging {
+  def locateBagInfo(objectLocation: ObjectLocation): Try[ObjectLocation] = {
+    val bagInfoInRoot = findBagInfoInRoot(objectLocation)
+    val bagInfoInDirectory = findBagInfoInDirectory(objectLocation)
 
-  private def findBagInfoInRoot(objectLocation: ObjectLocation): Option[String] = {
+    (bagInfoInRoot, bagInfoInDirectory) match {
+      case (Success(key), _) => Success(objectLocation.copy(key = key))
+      case (_, Success(key)) => Success(objectLocation.copy(key = key))
+      case (Failure(rootErr), Failure(dirError)) => {
+        warn(s"Could not find bag in root: ${rootErr.getMessage}")
+        warn(s"Could not find bag in subdir: ${dirError.getMessage}")
+        Failure(new IllegalArgumentException("Unable to locate root of bag"))
+      }
+    }
+  }
+
+  private def findBagInfoInRoot(objectLocation: ObjectLocation): Try[String] = {
     val listObjectsResult = s3Client.listObjectsV2(
       objectLocation.namespace,
       createBagInfoPath(objectLocation.key)
@@ -21,9 +33,34 @@ class S3BagLocator(s3Client: AmazonS3) {
     val keyCount = listObjectsResult.getObjectSummaries.size()
 
     if (keyCount == 1) {
-      Some(createBagInfoPath(objectLocation.key))
+      Success(createBagInfoPath(objectLocation.key))
     } else {
-      None
+      Failure(new RuntimeException(s"No bag-info.txt inside $objectLocation"))
+    }
+  }
+
+  private def findBagInfoInDirectory(objectLocation: ObjectLocation): Try[String] = {
+    val listObjectsRequest = new ListObjectsV2Request()
+      .withBucketName(objectLocation.namespace)
+      .withPrefix(objectLocation.key + "/")
+      .withDelimiter("/")
+
+    val listObjectsResult: ListObjectsV2Result = s3Client.listObjectsV2(listObjectsRequest)
+
+    val directoriesInBag: Seq[String] = listObjectsResult.getCommonPrefixes.asScala
+
+    if (directoriesInBag.size == 1) {
+      val directoryLocation = objectLocation.copy(
+        key = directoriesInBag.head
+      )
+
+      findBagInfoInRoot(directoryLocation)
+    } else {
+      Failure(
+        new RuntimeException(
+          s"Expected exactly one directory in archive; saw <<${listObjectsResult.getCommonPrefixes}>>"
+        )
+      )
     }
   }
 
--- bag_replicator/src/test/scala/uk/ac/wellcome/platform/archive/bagreplicator/services/S3BagLocatorTest.scala
+++ bag_replicator/src/test/scala/uk/ac/wellcome/platform/archive/bagreplicator/services/S3BagLocatorTest.scala
@@ -9,21 +9,37 @@ import scala.util.Success
 class S3BagLocatorTest extends FunSpec with Matchers with S3 {
   it("locates a bag-info.txt file in the root") {
     withLocalS3Bucket { bucket =>
-      createObjectWith(bucket,
-        "/bag123/bag-info.txt", "/bag123/data/1.jpg", "/bag123/data/2.jpg"
+      createObjectsWith(bucket,
+        "bag123/bag-info.txt", "bag123/data/1.jpg", "bag123/data/2.jpg"
       )
 
-      val objectLocation = createObjectLocationWith(bucket, "/bag123")
+      val objectLocation = createObjectLocationWith(bucket, "bag123")
 
       s3BagLocator.locateBagInfo(objectLocation) shouldBe Success(
-        createObjectLocationWith(bucket, "/bag123/bag-info.txt")
+        createObjectLocationWith(bucket, "bag123/bag-info.txt")
+      )
+    }
+  }
+
+  it("finds a bag-info.txt in an immediate subdirectory") {
+    withLocalS3Bucket { bucket =>
+      createObjectsWith(bucket,
+        "bag123/subdir/bag-info.txt",
+        "bag123/subdir/data/1.jpg",
+        "bag123/subdir/data/2.jpg"
+      )
+
+      val objectLocation = createObjectLocationWith(bucket, "bag123")
+
+      s3BagLocator.locateBagInfo(objectLocation) shouldBe Success(
+        createObjectLocationWith(bucket, "bag123/subdir/bag-info.txt")
       )
     }
   }
 
   it("throws an IllegalArgumentException if it cannot find a bag-info.txt") {
     withLocalS3Bucket { bucket =>
-      val objectLocation = createObjectLocationWith(bucket, "/doesnotexist")
+      val objectLocation = createObjectLocationWith(bucket, "doesnotexist")
 
       val result = s3BagLocator.locateBagInfo(objectLocation)
       result.isFailure shouldBe true
@@ -33,7 +49,7 @@ class S3BagLocatorTest extends FunSpec with Matchers with S3 {
 
   val s3BagLocator = new S3BagLocator(s3Client)
 
-  def createObjectWith(bucket: Bucket, keys: String*): Unit =
+  def createObjectsWith(bucket: Bucket, keys: String*): Unit =
     keys.foreach { k =>
       s3Client.putObject(bucket.name, k, "example object")
     }
