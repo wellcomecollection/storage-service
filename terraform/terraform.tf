a00e39b0c0ef32319a8a0200b7a739ce60501ddb
Alex Chan <a.chan@wellcome.ac.uk>
2019-04-02 13:51:00 +0100

    Roll back some changes to reduce diff churn

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -9,6 +9,26 @@ terraform {
   }
 }
 
+provider "aws" {
+  assume_role {
+    role_arn = "arn:aws:iam::975596993436:role/admin"
+  }
+
+  region  = "${var.aws_region}"
+  version = "1.60.0"
+}
+
+provider "aws" {
+  alias = "platform"
+
+  assume_role {
+    role_arn = "arn:aws:iam::760097843905:role/developer"
+  }
+
+  region  = "${var.aws_region}"
+  version = "1.60.0"
+}
+
 data "terraform_remote_state" "infra_shared" {
   backend = "s3"
 
