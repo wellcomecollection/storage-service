2f797a41f237fd61a7d2bcbebeb08741f7cb7d25
Alex Chan <a.chan@wellcome.ac.uk>
2019-04-02 13:46:32 +0100

    Don't define the Archivematica bucket policy here

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -14,31 +14,3 @@ data "aws_iam_role" "colbert_bag_unpacker_task_role" {
 data "aws_iam_role" "stewart_bag_unpacker_task_role" {
   name = "${module.stack-stewart.unpacker_task_role_name}"
 }
-
-data "aws_iam_policy_document" "archivematica_ingests_bucket_policy" {
-  statement {
-    actions = [
-      "s3:Get*",
-    ]
-
-    resources = [
-      "arn:aws:s3:::${local.archivematica_ingests_bucket}/*",
-    ]
-
-    principals {
-      type = "AWS"
-
-      identifiers = [
-        "${data.aws_iam_role.colbert_bag_unpacker_task_role.arn}",
-        "${data.aws_iam_role.stewart_bag_unpacker_task_role.arn}",
-      ]
-    }
-  }
-}
-
-resource "aws_s3_bucket_policy" "archivematica_ingests_bucket_policy" {
-  bucket = "${local.archivematica_ingests_bucket}"
-  policy = "${data.aws_iam_policy_document.archivematica_ingests_bucket_policy.json}"
-
-  provider = "aws.workflow"
-}
