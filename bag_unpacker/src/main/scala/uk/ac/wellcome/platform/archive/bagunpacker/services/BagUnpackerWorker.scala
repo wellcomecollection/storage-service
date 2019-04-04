feb1da09de1f44811658be541d7682024ba53124
mklander <mklanderyou@gmail.com>
2019-04-04 11:34:51 +0100

    remove Future when converting summary to stepResult

--- bag_unpacker/src/main/scala/uk/ac/wellcome/platform/archive/bagunpacker/services/BagUnpackerWorker.scala
+++ bag_unpacker/src/main/scala/uk/ac/wellcome/platform/archive/bagunpacker/services/BagUnpackerWorker.scala
@@ -4,39 +4,21 @@ import akka.actor.ActorSystem
 import com.amazonaws.services.sqs.AmazonSQSAsync
 import grizzled.slf4j.Logging
 import uk.ac.wellcome.json.JsonUtil._
-import uk.ac.wellcome.messaging.sqsworker.alpakka.{
-  AlpakkaSQSWorker,
-  AlpakkaSQSWorkerConfig
-}
+import uk.ac.wellcome.messaging.sqsworker.alpakka.{AlpakkaSQSWorker, AlpakkaSQSWorkerConfig}
 import uk.ac.wellcome.messaging.worker.models.{DeterministicFailure, Successful}
 import uk.ac.wellcome.messaging.worker.monitoring.MonitoringClient
 import uk.ac.wellcome.platform.archive.bagunpacker.builders.BagLocationBuilder
 import uk.ac.wellcome.platform.archive.bagunpacker.config.models.BagUnpackerWorkerConfig
 import uk.ac.wellcome.platform.archive.bagunpacker.models.UnpackSummary
-import uk.ac.wellcome.platform.archive.common.exception.{
-  InvalidObjectLocationException,
-  PutObjectLocationException
-}
-import uk.ac.wellcome.platform.archive.common.ingests.models.{
-  BagRequest,
-  UnpackBagRequest
-}
+import uk.ac.wellcome.platform.archive.common.exception.{InvalidObjectLocationException, PutObjectLocationException}
+import uk.ac.wellcome.platform.archive.common.ingests.models.{BagRequest, UnpackBagRequest}
 import uk.ac.wellcome.platform.archive.common.ingests.services.IngestUpdater
-import uk.ac.wellcome.platform.archive.common.operation.models.{
-  WorkFailed,
-  WorkResult,
-  WorkSucceeded
-}
+import uk.ac.wellcome.platform.archive.common.operation.models.{WorkFailed, WorkResult, WorkSucceeded}
 import uk.ac.wellcome.platform.archive.common.operation.services._
-import uk.ac.wellcome.platform.archive.common.storage.models.{
-  IngestCompleted,
-  IngestFailed,
-  IngestStepSuccess
-}
+import uk.ac.wellcome.platform.archive.common.storage.models.{IngestCompleted, IngestFailed, IngestStepSuccess}
 import uk.ac.wellcome.typesafe.Runnable
 
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.{Failure, Success}
 
 case class BagUnpackerWorker(alpakkaSQSWorkerConfig: AlpakkaSQSWorkerConfig,
                              bagUnpackerWorkerConfig: BagUnpackerWorkerConfig,
@@ -55,44 +37,35 @@ case class BagUnpackerWorker(alpakkaSQSWorkerConfig: AlpakkaSQSWorkerConfig,
         val location =
           BagLocationBuilder.build(unpackBagRequest, bagUnpackerWorkerConfig)
         for {
-          summaryResult <- stepResultFor(
-            unpacker.unpack(
-              unpackBagRequest.requestId.toString,
-              unpackBagRequest.sourceLocation,
-              location.objectLocation))
-          _ <- ingestUpdater.send(unpackBagRequest.requestId, summaryResult)
+          summaryResult <- unpacker.unpack(
+            unpackBagRequest.requestId.toString,
+            unpackBagRequest.sourceLocation,
+            location.objectLocation)
+          stepResult = stepResultFor(summaryResult)
+          _ <- ingestUpdater.send(unpackBagRequest.requestId, stepResult)
           _ <- outgoingPublisher.sendIfSuccessful(
-            summaryResult,
+            stepResult,
             BagRequest(unpackBagRequest.requestId, location))
-
-          result = summaryResult match {
-            case IngestStepSuccess(s)  => Successful(Some(s))
-            case IngestCompleted(s)    => Successful(Some(s))
-            case IngestFailed(s, t, _) => DeterministicFailure(t, Some(s))
-          }
-        } yield result
+        } yield stepResult match {
+          case IngestStepSuccess(s)  => Successful(Some(s))
+          case IngestCompleted(s)    => Successful(Some(s))
+          case IngestFailed(s, t, _) => DeterministicFailure(t, Some(s))
+        }
     }
 
-  private def stepResultFor(
-    eventualResult: Future[WorkResult[UnpackSummary]]) = {
-    eventualResult.transform {
-      case Success(workSucceeded: WorkSucceeded[UnpackSummary]) =>
-        Success(IngestStepSuccess(workSucceeded.summary))
-      case Success(
-          WorkFailed(s: UnpackSummary, e: InvalidObjectLocationException)) =>
-        Success(
+  private def stepResultFor(result: WorkResult[UnpackSummary]) = {
+    result match {
+      case workSucceeded: WorkSucceeded[UnpackSummary] =>
+        IngestStepSuccess(workSucceeded.summary)
+      case WorkFailed(unpackSummary: UnpackSummary, exception: InvalidObjectLocationException) =>
           IngestFailed(
-            s,
-            e,
-            Some(s"location '${e.getObjectLocation}' was invalid")))
-      case Success(
-          WorkFailed(s: UnpackSummary, e: PutObjectLocationException)) =>
-        Success(
-          IngestFailed(s, e, Some(s"${e.getMessage} '${e.getObjectLocation}'")))
-      case Success(WorkFailed(s: UnpackSummary, e: Throwable)) =>
-        Success(IngestFailed(s, e))
-      case Failure(e) =>
-        Failure(e)
+            unpackSummary,
+            exception,
+            Some(s"location '${exception.getObjectLocation}' was invalid"))
+      case WorkFailed(s: UnpackSummary, e: PutObjectLocationException) =>
+          IngestFailed(s, e, Some(s"${e.getMessage} '${e.getObjectLocation}'"))
+      case WorkFailed(s: UnpackSummary, e: Throwable) =>
+        IngestFailed(s, e)
     }
   }
 
