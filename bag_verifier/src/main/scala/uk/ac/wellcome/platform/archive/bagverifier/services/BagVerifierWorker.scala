b20cc57024d1f6f3156960df26e4cdb955153c7f
mklander <mklanderyou@gmail.com>
2019-04-08 14:25:47 +0100

    Resolve merge branch 'master' into user-errors-from-exceptions

--- bag_verifier/src/main/scala/uk/ac/wellcome/platform/archive/bagverifier/services/BagVerifierWorker.scala
+++ bag_verifier/src/main/scala/uk/ac/wellcome/platform/archive/bagverifier/services/BagVerifierWorker.scala
@@ -15,7 +15,7 @@ import uk.ac.wellcome.platform.archive.bagverifier.models.VerificationSummary
 import uk.ac.wellcome.platform.archive.common.ingests.models.BagRequest
 import uk.ac.wellcome.platform.archive.common.ingests.services.IngestUpdater
 import uk.ac.wellcome.platform.archive.common.operation.services.OutgoingPublisher
-import uk.ac.wellcome.platform.archive.common.storage.models.IngestStepWorker
+import uk.ac.wellcome.platform.archive.common.storage.models.{IngestStepResult, IngestStepWorker}
 import uk.ac.wellcome.typesafe.Runnable
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -43,8 +43,7 @@ class BagVerifierWorker(
 
   def processMessage(request: BagRequest): Future[Result[VerificationSummary]] =
     for {
-      verificationSummary <- verifier.verify(request.bagLocation)
-
+      verificationSummary: IngestStepResult[VerificationSummary] <- verifier.verify(request.bagLocation)
       _ <- ingestUpdater.send(request.requestId, verificationSummary)
       _ <- outgoingPublisher.sendIfSuccessful(verificationSummary, request)
     } yield toResult(verificationSummary)
