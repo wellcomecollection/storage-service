4644e6156956ded9975af76ef89e053924ee4d03
Alex Chan <alex@alexwlchan.net>
2019-04-29 08:41:40 +0100

    Use the task role ARN directly, not a data block

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,7 +3,7 @@
 
 output "unpacker_task_role_arns" {
   value = [
-    "${data.aws_iam_role.colbert_bag_unpacker_task_role.arn}",
-    "${data.aws_iam_role.stewart_bag_unpacker_task_role.arn}",
+    "${module.stack-colbert.unpacker_task_role_arn}",
+    "${module.stack-stewart.unpacker_task_role_arn}",
   ]
 }
