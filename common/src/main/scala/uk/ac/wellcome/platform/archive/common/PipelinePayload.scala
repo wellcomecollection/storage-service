810f2a52264e7b579b3b3888073e1355b8247da6
Alex Chan <a.chan@wellcome.ac.uk>
2019-05-01 10:41:46 +0100

    Add a new IngestRequestPayload

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/PipelinePayload.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/PipelinePayload.scala
@@ -1,6 +1,9 @@
 package uk.ac.wellcome.platform.archive.common
 
+import java.time.Instant
+
 import uk.ac.wellcome.platform.archive.common.bagit.models.ExternalIdentifier
+import uk.ac.wellcome.platform.archive.common.ingests.models.Ingest
 import uk.ac.wellcome.platform.archive.common.storage.models.StorageSpace
 import uk.ac.wellcome.storage.ObjectLocation
 
@@ -13,6 +16,23 @@ case class ObjectLocationPayload(
   storageSpace: StorageSpace,
   objectLocation: ObjectLocation
 ) extends PipelinePayload
+
+case class IngestRequestPayload(
+  ingestId: IngestID,
+  ingestDate: Instant,
+  storageSpace: StorageSpace,
+  sourceLocation: ObjectLocation
+) extends PipelinePayload
+
+case object IngestRequestPayload {
+  def apply(ingest: Ingest): IngestRequestPayload =
+    IngestRequestPayload(
+      ingestId = ingest.id,
+      ingestDate = ingest.createdDate,
+      storageSpace = StorageSpace(ingest.space.underlying),
+      sourceLocation = ingest.sourceLocation.location
+    )
+}
 
 case class BagInformationPayload(
   ingestId: IngestID,
---
+++ common/src/test/scala/uk/ac/wellcome/platform/archive/common/IngestRequestPayloadTest.scala
@@ -0,0 +1,37 @@
+package uk.ac.wellcome.platform.archive.common
+
+import java.time.Instant
+
+import org.scalatest.{FunSpec, Matchers}
+import uk.ac.wellcome.platform.archive.common.fixtures.RandomThings
+import uk.ac.wellcome.platform.archive.common.ingests.models.{Ingest, Namespace, StandardStorageProvider, StorageLocation}
+import uk.ac.wellcome.platform.archive.common.storage.models.StorageSpace
+import uk.ac.wellcome.storage.fixtures.S3
+
+class IngestRequestPayloadTest extends FunSpec with Matchers with RandomThings with S3 {
+  it("creates a payload from an ingest") {
+    val ingestId = createIngestID
+    val sourceLocation = createObjectLocation
+    val space = randomAlphanumeric()
+    val ingestDate = Instant.now()
+
+    val ingest = Ingest(
+      id = ingestId,
+      sourceLocation = StorageLocation(
+        provider = StandardStorageProvider,
+        location = sourceLocation
+      ),
+      space = Namespace(space),
+      createdDate = ingestDate
+    )
+
+    val expectedPayload = IngestRequestPayload(
+      ingestId = ingestId,
+      ingestDate = ingestDate,
+      storageSpace = StorageSpace(space),
+      sourceLocation = sourceLocation
+    )
+
+    IngestRequestPayload(ingest) shouldBe expectedPayload
+  }
+}
