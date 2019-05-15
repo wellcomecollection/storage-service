1c69fe46ff2c5146308e6e78982dbb4a94c6e5aa
Alex Chan <a.chan@wellcome.ac.uk>
2019-05-15 11:20:37 +0100

    Break the dao trait into a separate file

---
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/versioning/IngestVersionManagerDao.scala
@@ -0,0 +1,15 @@
+package uk.ac.wellcome.platform.archive.common.versioning
+
+import uk.ac.wellcome.platform.archive.common.IngestID
+import uk.ac.wellcome.platform.archive.common.bagit.models.ExternalIdentifier
+
+import scala.util.Try
+
+trait IngestVersionManagerDao {
+  def lookupExistingVersion(ingestID: IngestID): Try[Option[VersionRecord]]
+
+  def lookupLatestVersionFor(
+    externalIdentifier: ExternalIdentifier): Try[Option[VersionRecord]]
+
+  def storeNewVersion(record: VersionRecord): Try[Unit]
+}
