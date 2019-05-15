f02625d5f0fddfc6e42d4950db70827ff3efd7f1
Alex Chan <a.chan@wellcome.ac.uk>
2019-05-15 14:29:54 +0100

    Don't store StorageSpace/ExternalIdentifier with extra quotes

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/bagit/models/ExternalIdentifier.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/bagit/models/ExternalIdentifier.scala
@@ -1,28 +1,25 @@
 package uk.ac.wellcome.platform.archive.common.bagit.models
 
 import com.gu.scanamo.DynamoFormat
-import com.gu.scanamo.error.TypeCoercionError
 import io.circe.{Decoder, Encoder, Json}
-import uk.ac.wellcome.json.JsonUtil.{fromJson, toJson}
 
 case class ExternalIdentifier(underlying: String) extends AnyVal {
   override def toString: String = underlying
 }
 
 object ExternalIdentifier {
-  implicit val spaceEnc = Encoder.instance[ExternalIdentifier] {
+  implicit val encoder: Encoder[ExternalIdentifier] = Encoder.instance[ExternalIdentifier] {
     space: ExternalIdentifier =>
       Json.fromString(space.toString)
   }
 
-  implicit val spaceDec = Decoder.instance[ExternalIdentifier](cursor =>
+  implicit val decoder: Decoder[ExternalIdentifier] = Decoder.instance[ExternalIdentifier](cursor =>
     cursor.value.as[String].map(ExternalIdentifier(_)))
 
-  implicit def fmtSpace: DynamoFormat[ExternalIdentifier] =
-    DynamoFormat.xmap[ExternalIdentifier, String](
-      fromJson[ExternalIdentifier](_)(spaceDec).toEither.left
-        .map(TypeCoercionError)
+  implicit def evidence: DynamoFormat[ExternalIdentifier] =
+    DynamoFormat.coercedXmap[ExternalIdentifier, String, IllegalArgumentException](
+      ExternalIdentifier(_)
     )(
-      toJson[ExternalIdentifier](_).get
+      _.underlying
     )
 }
