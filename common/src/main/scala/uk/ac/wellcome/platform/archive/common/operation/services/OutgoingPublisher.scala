302902f8a5c7c708ac0e424d8c3341296a1c6ba6
Alex Chan <a.chan@wellcome.ac.uk>
2019-06-05 11:22:06 +0100

    Slightly improve the logging, perhaps?

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/operation/services/OutgoingPublisher.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/operation/services/OutgoingPublisher.scala
@@ -1,5 +1,6 @@
 package uk.ac.wellcome.platform.archive.common.operation.services
 
+import grizzled.slf4j.Logging
 import io.circe.Encoder
 import uk.ac.wellcome.messaging.MessageSender
 import uk.ac.wellcome.platform.archive.common.storage.models._
@@ -8,9 +9,10 @@ import scala.util.{Success, Try}
 
 class OutgoingPublisher[Destination](
   messageSender: MessageSender[Destination]
-) {
+) extends Logging {
   def sendIfSuccessful[R, O](result: IngestStepResult[R], outgoing: => O)(
     implicit enc: Encoder[O]): Try[Unit] = {
+    debug(s"Sending outgoing message for result $result")
     result match {
       case IngestStepSucceeded(_) | IngestCompleted(_) =>
         messageSender.sendT(outgoing)
