b00ac0ca8e8bad49202a76477fa9bbc503171e40
Alex Chan <a.chan@wellcome.ac.uk>
2019-06-20 15:27:30 +0100

    Always include the type parameter on a payload
    The verifier in particular is payload-agnostic, but it still needs
    to send an outgoing payload with a type parameter.

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/operation/services/OutgoingPublisher.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/operation/services/OutgoingPublisher.scala
@@ -1,8 +1,9 @@
 package uk.ac.wellcome.platform.archive.common.operation.services
 
 import grizzled.slf4j.Logging
-import io.circe.Encoder
+import uk.ac.wellcome.json.JsonUtil._
 import uk.ac.wellcome.messaging.MessageSender
+import uk.ac.wellcome.platform.archive.common.PipelinePayload
 import uk.ac.wellcome.platform.archive.common.storage.models._
 
 import scala.util.{Success, Try}
@@ -10,14 +11,13 @@ import scala.util.{Success, Try}
 class OutgoingPublisher[Destination](
   messageSender: MessageSender[Destination]
 ) extends Logging {
-  def sendIfSuccessful[R, O](result: IngestStepResult[R], outgoing: => O)(
-    implicit enc: Encoder[O]): Try[Unit] = {
+  def sendIfSuccessful[R, O <: PipelinePayload](result: IngestStepResult[R], outgoing: => O): Try[Unit] = {
     debug(s"Sending outgoing message for result $result")
     result match {
       case IngestStepSucceeded(_) | IngestCompleted(_) =>
         debug(
           s"Ingest success/completed: sending an outgoing message $outgoing")
-        messageSender.sendT(outgoing)
+        messageSender.sendT[PipelinePayload](outgoing)
       case IngestFailed(_, _, _) =>
         debug(s"Ingest failed: not sending a message")
         Success(())
