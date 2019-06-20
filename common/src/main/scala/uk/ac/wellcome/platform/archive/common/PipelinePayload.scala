768a939e4e59d853c329faeff53b95f5d94c4046
Alex Chan <a.chan@wellcome.ac.uk>
2019-06-20 11:38:51 +0100

    Add a case class for PipelineContext

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/PipelinePayload.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/PipelinePayload.scala
@@ -3,12 +3,38 @@ package uk.ac.wellcome.platform.archive.common
 import java.time.Instant
 
 import uk.ac.wellcome.platform.archive.common.bagit.models.ExternalIdentifier
-import uk.ac.wellcome.platform.archive.common.ingests.models.{Ingest, IngestID}
+import uk.ac.wellcome.platform.archive.common.ingests.models.{Ingest, IngestID, IngestType}
 import uk.ac.wellcome.platform.archive.common.storage.models.StorageSpace
 import uk.ac.wellcome.storage.ObjectLocation
 
 sealed trait PipelinePayload {
-  val ingestId: IngestID
+  def ingestId: IngestID
+}
+
+case class PipelineContext(
+  ingestId: IngestID,
+  ingestType: IngestType,
+  storageSpace: StorageSpace,
+  ingestDate: Instant
+)
+
+case object PipelineContext {
+  def apply(ingest: Ingest): PipelineContext =
+    PipelineContext(
+      ingestId = ingest.id,
+      ingestType = ingest.ingestType,
+      storageSpace = StorageSpace(ingest.space.underlying),
+      ingestDate = ingest.createdDate
+    )
+}
+
+sealed trait BetterPipelinePayload extends PipelinePayload {
+  val context: PipelineContext
+
+  def ingestId: IngestID = context.ingestId
+  def ingestType: IngestType = context.ingestType
+  def storageSpace: StorageSpace = context.storageSpace
+  def ingestDate: Instant = context.ingestDate
 }
 
 case class SourceLocationPayload(
