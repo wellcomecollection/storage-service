016e597a0ce2b974aeb643b3a66ecce40b5db9e8
Alex Chan <alex@alexwlchan.net>
2019-06-27 13:40:57 +0100

    Get the bag verifier compiling

--- bag_verifier/src/test/scala/uk/ac/wellcome/platform/archive/bagverifier/fixtures/VerifyFixtures.scala
+++ bag_verifier/src/test/scala/uk/ac/wellcome/platform/archive/bagverifier/fixtures/VerifyFixtures.scala
@@ -1,15 +1,12 @@
 package uk.ac.wellcome.platform.archive.bagverifier.fixtures
 
 import uk.ac.wellcome.platform.archive.common.fixtures.StorageRandomThings
-import uk.ac.wellcome.platform.archive.common.storage.services.{
-  S3ObjectVerifier,
-  S3Resolvable
-}
+import uk.ac.wellcome.platform.archive.common.storage.services.{S3ObjectVerifier, S3Resolvable}
 import uk.ac.wellcome.platform.archive.common.verify._
 import uk.ac.wellcome.storage.ObjectLocation
-import uk.ac.wellcome.storage.fixtures.S3
+import uk.ac.wellcome.storage.fixtures.S3Fixtures
 
-trait VerifyFixtures extends S3 with StorageRandomThings {
+trait VerifyFixtures extends S3Fixtures with StorageRandomThings {
 
   implicit val objectVerifier = new S3ObjectVerifier()
 
