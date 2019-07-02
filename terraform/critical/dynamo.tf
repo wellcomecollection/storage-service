039b13cafd28f51270371cf908ad3ff91ad8d952
Robert Kenny <r.kenny@wellcome.ac.uk>
2019-07-02 14:23:48 +0100

    Add DynamoDB for bag_id_lookup table

--- terraform/critical/dynamo.tf
+++ terraform/critical/dynamo.tf
@@ -1,8 +1,27 @@
-locals {
-  gsi_name = "${var.namespace}-bag-id-index"
+resource "aws_dynamodb_table" "ingests" {
+  name           = "${var.namespace}-ingests"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "id"
+
+  billing_mode = "${var.billing_mode}"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+
+    ignore_changes = [
+      "read_capacity",
+      "write_capacity",
+    ]
+  }
 }
 
-resource "aws_dynamodb_table" "ingests" {
+resource "aws_dynamodb_table" "bag_id_lookup" {
   name           = "${var.namespace}-ingests"
   read_capacity  = 1
   write_capacity = 1
@@ -23,13 +42,6 @@ resource "aws_dynamodb_table" "ingests" {
   attribute {
     name = "space"
     type = "S"
-  }
-
-  global_secondary_index {
-    name            = "${local.gsi_name}"
-    hash_key        = "externalIdentifier"
-    range_key       = "space"
-    projection_type = "ALL"
   }
 
   lifecycle {
