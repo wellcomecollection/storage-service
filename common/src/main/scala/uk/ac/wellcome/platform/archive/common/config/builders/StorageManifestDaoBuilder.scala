dcbf27793ece25c061a7c07b4d16f615ffe5fa7e
Alex Chan <a.chan@wellcome.ac.uk>
2019-07-02 16:42:23 +0100

    Add a string DynamoFormat for BagId

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/config/builders/StorageManifestDaoBuilder.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/config/builders/StorageManifestDaoBuilder.scala
@@ -4,20 +4,19 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import com.amazonaws.services.s3.AmazonS3
 import com.typesafe.config.Config
 import org.scanamo.auto._
+import org.scanamo.error.{DynamoReadError, TypeCoercionError}
+import org.scanamo.{DynamoFormat, DynamoValue}
 import uk.ac.wellcome.json.JsonUtil._
-import uk.ac.wellcome.storage.streaming.Codec._
 import uk.ac.wellcome.platform.archive.common.bagit.models.BagId
 import uk.ac.wellcome.platform.archive.common.storage.models.StorageManifest
 import uk.ac.wellcome.platform.archive.common.storage.services.StorageManifestDao
-import uk.ac.wellcome.storage.{ObjectLocation, ObjectLocationPrefix, Version}
+import uk.ac.wellcome.platform.archive.common.versioning.dynamo.DynamoID
 import uk.ac.wellcome.storage.store.HybridIndexedStoreEntry
-import uk.ac.wellcome.storage.store.dynamo.{
-  DynamoHashRangeStore,
-  DynamoHybridStoreWithMaxima,
-  DynamoVersionedHybridStore
-}
+import uk.ac.wellcome.storage.store.dynamo.{DynamoHashRangeStore, DynamoHybridStoreWithMaxima, DynamoVersionedHybridStore}
 import uk.ac.wellcome.storage.store.s3.{S3StreamStore, S3TypedStore}
+import uk.ac.wellcome.storage.streaming.Codec._
 import uk.ac.wellcome.storage.typesafe.{DynamoBuilder, S3Builder}
+import uk.ac.wellcome.storage.{ObjectLocation, ObjectLocationPrefix, Version}
 
 object StorageManifestDaoBuilder {
   def build(config: Config): StorageManifestDao = {
@@ -32,6 +31,31 @@ object StorageManifestDaoBuilder {
     implicit val streamStore: S3StreamStore = new S3StreamStore()
     implicit val typedStore: S3TypedStore[StorageManifest] =
       new S3TypedStore[StorageManifest]()
+
+    // TODO: Add a test for this
+    implicit val dynamoFormat: DynamoFormat[BagId] = new DynamoFormat[BagId] {
+      override def read(av: DynamoValue): Either[DynamoReadError, BagId] = {
+        val result = for {
+          stringId <- av.as[String]
+          space <- DynamoID.getStorageSpace(stringId).toEither
+          externalIdentifier <- DynamoID.getExternalIdentifier(stringId).toEither
+          bagId = BagId(space, externalIdentifier)
+        } yield bagId
+
+        result match {
+          case Left(err) =>
+            Left(TypeCoercionError(
+              new Throwable(s"Error decoding BagId from DynamoDB: $err")
+            ))
+          case Right(bagId) => Right(bagId)
+        }
+      }
+
+      override def write(bagId: BagId): DynamoValue =
+        DynamoValue.fromString(
+          DynamoID.createId(bagId.space, bagId.externalIdentifier)
+        )
+    }
 
     implicit val indexedStore
       : DynamoHashRangeStore[BagId,
