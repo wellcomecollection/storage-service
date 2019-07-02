06570fe4fbc3821028efd07eb41423baa978da23
Alex Chan <a.chan@wellcome.ac.uk>
2019-07-02 17:30:14 +0100

    Is this what we need for the StorageManifestDao?

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/storage/services/StorageManifestDao.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/storage/services/StorageManifestDao.scala
@@ -8,15 +8,13 @@ import uk.ac.wellcome.storage.{ReadError, Version, WriteError}
 // TODO: Do we need this wrapper at all now?
 // TODO: This could be a Store!
 class StorageManifestDao(
-  vhs: VersionedStore[BagId,
-                      Int,
-                      HybridStoreEntry[StorageManifest, Map[String, String]]]
+  vhs: VersionedStore[BagId, Int, HybridStoreEntry[StorageManifest, Map[String, String]]
 ) {
   def getLatest(id: BagId): Either[ReadError, StorageManifest] =
-    vhs.getLatest(id).map { _.identifiedT.t }
+    vhs.getLatest(id).map { _.identifiedT }
 
   def get(id: BagId, version: Int): Either[ReadError, StorageManifest] =
-    vhs.get(Version(id, version)).map { _.identifiedT.t }
+    vhs.get(Version(id, version)).map { _.identifiedT }
 
   def put(
     storageManifest: StorageManifest): Either[WriteError, StorageManifest] =
@@ -24,5 +22,5 @@ class StorageManifestDao(
       .put(id = Version(storageManifest.id, storageManifest.version))(
         HybridStoreEntry(storageManifest, metadata = Map.empty)
       )
-      .map { _.identifiedT.t }
+      .map { _.identifiedT }
 }
