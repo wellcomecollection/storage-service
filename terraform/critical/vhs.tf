3dcc73f2fc070bca753173fab3decee006ed74ff
Alex Chan <alex@alexwlchan.net>
2019-07-03 07:53:48 +0100

    Bump the VHS module to add the range key

--- terraform/critical/vhs.tf
+++ terraform/critical/vhs.tf
@@ -1,4 +1,4 @@
 module "vhs_manifests" {
-  source = "git::https://github.com/wellcometrust/terraform-modules.git//vhs/modules/vhs?ref=v19.2.0"
+  source = "git::https://github.com/wellcometrust/terraform-modules.git//vhs/modules/vhs?ref=4605fbb"
   name   = "${var.namespace}-manifests"
 }
