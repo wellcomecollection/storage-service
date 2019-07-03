473f8e1c1b87787daee2e4e3c77561d1cdb11b5b
Alex Chan <a.chan@wellcome.ac.uk>
2019-07-03 14:49:57 +0100

    Write a fetch file as part of bags in tests

--- common/src/test/scala/uk/ac/wellcome/platform/archive/common/fixtures/BagLocationFixtures.scala
+++ common/src/test/scala/uk/ac/wellcome/platform/archive/common/fixtures/BagLocationFixtures.scala
@@ -1,15 +1,16 @@
 package uk.ac.wellcome.platform.archive.common.fixtures
 
+import java.net.URI
+
 import uk.ac.wellcome.fixtures.TestWith
-import uk.ac.wellcome.platform.archive.common.bagit.models.BagInfo
-import uk.ac.wellcome.platform.archive.common.generators.{
-  BagInfoGenerators,
-  StorageSpaceGenerators
-}
+import uk.ac.wellcome.platform.archive.common.bagit.models.{BagFetch, BagFetchEntry, BagInfo, BagPath}
+import uk.ac.wellcome.platform.archive.common.generators.{BagInfoGenerators, StorageSpaceGenerators}
 import uk.ac.wellcome.platform.archive.common.storage.models.StorageSpace
 import uk.ac.wellcome.storage.ObjectLocation
 import uk.ac.wellcome.storage.fixtures.S3Fixtures
 import uk.ac.wellcome.storage.fixtures.S3Fixtures.Bucket
+
+import scala.util.Random
 
 trait BagLocationFixtures
     extends S3Fixtures
@@ -53,7 +54,17 @@ trait BagLocationFixtures
       bagRootDirectory.getOrElse("")
     )
 
-    fileEntries.map((entry: FileEntry) => {
+    // To simulate a bag that contains both concrete objects and
+    // fetch files, siphon off some of the entries to be written
+    // as a fetch file.
+    //
+    // We need to make sure bag-info.txt, manifest.txt, and so on,
+    // are written into the bag proper.
+    val (realFiles, fetchFiles) = fileEntries.partition { file =>
+      file.name.endsWith(".txt") || Random.nextFloat() < 0.8
+    }
+
+    realFiles.map { entry =>
       val entryLocation = unpackedBagLocation.join(entry.name)
       s3Client
         .putObject(
@@ -61,7 +72,34 @@ trait BagLocationFixtures
           entryLocation.path,
           entry.contents
         )
-    })
+    }
+
+    val bagFetchEntries = fetchFiles.map { entry =>
+      val entryLocation = createObjectLocationWith(bucket)
+      s3Client
+        .putObject(
+          entryLocation.namespace,
+          entryLocation.path,
+          entry.contents
+        )
+
+      BagFetchEntry(
+        uri = new URI(s"s3://${bucket.name}/${entryLocation.path}"),
+        length = Some(entry.contents.length),
+        path = BagPath(entry.name)
+      )
+    }
+
+    if (fetchFiles.nonEmpty) {
+      val fetchLocation = unpackedBagLocation.join("fetch.txt")
+
+      s3Client
+        .putObject(
+          fetchLocation.namespace,
+          fetchLocation.path,
+          BagFetch.write(bagFetchEntries)
+        )
+    }
 
     testWith((bagRootLocation, storageSpace))
   }
