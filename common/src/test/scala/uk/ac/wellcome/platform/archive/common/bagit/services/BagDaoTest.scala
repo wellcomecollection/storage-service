c4b6efed9df3127eb0d2b97209dbe76c4d165f14
Alex Chan <a.chan@wellcome.ac.uk>
2019-07-04 14:55:18 +0100

    Add some more tests for error cases

--- common/src/test/scala/uk/ac/wellcome/platform/archive/common/bagit/services/BagDaoTest.scala
+++ common/src/test/scala/uk/ac/wellcome/platform/archive/common/bagit/services/BagDaoTest.scala
@@ -30,12 +30,40 @@ class BagDaoTest extends FunSpec with Matchers with S3Fixtures with BagLocationF
     }
   }
 
+  it("errors if the bag-info.txt file is malformed") {
+    withLocalS3Bucket { bucket =>
+      withBag(bucket) { case (rootLocation, _) =>
+        s3Client.putObject(
+          rootLocation.namespace,
+          rootLocation.join("bag-info.txt").path,
+          randomAlphanumeric
+        )
+
+        bagDao.get(rootLocation).left.value.msg should startWith("Error loading bag-info.txt")
+      }
+    }
+  }
+
   it("errors if the file manifest does not exist") {
     withLocalS3Bucket { bucket =>
       withBag(bucket) { case (rootLocation, _) =>
         s3Client.deleteObject(
           rootLocation.namespace,
           rootLocation.join("manifest-sha256.txt").path
+        )
+
+        bagDao.get(rootLocation).left.value.msg should startWith("Error loading manifest-sha256.txt")
+      }
+    }
+  }
+
+  it("errors if the file manifest is malformed") {
+    withLocalS3Bucket { bucket =>
+      withBag(bucket) { case (rootLocation, _) =>
+        s3Client.putObject(
+          rootLocation.namespace,
+          rootLocation.join("manifest-sha256.txt").path,
+          randomAlphanumeric
         )
 
         bagDao.get(rootLocation).left.value.msg should startWith("Error loading manifest-sha256.txt")
@@ -56,6 +84,20 @@ class BagDaoTest extends FunSpec with Matchers with S3Fixtures with BagLocationF
     }
   }
 
+  it("errors if the tag manifest is malformed") {
+    withLocalS3Bucket { bucket =>
+      withBag(bucket) { case (rootLocation, _) =>
+        s3Client.putObject(
+          rootLocation.namespace,
+          rootLocation.join("tagmanifest-sha256.txt").path,
+          randomAlphanumeric
+        )
+
+        bagDao.get(rootLocation).left.value.msg should startWith("Error loading tagmanifest-sha256.txt")
+      }
+    }
+  }
+
   it("passes if the fetch.txt does not exist") {
     val bagInfo = createBagInfo
 
@@ -67,6 +109,20 @@ class BagDaoTest extends FunSpec with Matchers with S3Fixtures with BagLocationF
         )
 
         bagDao.get(rootLocation).right.value.fetch shouldBe None
+      }
+    }
+  }
+
+  it("errors if the fetch file is malformed") {
+    withLocalS3Bucket { bucket =>
+      withBag(bucket) { case (rootLocation, _) =>
+        s3Client.putObject(
+          rootLocation.namespace,
+          rootLocation.join("fetch.txt").path,
+          randomAlphanumeric
+        )
+
+        bagDao.get(rootLocation).left.value.msg should startWith("Error loading fetch.txt")
       }
     }
   }
