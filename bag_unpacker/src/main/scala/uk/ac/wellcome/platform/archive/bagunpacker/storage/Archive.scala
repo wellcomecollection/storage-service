04267b5d5057d825b8c5956eddb158df139a7b76
Alex Chan <a.chan@wellcome.ac.uk>
2019-07-05 01:36:31 +0100

    Add a note about views into streams

--- bag_unpacker/src/main/scala/uk/ac/wellcome/platform/archive/bagunpacker/storage/Archive.scala
+++ bag_unpacker/src/main/scala/uk/ac/wellcome/platform/archive/bagunpacker/storage/Archive.scala
@@ -7,6 +7,29 @@ import org.apache.commons.compress.compressors.{CompressorInputStream, Compresso
 
 import scala.util.Try
 
+/** You pass this class an inputStream that comes from the storage provider,
+  * e.g. a FileInputStream or an S3InputStream.  The stream should be a tar.gz
+  * or similar container + compression format.
+  *
+  * The iterator it produces has two pieces:
+  *  1. ArchiveEntry -- metadata about the original file, e.g. name/size
+  *  2. InputStream -- a stream that gives you the content for this file
+  *
+  * This InputStream is really just a view into the original stream.
+  * If you advance one, the other advances.  They move together.
+  *
+  *       +------------------------------------------+ original stream
+  *
+  *       +-------+                                    file1
+  *               +------------------+                 file2
+  *                                  +---------+       file3
+  *                                            +-----+ file4
+  *
+  * This means you need to make sure the individual input streams never
+  * get closed -- or it'll close the underlying stream, and you'll be
+  * unable to get any more entries.
+  *
+  */
 object Archive {
   def unpack(inputStream: InputStream): Try[Iterator[(ArchiveEntry, InputStream)]] =
     for {
