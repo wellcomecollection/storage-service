b5d3d88c28ec7507904129b6b5e61a4eb1c1aed7
Alex Chan <a.chan@wellcome.ac.uk>
2019-07-05 01:22:00 +0100

    And now we have a MemoryUnpacker

---
+++ bag_unpacker/src/main/scala/uk/ac/wellcome/platform/archive/bagunpacker/services/memory/MemoryUnpacker.scala
@@ -0,0 +1,22 @@
+package uk.ac.wellcome.platform.archive.bagunpacker.services.memory
+
+import java.io.InputStream
+
+import uk.ac.wellcome.platform.archive.bagunpacker.services.Unpacker
+import uk.ac.wellcome.storage.{ObjectLocation, StorageError}
+import uk.ac.wellcome.storage.store.memory.MemoryStreamStore
+import uk.ac.wellcome.storage.streaming.{InputStreamWithLength, InputStreamWithLengthAndMetadata}
+
+class MemoryUnpacker()(implicit streamStore: MemoryStreamStore[ObjectLocation]) extends Unpacker {
+  override def get(location: ObjectLocation): Either[StorageError, InputStream] =
+    streamStore.get(location).map { _.identifiedT }
+
+  override def put(location: ObjectLocation)(inputStream: InputStreamWithLength): Either[StorageError, Unit] =
+    streamStore.put(location)(
+      new InputStreamWithLengthAndMetadata(
+        inputStream,
+        length = inputStream.length,
+        metadata = Map.empty
+      )
+    ).map { _ => () }
+}
