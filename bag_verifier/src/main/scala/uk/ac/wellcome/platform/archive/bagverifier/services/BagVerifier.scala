3402d116bd19c044aaaae04b2ca6212408a8ab4c
Robert Kenny <r.kenny@wellcome.ac.uk>
2019-07-07 08:12:38 -0400

    verifier syntax fix

--- bag_verifier/src/main/scala/uk/ac/wellcome/platform/archive/bagverifier/services/BagVerifier.scala
+++ bag_verifier/src/main/scala/uk/ac/wellcome/platform/archive/bagverifier/services/BagVerifier.scala
@@ -5,10 +5,7 @@ import java.time.Instant
 import grizzled.slf4j.Logging
 import uk.ac.wellcome.platform.archive.bagverifier.models._
 import uk.ac.wellcome.platform.archive.common.bagit.models._
-import uk.ac.wellcome.platform.archive.common.bagit.services.{
-  BagReader,
-  BagVerifiable
-}
+import uk.ac.wellcome.platform.archive.common.bagit.services.{BagReader, BagVerifiable}
 import uk.ac.wellcome.platform.archive.common.storage.Resolvable
 import uk.ac.wellcome.platform.archive.common.storage.models._
 import uk.ac.wellcome.platform.archive.common.verify.Verification._
@@ -29,21 +26,21 @@ class BagVerifier()(
       implicit val bagVerifiable = new BagVerifiable(root)
       val startTime = Instant.now()
 
-    bagReader.get(root) match {
-      case Left(e) =>
-        IngestFailed(VerificationSummary.incomplete(root, e, startTime), e)
-      case Right(bag) =>
-        VerificationSummary.create(root, bag.verify, startTime) match {
-          case success @ VerificationSuccessSummary(_, _, _, _) =>
-            IngestStepSucceeded(success)
-          case failure @ VerificationFailureSummary(_, _, _, _) =>
-            IngestFailed(failure, InvalidBag(bag))
-          case incomplete @ VerificationIncompleteSummary(_, _, _, _) =>
-            IngestFailed(incomplete, incomplete.e)
-        }
-
+      bagReader.get(root) match {
+        case Left(e) =>
+          IngestFailed(VerificationSummary.incomplete(root, e, startTime), e)
+        case Right(bag) =>
+          VerificationSummary.create(root, bag.verify, startTime) match {
+            case success@VerificationSuccessSummary(_, _, _, _) =>
+              IngestStepSucceeded(success)
+            case failure@VerificationFailureSummary(_, _, _, _) =>
+              IngestFailed(failure, InvalidBag(bag))
+            case incomplete@VerificationIncompleteSummary(_, _, _, _) =>
+              IngestFailed(incomplete, incomplete.e)
+          }
+      }
     }
 }
 
 case class InvalidBag(bag: Bag)
-    extends Throwable(s"Invalid bag: ${bag.info.externalIdentifier}")
+  extends Throwable(s"Invalid bag: ${bag.info.externalIdentifier}")
