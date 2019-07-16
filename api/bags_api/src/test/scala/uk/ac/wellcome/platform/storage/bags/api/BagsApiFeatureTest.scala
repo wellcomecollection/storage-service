840479d1006ebb90f258f4b4fe837a4ef5ec68dd
Alex Chan <alex@alexwlchan.net>
2019-07-16 20:38:25 +0100

    Add a test for looking up multiple versions of a manifest

--- api/bags_api/src/test/scala/uk/ac/wellcome/platform/storage/bags/api/BagsApiFeatureTest.scala
+++ api/bags_api/src/test/scala/uk/ac/wellcome/platform/storage/bags/api/BagsApiFeatureTest.scala
@@ -324,7 +324,76 @@ class BagsApiFeatureTest
     }
 
     it("finds multiple versions of a manifest") {
-      true shouldBe false
+      val storageManifest = createStorageManifest
+
+      val multipleManifests = (1 to 5)
+        .map { version =>
+          version -> storageManifest.copy(
+            createdDate = randomInstant,
+            version = version
+          )
+        }
+        .toMap
+
+      val initialManifests = multipleManifests.values.toSeq.sortBy { _.version }
+
+      withConfiguredApp(initialManifests) { case (_, metrics, baseUrl) =>
+        val expectedJson =
+          s"""
+             |{
+             |  "@context": "http://api.wellcomecollection.org/storage/v1/context.json",
+             |  "type": "ResultList",
+             |  "results": [
+             |    {
+             |      "type": "Bag",
+             |      "id": "${storageManifest.id.toString}",
+             |      "version": "v5",
+             |      "createdDate": "${DateTimeFormatter.ISO_INSTANT.format(multipleManifests(5).createdDate)}"
+             |    },
+             |    {
+             |      "type": "Bag",
+             |      "id": "${ storageManifest.id.toString }",
+             |      "version": "v4",
+             |      "createdDate": "${ DateTimeFormatter.ISO_INSTANT.format(multipleManifests(4).createdDate) }"
+             |    },
+             |    {
+             |      "type": "Bag",
+             |      "id": "${ storageManifest.id.toString }",
+             |      "version": "v3",
+             |      "createdDate": "${ DateTimeFormatter.ISO_INSTANT.format(multipleManifests(3).createdDate) }"
+             |    },
+             |    {
+             |      "type": "Bag",
+             |      "id": "${ storageManifest.id.toString }",
+             |      "version": "v2",
+             |      "createdDate": "${ DateTimeFormatter.ISO_INSTANT.format(multipleManifests(2).createdDate) }"
+             |    },
+             |    {
+             |      "type": "Bag",
+             |      "id": "${ storageManifest.id.toString }",
+             |      "version": "v1",
+             |      "createdDate": "${ DateTimeFormatter.ISO_INSTANT.format(multipleManifests(1).createdDate) }"
+             |    }
+             |  ]
+             |}
+              """.stripMargin
+
+        val url =
+          s"$baseUrl/bags/${storageManifest.id.space}/${storageManifest.id.externalIdentifier}/versions"
+
+        whenGetRequestReady(url) { response =>
+          response.status shouldBe StatusCodes.OK
+
+          withStringEntity(response.entity) { actualJson =>
+            assertJsonStringsAreEqual(actualJson, expectedJson)
+          }
+
+          assertMetricSent(
+            metrics,
+            result = HttpMetricResults.Success
+          )
+        }
+      }
     }
 
     it("supports searching for manifests before a given version") {
