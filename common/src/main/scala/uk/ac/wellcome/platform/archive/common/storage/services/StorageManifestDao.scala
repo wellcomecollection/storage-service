2311039581b9957bf47e0bc610d174516f730709
Alex Chan <alex@alexwlchan.net>
2019-07-16 09:54:00 +0100

    Add a new StorageManifestDao trait

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/storage/services/StorageManifestDao.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/storage/services/StorageManifestDao.scala
@@ -5,6 +5,23 @@ import uk.ac.wellcome.platform.archive.common.storage.models.StorageManifest
 import uk.ac.wellcome.storage.store.{HybridStoreEntry, VersionedStore}
 import uk.ac.wellcome.storage.{ReadError, Version, WriteError}
 
+trait BetterStorageManifestDao {
+  val vhs: VersionedStore[String, Int, HybridStoreEntry[StorageManifest, _]]
+  def getLatest(id: BagId): Either[ReadError, StorageManifest] =
+    vhs.getLatest(id.toString).map { _.identifiedT.t }
+
+  def get(id: BagId, version: Int): Either[ReadError, StorageManifest] =
+    vhs.get(Version(id.toString, version)).map { _.identifiedT.t }
+
+  def put(
+    storageManifest: StorageManifest): Either[WriteError, StorageManifest] =
+    vhs
+      .put(id = Version(storageManifest.id.toString, storageManifest.version))(
+        HybridStoreEntry(storageManifest, metadata = Map("alex" -> "true"))
+      )
+      .map { _.identifiedT.t }
+}
+
 // TODO: Do we need this wrapper at all now?
 // TODO: This could be a Store!
 class StorageManifestDao(
