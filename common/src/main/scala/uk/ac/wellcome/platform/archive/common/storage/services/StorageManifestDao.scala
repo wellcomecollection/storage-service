3c1aaf4ec9ea4d7b4fb0fcdd3129138b67fcacf7
Alex Chan <alex@alexwlchan.net>
2019-07-16 10:08:57 +0100

    Add in-memory and DynamoDB implementations

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/storage/services/StorageManifestDao.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/storage/services/StorageManifestDao.scala
@@ -1,25 +1,81 @@
 package uk.ac.wellcome.platform.archive.common.storage.services
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
+import com.amazonaws.services.s3.AmazonS3
+import org.scanamo.auto._
+import org.scanamo.{DynamoFormat, DynamoValue}
+import org.scanamo.error.DynamoReadError
+import uk.ac.wellcome.json.JsonUtil._
 import uk.ac.wellcome.platform.archive.common.bagit.models.BagId
 import uk.ac.wellcome.platform.archive.common.storage.models.StorageManifest
-import uk.ac.wellcome.storage.store.{HybridStoreEntry, VersionedStore}
-import uk.ac.wellcome.storage.{ReadError, Version, WriteError}
+import uk.ac.wellcome.storage._
+import uk.ac.wellcome.storage.dynamo.DynamoConfig
+import uk.ac.wellcome.storage.s3.S3Config
+import uk.ac.wellcome.storage.store.dynamo.{DynamoHashRangeStore, DynamoHybridStoreWithMaxima, DynamoVersionedHybridStore}
+import uk.ac.wellcome.storage.store.memory.MemoryVersionedStore
+import uk.ac.wellcome.storage.store.s3.{S3StreamStore, S3TypedStore}
+import uk.ac.wellcome.storage.store.{HybridIndexedStoreEntry, HybridStoreEntry, VersionedStore}
+import uk.ac.wellcome.storage.streaming.Codec._
+
+case class EmptyMetadata()
 
 trait BetterStorageManifestDao {
-  val vhs: VersionedStore[String, Int, HybridStoreEntry[StorageManifest, _]]
+  val vhs: VersionedStore[BagId, Int, HybridStoreEntry[StorageManifest, EmptyMetadata]]
   def getLatest(id: BagId): Either[ReadError, StorageManifest] =
-    vhs.getLatest(id.toString).map { _.identifiedT.t }
+    vhs.getLatest(id).map { _.identifiedT.t }
 
   def get(id: BagId, version: Int): Either[ReadError, StorageManifest] =
-    vhs.get(Version(id.toString, version)).map { _.identifiedT.t }
+    vhs.get(Version(id, version)).map { _.identifiedT.t }
 
-  def put(
-    storageManifest: StorageManifest): Either[WriteError, StorageManifest] =
+  def put(storageManifest: StorageManifest): Either[WriteError, StorageManifest] =
     vhs
-      .put(id = Version(storageManifest.id.toString, storageManifest.version))(
-        HybridStoreEntry(storageManifest, metadata = Map("alex" -> "true"))
+      .put(id = Version(storageManifest.id, storageManifest.version))(
+        HybridStoreEntry(storageManifest, metadata = EmptyMetadata())
       )
       .map { _.identifiedT.t }
+}
+
+class MemoryStorageManifestDao(
+  val vhs: MemoryVersionedStore[BagId, HybridStoreEntry[StorageManifest, EmptyMetadata]]
+) extends BetterStorageManifestDao
+
+class DynamoStorageManifestDao(
+  dynamoConfig: DynamoConfig,
+  s3Config: S3Config
+)(
+  implicit
+  dynamoClient: AmazonDynamoDB,
+  s3Client: AmazonS3
+) extends BetterStorageManifestDao {
+  type DynamoStoreEntry =
+    HybridIndexedStoreEntry[ObjectLocation, EmptyMetadata]
+
+  implicit val evidence: DynamoFormat[EmptyMetadata] = new DynamoFormat[EmptyMetadata] {
+    override def read(av: DynamoValue): scala.Either[DynamoReadError, EmptyMetadata] =
+      Right(EmptyMetadata())
+
+    override def write(t: EmptyMetadata): DynamoValue =
+      DynamoValue.nil
+  }
+
+  implicit val indexedStore: DynamoHashRangeStore[BagId, Int, DynamoStoreEntry] =
+    new DynamoHashRangeStore[BagId, Int, DynamoStoreEntry](dynamoConfig)
+
+  implicit val streamStore: S3StreamStore = new S3StreamStore()
+  implicit val typedStore: S3TypedStore[StorageManifest] =
+    new S3TypedStore[StorageManifest]()
+
+  override val vhs: VersionedStore[BagId, Int, HybridStoreEntry[StorageManifest, EmptyMetadata]] =
+    new DynamoVersionedHybridStore[
+      BagId, Int, StorageManifest, EmptyMetadata](
+      store = new DynamoHybridStoreWithMaxima[
+        BagId, Int, StorageManifest, EmptyMetadata](
+        prefix = ObjectLocationPrefix(
+          namespace = s3Config.bucketName,
+          path = ""
+        )
+      )
+    )
 }
 
 // TODO: Do we need this wrapper at all now?
--- common/src/test/scala/uk/ac/wellcome/platform/archive/common/storage/services/StorageManifestDaoTest.scala
+++ common/src/test/scala/uk/ac/wellcome/platform/archive/common/storage/services/StorageManifestDaoTest.scala
@@ -1,12 +1,121 @@
 package uk.ac.wellcome.platform.archive.common.storage.services
 
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType
 import org.scalatest.{EitherValues, FunSpec, Matchers}
+import uk.ac.wellcome.fixtures.TestWith
+import uk.ac.wellcome.platform.archive.common.bagit.models.BagId
 import uk.ac.wellcome.platform.archive.common.fixtures.StorageManifestVHSFixture
 import uk.ac.wellcome.platform.archive.common.generators.StorageManifestGenerators
-import uk.ac.wellcome.storage.{
-  NoVersionExistsError,
-  VersionAlreadyExistsError,
-  WriteError
+import uk.ac.wellcome.platform.archive.common.storage.models.StorageManifest
+import uk.ac.wellcome.storage.fixtures.{DynamoFixtures, S3Fixtures}
+import uk.ac.wellcome.storage.fixtures.DynamoFixtures.Table
+import uk.ac.wellcome.storage.fixtures.S3Fixtures.Bucket
+import uk.ac.wellcome.storage.store.HybridStoreEntry
+import uk.ac.wellcome.storage.store.memory.MemoryVersionedStore
+import uk.ac.wellcome.storage.{NoVersionExistsError, VersionAlreadyExistsError, WriteError}
+
+trait BetterStorageManifestDaoTestCases[Context]
+  extends FunSpec
+    with Matchers
+    with EitherValues
+    with StorageManifestGenerators {
+  def withContext[R](testWith: TestWith[Context, R]): R
+
+  def withDao[R](testWith: TestWith[BetterStorageManifestDao, R])(implicit context: Context): R
+
+  it("allows storing and retrieving a record") {
+    val storageManifest = createStorageManifest
+
+    val newStorageManifest = createStorageManifestWith(
+      space = storageManifest.space,
+      bagInfo = storageManifest.info,
+      version = storageManifest.version
+    )
+
+    storageManifest.id shouldBe newStorageManifest.id
+
+    withContext { implicit context =>
+      withDao { dao =>
+        // Empty get
+
+        val getResultPreInsert = dao.getLatest(storageManifest.id)
+        getResultPreInsert.left.value shouldBe a[NoVersionExistsError]
+
+        // Insert
+
+        val insertResult = dao.put(storageManifest)
+        insertResult shouldBe a[Right[_, _]]
+
+        val getResultPostInsert = dao.getLatest(storageManifest.id)
+        getResultPostInsert.right.value shouldBe storageManifest
+
+        // Update
+
+        val updateResult = dao.put(newStorageManifest)
+        updateResult.left.value shouldBe a[WriteError]
+      }
+    }
+  }
+
+  it("blocks putting two manifests with the same version") {
+    val storageManifest = createStorageManifest
+
+    withContext { implicit context =>
+      withDao { dao =>
+        dao.put(storageManifest).right.value shouldBe storageManifest
+        dao.put(storageManifest).left.value shouldBe a[VersionAlreadyExistsError]
+      }
+    }
+  }
+}
+
+class MemoryStorageManifestDaoTest
+  extends BetterStorageManifestDaoTestCases[MemoryVersionedStore[BagId, HybridStoreEntry[StorageManifest, EmptyMetadata]]] {
+  type MemoryStore = MemoryVersionedStore[BagId, HybridStoreEntry[StorageManifest, EmptyMetadata]]
+
+  override def withContext[R](
+    testWith: TestWith[MemoryStore, R]): R =
+    testWith(
+      MemoryVersionedStore[BagId, HybridStoreEntry[StorageManifest, EmptyMetadata]](initialEntries = Map.empty)
+    )
+
+  override def withDao[R](testWith: TestWith[BetterStorageManifestDao, R])(implicit store: MemoryStore): R =
+    testWith(
+      new MemoryStorageManifestDao(store)
+    )
+}
+
+class DynamoStorageManifestDaoTest
+  extends BetterStorageManifestDaoTestCases[(Table, Bucket)]
+    with DynamoFixtures
+    with S3Fixtures {
+  override def withContext[R](testWith: TestWith[(Table, Bucket), R]): R =
+    withLocalDynamoDbTable { table =>
+      withLocalS3Bucket { bucket =>
+        testWith((table, bucket))
+      }
+    }
+
+  override def withDao[R](testWith: TestWith[BetterStorageManifestDao, R])(
+    implicit context: (Table, Bucket)): R = {
+    val (table, bucket) = context
+
+    testWith(
+      new DynamoStorageManifestDao(
+        dynamoConfig = createDynamoConfigWith(table),
+        s3Config = createS3ConfigWith(bucket)
+      )
+    )
+  }
+
+  override def createTable(table: Table): Table =
+    createTableWithHashRangeKey(
+      table,
+      hashKeyName = "id",
+      hashKeyType = ScalarAttributeType.S,
+      rangeKeyName = "version",
+      rangeKeyType = ScalarAttributeType.N
+    )
 }
 
 class StorageManifestDaoTest
