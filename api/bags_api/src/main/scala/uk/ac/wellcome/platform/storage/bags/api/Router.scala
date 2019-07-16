3173828fd3bbcdb26ebfa14b3ee269b2526beaab
Alex Chan <alex@alexwlchan.net>
2019-07-16 20:43:59 +0100

    Tidy up the logic for parsing version strings in the router

--- api/bags_api/src/main/scala/uk/ac/wellcome/platform/storage/bags/api/Router.scala
+++ api/bags_api/src/main/scala/uk/ac/wellcome/platform/storage/bags/api/Router.scala
@@ -10,19 +10,18 @@ import io.circe.Printer
 import uk.ac.wellcome.json.JsonUtil._
 import uk.ac.wellcome.platform.archive.common.bagit.models.{BagId, ExternalIdentifier}
 import uk.ac.wellcome.platform.archive.common.http.models.{InternalServerErrorResponse, UserErrorResponse}
-import uk.ac.wellcome.platform.archive.common.storage.models.{StorageManifest, StorageSpace}
+import uk.ac.wellcome.platform.archive.common.storage.models.StorageSpace
 import uk.ac.wellcome.platform.archive.common.storage.services.StorageManifestDao
 import uk.ac.wellcome.platform.storage.bags.api.models.{DisplayResultList, ResponseDisplayBag, ResultListEntry}
-import uk.ac.wellcome.storage.{NoVersionExistsError, ReadError}
+import uk.ac.wellcome.storage.NoVersionExistsError
 
 import scala.concurrent.ExecutionContext
 import scala.util.matching.Regex
+import scala.util.{Failure, Success, Try}
 
 class Router(storageManifestDao: StorageManifestDao, contextURL: URL)(
   implicit val ec: ExecutionContext)
     extends Logging {
-
-  private val versionRegex: Regex = new Regex("^v(\\d+)$", "version")
 
   def routes: Route = {
     import akka.http.scaladsl.server.Directives._
@@ -39,18 +38,16 @@ class Router(storageManifestDao: StorageManifestDao, contextURL: URL)(
 
         get {
           parameter('version.as[String] ?) { maybeVersion =>
-            val result: Either[ReadError, StorageManifest] =
-              maybeVersion match {
-                case Some(versionString) =>
-                  versionRegex.findFirstMatchIn(versionString) match {
-                    case Some(regexMatch) =>
-                      storageManifestDao.get(
-                        bagId,
-                        version = regexMatch.group("version").toInt)
-                    case None => Left(NoVersionExistsError())
-                  }
-                case None => storageManifestDao.getLatest(bagId)
-              }
+            val result = parseVersion(maybeVersion) match {
+              case Success(Some(version)) =>
+                storageManifestDao.get(bagId, version = version)
+
+              case Success(None) =>
+                storageManifestDao.getLatest(bagId)
+
+              case Failure(_) =>
+                Left(NoVersionExistsError())
+            }
 
             result match {
               case Right(storageManifest) =>
@@ -117,4 +114,17 @@ class Router(storageManifestDao: StorageManifestDao, contextURL: URL)(
       }
     }
   }
+
+  private val versionRegex: Regex = new Regex("^v(\\d+)$", "version")
+
+  private def parseVersion(queryParam: Option[String]): Try[Option[Int]] =
+    queryParam match {
+      case Some(versionString) =>
+        versionRegex.findFirstMatchIn(versionString) match {
+          case Some(regexMatch) => Success(Some(regexMatch.group("version").toInt))
+          case None             => Failure(new Throwable(s"Could not parse version string: $versionString"))
+        }
+
+      case None => Success(None)
+    }
 }
