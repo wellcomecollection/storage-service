1fb3e154519ff7c05fad6b87771b22e5b48a1efd
Alex Chan <a.chan@wellcome.ac.uk>
2019-07-16 14:54:35 +0100

    Implement an initial listVersions on the memory dao

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/storage/services/memory/MemoryStorageManifestDao.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/storage/services/memory/MemoryStorageManifestDao.scala
@@ -2,18 +2,26 @@ package uk.ac.wellcome.platform.archive.common.storage.services.memory
 
 import uk.ac.wellcome.platform.archive.common.bagit.models.BagId
 import uk.ac.wellcome.platform.archive.common.storage.models.StorageManifest
-import uk.ac.wellcome.platform.archive.common.storage.services.{
-  EmptyMetadata,
-  StorageManifestDao
-}
-import uk.ac.wellcome.storage.ReadError
+import uk.ac.wellcome.platform.archive.common.storage.services.{EmptyMetadata, StorageManifestDao}
+import uk.ac.wellcome.storage.{ReadError, Version}
 import uk.ac.wellcome.storage.store.HybridStoreEntry
-import uk.ac.wellcome.storage.store.memory.MemoryVersionedStore
+import uk.ac.wellcome.storage.store.memory.{MemoryStore, MemoryVersionedStore}
 
 class MemoryStorageManifestDao(
   val vhs: MemoryVersionedStore[
     BagId,
     HybridStoreEntry[StorageManifest, EmptyMetadata]]
 ) extends StorageManifestDao {
-  override def listVersions(bagId: BagId, before: Option[Int]): Either[ReadError, Seq[StorageManifest]] = ???
+  override def listVersions(bagId: BagId, before: Option[Int]): Either[ReadError, Seq[StorageManifest]] = {
+    val underlying =
+      vhs.store
+        .asInstanceOf[MemoryStore[Version[BagId, Int], HybridStoreEntry[StorageManifest, EmptyMetadata]]]
+
+    Right(
+      underlying.entries
+        .filter { case (_, HybridStoreEntry(manifest, _)) => manifest.id == bagId }
+        .map { case (_, HybridStoreEntry(manifest, _)) => manifest }
+        .toSeq
+    )
+  }
 }
