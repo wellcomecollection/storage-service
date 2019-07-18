4bb70344fe38a4d0a6d3a664506ca2e58eda0b0c
Helen Sherwood-Taylor <helen@rrdlabs.co.uk>
2019-07-18 11:53:48 +0000

    Add support for getting bag by version

--- python_client/src/wellcome_storage_service/__init__.py
+++ python_client/src/wellcome_storage_service/__init__.py
@@ -131,16 +131,20 @@ class StorageServiceClient:
 
     @needs_token
     @check_api_resp
-    def get_bag(self, space_id, source_id):
+    def get_bag(self, space_id, source_id, version=None):
         """
         Get an individual bag.
         """
-        bags_api_url = self.api_url + "/bags/%s/%s" % (space_id, source_id)
+        bags_url_suffix = '%s/%s' % (space_id, source_id)
+        if version:
+            bags_url_suffix += '?version=%s' % version
+
+        bags_api_url = self.api_url + "/bags/%s" % bags_url_suffix
         resp = self.sess.get(bags_api_url)
 
         if resp.status_code == 404:
             raise BagNotFound(
-                "Bags API returned 404 for bag %s/%s" % (space_id, source_id)
+                "Bags API returned 404 for bag %s" % bags_url_suffix
             )
         else:
             return resp
--- python_client/tests/test_bags.py
+++ python_client/tests/test_bags.py
@@ -12,6 +12,13 @@ def test_can_get_bag(client):
     assert len(resp["manifest"]["files"]) == 3
 
 
+def test_can_get_bag_version(client):
+    resp = client.get_bag(space_id="alex-testing", source_id="b12345", version='v1')
+    assert resp["space"]["id"] == "alex-testing"
+    assert resp["id"] == "alex-testing/b12345"
+    assert len(resp["manifest"]["files"]) == 3
+
+
 def test_raises_404_for_missing_bag(client):
     expected_message = "Bags API returned 404 for bag alex-testing/doesnotexist"
     with pytest.raises(BagNotFound, match=expected_message):
