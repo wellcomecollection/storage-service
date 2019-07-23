de408783c9978d45e6e82bec4a38c56d8173be2c
Robert Kenny <r.kenny@wellcome.ac.uk>
2019-07-23 09:31:17 +0100

    missing file

---
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/bagit/services/BagReadError.scala
@@ -0,0 +1,10 @@
+package uk.ac.wellcome.platform.archive.common.bagit.services
+
+sealed trait BagReadError extends Throwable {
+  val e: Throwable
+}
+
+case class BagInfoReadError(e: Throwable) extends Throwable(e) with BagReadError
+case class BagManifestReadError(e: Throwable) extends Throwable(e) with BagReadError
+case class TagManifestReadError(e: Throwable) extends Throwable(e) with BagReadError
+case class BagFetchReadError(e: Throwable) extends Throwable(e) with BagReadError
