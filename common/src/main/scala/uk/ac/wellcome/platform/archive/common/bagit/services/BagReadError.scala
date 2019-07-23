ffb630dfd3a3dcb12ba19f05d7b329b7e2a9ceb0
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2019-07-23 08:35:00 +0000

    Apply auto-formatting rules

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/bagit/services/BagReadError.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/bagit/services/BagReadError.scala
@@ -5,6 +5,12 @@ sealed trait BagReadError extends Throwable {
 }
 
 case class BagInfoReadError(e: Throwable) extends Throwable(e) with BagReadError
-case class BagManifestReadError(e: Throwable) extends Throwable(e) with BagReadError
-case class TagManifestReadError(e: Throwable) extends Throwable(e) with BagReadError
-case class BagFetchReadError(e: Throwable) extends Throwable(e) with BagReadError
+case class BagManifestReadError(e: Throwable)
+    extends Throwable(e)
+    with BagReadError
+case class TagManifestReadError(e: Throwable)
+    extends Throwable(e)
+    with BagReadError
+case class BagFetchReadError(e: Throwable)
+    extends Throwable(e)
+    with BagReadError
