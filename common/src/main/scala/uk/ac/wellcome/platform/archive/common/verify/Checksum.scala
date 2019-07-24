806ba721b8376d914a6f75f9b05c780be1341b3e
Alex Chan <a.chan@wellcome.ac.uk>
2019-07-24 17:24:33 +0100

    Include the expected/actual checksums in a checksum mismatch

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/verify/Checksum.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/verify/Checksum.scala
@@ -97,7 +97,7 @@ case class FailedChecksumCreation(algorithm: HashingAlgorithm, e: Throwable)
     extends Throwable(s"Could not create checksum: ${e.getMessage}")
     with FailedChecksum
 case class FailedChecksumNoMatch(actual: Checksum, expected: Checksum)
-    extends Throwable("Checksum values do not match!")
+    extends Throwable(s"Checksum values do not match! Expected: $expected, saw: $actual")
     with FailedChecksum
 case class FailedChecksumLocationNotFound[T](location: VerifiableLocation)
     extends Throwable("VerifiableLocation not found!")
