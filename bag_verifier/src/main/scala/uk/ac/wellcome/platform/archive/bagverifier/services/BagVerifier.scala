29ece78c44cef72660a8c2641d38058b6cbf5e2a
Alex Chan <alex@alexwlchan.net>
2019-07-25 12:57:29 +0100

    Refactor the bag verifier for readability
    We need to add more checks here, so decomposing the different steps
    makes it easier to see each check we're doing in turn.

--- bag_verifier/src/main/scala/uk/ac/wellcome/platform/archive/bagverifier/services/BagVerifier.scala
+++ bag_verifier/src/main/scala/uk/ac/wellcome/platform/archive/bagverifier/services/BagVerifier.scala
@@ -23,74 +23,116 @@ class BagVerifier()(
   verifier: Verifier[_]
 ) extends Logging {
 
+  type InternalResult[T] = Either[IngestFailed[VerificationSummary], T]
+
   def verify(root: ObjectLocation, externalIdentifier: ExternalIdentifier)
     : Try[IngestStepResult[VerificationSummary]] =
     Try {
-      implicit val bagVerifiable: BagVerifiable =
-        new BagVerifiable(root)
       val startTime = Instant.now()
 
-      bagReader.get(root) match {
-        case Left(bagUnavailable) =>
+      val stepResult
+        : Either[IngestFailed[VerificationSummary], VerificationSuccessSummary] = for {
+        bag <- getBag(root, startTime = startTime)
+
+        _ <- verifyExternalIdentifier(
+          bag = bag,
+          externalIdentifier = externalIdentifier,
+          root = root,
+          startTime = startTime
+        )
+
+        result <- verifyChecksums(
+          root = root,
+          bag = bag,
+          startTime = startTime
+        )
+      } yield result
+
+      stepResult match {
+        case Left(ingestFailed)    => ingestFailed
+        case Right(successSummary) => IngestStepSucceeded(successSummary: VerificationSuccessSummary)
+      }
+    }
+
+  private def getBag(root: ObjectLocation,
+                     startTime: Instant): InternalResult[Bag] =
+    bagReader.get(root) match {
+      case Left(bagUnavailable) =>
+        Left(
           IngestFailed(
             summary =
               VerificationSummary.incomplete(root, bagUnavailable, startTime),
             e = bagUnavailable,
             maybeUserFacingMessage = Some(bagUnavailable.msg)
           )
+        )
 
-        case Right(bag) =>
-          if (bag.info.externalIdentifier != externalIdentifier) {
-            IngestFailed(
-              summary = VerificationFailureSummary(
-                rootLocation = root,
-                verification = None,
-                startTime = startTime,
-                endTime = Instant.now()
-              ),
-              e = new Throwable(
-                "External identifier in bag-info.txt does not match request"),
-              maybeUserFacingMessage = Some(
-                s"External identifier in bag-info.txt does not match request: ${bag.info.externalIdentifier.underlying} is not ${externalIdentifier.underlying}"
-              )
-            )
-          } else {
-            VerificationSummary.create(root, bag.verify, startTime) match {
-              case success @ VerificationSuccessSummary(_, _, _, _) =>
-                IngestStepSucceeded(success)
-              case failure @ VerificationFailureSummary(
-                    _,
-                    Some(verification),
-                    _,
-                    _) =>
-                val verificationFailureMessage =
-                  verification.failure
-                    .map { verifiedFailure =>
-                      s"${verifiedFailure.location.uri}: ${verifiedFailure.e.getMessage}"
-                    }
-                    .mkString("\n")
-
-                warn(s"Errors verifying $root:\n$verificationFailureMessage")
-
-                val errorCount = verification.failure.size
-
-                val userFacingMessage =
-                  if (errorCount == 1)
-                    "There was 1 error verifying the bag"
-                  else
-                    s"There were $errorCount errors verifying the bag"
-
-                IngestFailed(failure, InvalidBag(bag), Some(userFacingMessage))
-
-              case failure @ VerificationFailureSummary(_, None, _, _) =>
-                IngestFailed(failure, InvalidBag(bag))
-
-              case incomplete @ VerificationIncompleteSummary(_, _, _, _) =>
-                IngestFailed(incomplete, incomplete.e)
-            }
-          }
-      }
+      case Right(bag) => Right(bag)
     }
+
+  private def verifyExternalIdentifier(
+    bag: Bag,
+    externalIdentifier: ExternalIdentifier,
+    root: ObjectLocation,
+    startTime: Instant): InternalResult[Unit] =
+    if (bag.info.externalIdentifier != externalIdentifier) {
+      Left(
+        IngestFailed(
+          summary = VerificationFailureSummary(
+            rootLocation = root,
+            verification = None,
+            startTime = startTime,
+            endTime = Instant.now()
+          ),
+          e = new Throwable(
+            "External identifier in bag-info.txt does not match request"),
+          maybeUserFacingMessage = Some(
+            s"External identifier in bag-info.txt does not match request: ${bag.info.externalIdentifier.underlying} is not ${externalIdentifier.underlying}"
+          )
+        )
+      )
+    } else {
+      Right(())
+    }
+
+  private def verifyChecksums(
+    root: ObjectLocation,
+    bag: Bag,
+    startTime: Instant
+  ): InternalResult[VerificationSuccessSummary] = {
+    implicit val bagVerifiable: BagVerifiable =
+      new BagVerifiable(root)
+
+    VerificationSummary.create(root, bag.verify, startTime) match {
+      case success @ VerificationSuccessSummary(_, _, _, _) =>
+        Right(success)
+      case failure @ VerificationFailureSummary(_, Some(verification), _, _) =>
+        val verificationFailureMessage =
+          verification.failure
+            .map { verifiedFailure =>
+              s"${verifiedFailure.location.uri}: ${verifiedFailure.e.getMessage}"
+            }
+            .mkString("\n")
+
+        warn(s"Errors verifying $root:\n$verificationFailureMessage")
+
+        val errorCount = verification.failure.size
+
+        val userFacingMessage =
+          if (errorCount == 1)
+            "There was 1 error verifying the bag"
+          else
+            s"There were $errorCount errors verifying the bag"
+
+        Left(IngestFailed(failure, InvalidBag(bag), Some(userFacingMessage)))
+
+      case failure @ VerificationFailureSummary(_, None, _, _) =>
+        Left(IngestFailed(failure, InvalidBag(bag)))
+
+      case incomplete @ VerificationIncompleteSummary(_, _, _, _) =>
+        Left(IngestFailed(incomplete, incomplete.e))
+    }
+  }
 }
 
 case class InvalidBag(bag: Bag)
