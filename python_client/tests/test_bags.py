a58c71ec3ba392e101087312f12ffde4de8fd00c
Helen Sherwood-Taylor <helen@rrdlabs.co.uk>
2019-07-25 09:06:44 +0000

    Test for correct bag version retrieval

--- python_client/tests/test_bags.py
+++ python_client/tests/test_bags.py
@@ -16,7 +16,13 @@ def test_can_get_bag_version(client):
     resp = client.get_bag(space_id="alex-testing", source_id="b12345", version="v1")
     assert resp["space"]["id"] == "alex-testing"
     assert resp["id"] == "alex-testing/b12345"
+    assert resp["version"] == "v1"
     assert len(resp["manifest"]["files"]) == 3
+
+
+def test_gets_correct_bag_version(client):
+    resp = client.get_bag(space_id="alex-testing", source_id="b12345", version="v3")
+    assert resp["version"] == "v3"
 
 
 def test_raises_404_for_missing_bag(client):
