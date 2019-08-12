c51474ceb3c6000d5d9f2e491f8acb9a9bf25e3a
Alex Chan <a.chan@wellcome.ac.uk>
2019-08-12 13:51:43 +0100

    Add some more tests for DestinationBuilder

--- bag_replicator/src/test/scala/uk/ac/wellcome/platform/archive/bagreplicator/services/DestinationBuilderTest.scala
+++ bag_replicator/src/test/scala/uk/ac/wellcome/platform/archive/bagreplicator/services/DestinationBuilderTest.scala
@@ -13,6 +13,45 @@ class DestinationBuilderTest
     with ExternalIdentifierGenerators
     with StorageSpaceGenerators {
 
+  def createNamespace: String =
+    randomAlphanumeric
+
+  it("uses the given namespace") {
+    val namespace = createNamespace
+
+    val builder = new DestinationBuilder(
+      namespace = namespace,
+      rootPath = None
+    )
+
+    val location = builder.buildDestination(
+      storageSpace = createStorageSpace,
+      externalIdentifier = createExternalIdentifier,
+      version = createBagVersion
+    )
+
+    location.namespace shouldBe namespace
+  }
+
+  it("constructs the correct path") {
+    val builder = new DestinationBuilder(
+      namespace = createNamespace,
+      rootPath = None
+    )
+
+    val storageSpace = createStorageSpace
+    val externalIdentifier = createExternalIdentifier
+    val version = createBagVersion
+
+    val location = builder.buildDestination(
+      storageSpace = storageSpace,
+      externalIdentifier = externalIdentifier,
+      version = version
+    )
+
+    location.path shouldBe s"${storageSpace.underlying}/${externalIdentifier.toString}/$version"
+  }
+
   it("uses the root path if provided") {
     val builder = new DestinationBuilder(
       namespace = "MyNamespace",
