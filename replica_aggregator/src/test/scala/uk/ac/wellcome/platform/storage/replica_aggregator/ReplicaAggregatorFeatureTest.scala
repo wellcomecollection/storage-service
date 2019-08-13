d56ebd2ec80a08c6cea2f04119e569cba9ff5ce0
Alex Chan <a.chan@wellcome.ac.uk>
2019-08-13 11:53:59 +0100

    Get the replica aggregator tests passing

--- replica_aggregator/src/test/scala/uk/ac/wellcome/platform/storage/replica_aggregator/ReplicaAggregatorFeatureTest.scala
+++ replica_aggregator/src/test/scala/uk/ac/wellcome/platform/storage/replica_aggregator/ReplicaAggregatorFeatureTest.scala
@@ -1,44 +1,48 @@
 package uk.ac.wellcome.platform.storage.replica_aggregator
 
+import org.scalatest.concurrent.{Eventually, IntegrationPatience}
 import org.scalatest.{FunSpec, Matchers}
 import uk.ac.wellcome.json.JsonUtil._
 import uk.ac.wellcome.messaging.memory.MemoryMessageSender
-import uk.ac.wellcome.platform.archive.common.fixtures.S3BagBuilder
+import uk.ac.wellcome.platform.archive.common.EnrichedBagInformationPayload
 import uk.ac.wellcome.platform.archive.common.generators.PayloadGenerators
+import uk.ac.wellcome.platform.archive.common.ingests.fixtures.IngestUpdateAssertions
 import uk.ac.wellcome.platform.storage.replica_aggregator.fixtures.ReplicaAggregatorFixtures
-import uk.ac.wellcome.storage.fixtures.S3Fixtures
 
 class ReplicaAggregatorFeatureTest
     extends FunSpec
     with Matchers
     with ReplicaAggregatorFixtures
-    with S3Fixtures
-    with PayloadGenerators {
+    with IngestUpdateAssertions
+    with PayloadGenerators
+    with Eventually
+    with IntegrationPatience {
 
-  it("fails") {
+  it("passes through the message ") {
     withLocalSqsQueueAndDlq { queuePair =>
       val ingests = new MemoryMessageSender()
       val outgoing = new MemoryMessageSender()
 
-      withLocalS3Bucket { bucket =>
-        val (bagRootLocation, bagInfo) = S3BagBuilder.createS3BagWith(bucket)
+      val payload = createEnrichedBagInformationPayload
 
-        withReplicaAggregatorWorker(
-          queuePair.queue,
-          ingests,
-          outgoing,
-          stepName = "aggregating replicas"
-        ) { _ =>
-          val payload = createEnrichedBagInformationPayloadWith(
-            context = createPipelineContextWith(
-              externalIdentifier = bagInfo.externalIdentifier
-            ),
-            bagRootLocation = bagRootLocation
+      withReplicaAggregatorWorker(
+        queuePair.queue,
+        ingests,
+        outgoing,
+        stepName = "aggregating replicas"
+      ) { _ =>
+        sendNotificationToSQS(queuePair.queue, payload)
+
+        eventually {
+          assertTopicReceivesIngestEvents(
+            ingestId = payload.ingestId,
+            ingests = ingests,
+            expectedDescriptions = Seq(
+              "Aggregating replicas succeeded"
+            )
           )
 
-          sendNotificationToSQS(queuePair.queue, payload)
-
-          true shouldBe false
+          outgoing.getMessages[EnrichedBagInformationPayload] shouldBe Seq(payload)
         }
       }
     }
