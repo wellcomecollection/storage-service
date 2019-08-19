2feeeb4c35102955c4dfa1e97966c3c51f4069e2
Alex Chan <alex@alexwlchan.net>
2019-08-19 19:05:56 +0100

    Fix the name of a parameter here

--- common/src/test/scala/uk/ac/wellcome/platform/archive/common/storage/services/StorageManifestServiceTest.scala
+++ common/src/test/scala/uk/ac/wellcome/platform/archive/common/storage/services/StorageManifestServiceTest.scala
@@ -69,7 +69,7 @@ class StorageManifestServiceTest
 
     storageManifest.location shouldBe PrimaryStorageLocation(
       provider = InfrequentAccessStorageProvider,
-      location = bagRoot.asPrefix
+      prefix = bagRoot.asPrefix
     )
 
     storageManifest.replicaLocations shouldBe empty
