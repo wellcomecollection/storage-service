d5c70e182bc1962b13632df905875217e803d18d
Robert Kenny <r.kenny@wellcome.ac.uk>
2019-08-20 09:26:24 +0100

    Use DynamoSingleVersionStore from scala-storage

--- replica_aggregator/src/main/scala/uk/ac/wellcome/platform/storage/replica_aggregator/Main.scala
+++ replica_aggregator/src/main/scala/uk/ac/wellcome/platform/storage/replica_aggregator/Main.scala
@@ -5,33 +5,22 @@ import akka.stream.ActorMaterializer
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import com.amazonaws.services.sqs.AmazonSQSAsync
 import com.typesafe.config.Config
-import org.scanamo.auto._
 import uk.ac.wellcome.json.JsonUtil._
-import uk.ac.wellcome.messaging.typesafe.{
-  AlpakkaSqsWorkerConfigBuilder,
-  CloudwatchMonitoringClientBuilder,
-  SQSBuilder
-}
+import uk.ac.wellcome.messaging.typesafe.{AlpakkaSqsWorkerConfigBuilder, CloudwatchMonitoringClientBuilder, SQSBuilder}
 import uk.ac.wellcome.messaging.worker.monitoring.CloudwatchMonitoringClient
-import uk.ac.wellcome.platform.archive.common.config.builders.{
-  IngestUpdaterBuilder,
-  OperationNameBuilder,
-  OutgoingPublisherBuilder
-}
-import uk.ac.wellcome.platform.storage.replica_aggregator.models.{
-  AggregatorInternalRecord,
-  ReplicaPath
-}
-import uk.ac.wellcome.platform.storage.replica_aggregator.services.{
-  ReplicaAggregator,
-  ReplicaAggregatorWorker
-}
+import uk.ac.wellcome.platform.archive.common.config.builders.{IngestUpdaterBuilder, OperationNameBuilder, OutgoingPublisherBuilder}
+import uk.ac.wellcome.platform.storage.replica_aggregator.models.{AggregatorInternalRecord, ReplicaPath}
+import uk.ac.wellcome.platform.storage.replica_aggregator.services.{ReplicaAggregator, ReplicaAggregatorWorker}
 import uk.ac.wellcome.storage.dynamo.DynamoConfig
+import uk.ac.wellcome.storage.store.dynamo.DynamoSingleVersionStore
 import uk.ac.wellcome.storage.typesafe.DynamoBuilder
 import uk.ac.wellcome.typesafe.WellcomeTypesafeApp
 import uk.ac.wellcome.typesafe.config.builders.AkkaBuilder
+import org.scanamo.auto._
+import uk.ac.wellcome.json.JsonUtil._
 
 import scala.concurrent.ExecutionContextExecutor
+
 
 object Main extends WellcomeTypesafeApp {
   runWithConfig { config: Config =>
