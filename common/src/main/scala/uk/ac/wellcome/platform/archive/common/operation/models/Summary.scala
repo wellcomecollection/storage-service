4f63c331e37d8f3ba1b52414a4be56b560c80d62
Alex Chan <a.chan@wellcome.ac.uk>
2019-09-02 09:07:52 +0100

    Use a more consistent for Summary logging in the register

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/operation/models/Summary.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/operation/models/Summary.scala
@@ -1,14 +1,23 @@
 package uk.ac.wellcome.platform.archive.common.operation.models
 
 import org.apache.commons.io.FileUtils
+import uk.ac.wellcome.platform.archive.common.ingests.models.IngestID
 
 trait Summary extends Timed {
-  override def toString: String = {
-    f"""|duration=$formatDuration""".stripMargin
-      .replaceAll("\n", ", ")
-  }
+  val ingestId: IngestID
 
-  def formatDuration: String =
+  val fieldsToLog: Seq[(String, Any)]
+
+  override def toString: String =
+    (fieldsToLog ++ Seq(
+      ("ingestId", ingestId),
+      ("duration", formatDuration),
+      ("durationSeconds", durationSeconds)
+    ))
+      .map { case (key, value) => s"$key=$value" }
+      .mkString(", ")
+
+  private def formatDuration: String =
     duration.getOrElse("<not-completed>").toString
 
   def formatBytes(bytes: Long): String =
