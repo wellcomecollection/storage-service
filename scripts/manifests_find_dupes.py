2ccd81da21bb4f80926be08b51f7ff89bf42a9c5
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2019-09-20 05:29:19 +0000

    Apply auto-formatting rules

--- scripts/manifests_find_dupes.py
+++ scripts/manifests_find_dupes.py
@@ -28,6 +28,7 @@ def get_s3_object(bucket, key):
 
 if __name__ == "__main__":
     import glob
+
     manifests_dir = max(glob.glob("dynamodb/vhs-storage-manifests*"))
 
     manifests_by_bnumber = collections.defaultdict(list)
