e297cdf44d8a91688deed31b3944f80a84d00032
Alex Chan <a.chan@wellcome.ac.uk>
2019-10-04 11:19:12 +0100

    Also get the size of an object from Preservica

--- scripts/status_reporter/preservica.py
+++ scripts/status_reporter/preservica.py
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8
 
+import os
 import pathlib
 import tempfile
 
@@ -9,21 +10,11 @@ import pyodbc
 from aws_client import read_only_client
 
 
-def get_preservica_cursor():
-    cnxn = pyodbc.connect(
-        driver="{ODBC Driver 17 for SQL Server}",
-        server="wt-dhaka",
-        database="WELLCOME_SDB4",
-        trusted_connection="yes",
+def get_preservica_asset_path(guid):
+    result = (
+        _get_preservica_asset_path_from_s3(guid) or
+        _get_preservica_asset_path_from_euston_road(guid)
     )
-
-    return cnxn.cursor()
-
-
-def get_preservica_asset(guid):
-    result = _get_preservica_assets_from_s3(
-        guid
-    ) or _get_preservica_assets_from_euston_road(guid)
 
     if result is None:
         raise RuntimeError(
@@ -33,30 +24,88 @@ def get_preservica_asset(guid):
     return result
 
 
-def _get_preservica_assets_from_euston_road(guid):
+def get_preservica_asset_size(guid):
+    db_size = _get_preservica_asset_size_from_db(guid)
+
+    actual_size = (
+        _get_preservica_asset_size_from_s3(guid) or
+        _get_preservica_asset_size_from_euston_road(guid)
+    )
+
+    if actual_size is None:
+        raise RuntimeError(
+            f"Could not find asset {guid} in Preservica local storage or S3!"
+        )
+
+    assert db_size == actual_size
+
+    return db_size
+
+
+def get_preservica_cursor():
+    cnxn = pyodbc.connect(
+        driver="{ODBC Driver 17 for SQL Server}",
+        server="wt-dhaka",
+        database="WELLCOME_SDB4",
+        trusted_connection="yes"
+    )
+
+    return cnxn.cursor()
+
+
+def _get_preservica_asset_path_from_euston_road(guid):
     cursor = get_preservica_cursor()
 
-    cursor.execute(
-        """
+    cursor.execute("""
         SELECT File_Path AS FilePath, ManifestationRef FROM File_Location
         INNER JOIN ManifestationFile ON File_Ref=FileRef WHERE File_Ref=?
-    """,
-        guid,
-    )
+    """, guid)
 
     filepath, manifestation_ref = cursor.fetchone()
 
     return (
-        pathlib.Path("/Volumes/LIB_WDL_SDB_STORE001")
-        / manifestation_ref[:2]
-        / manifestation_ref[2:4]
-        / manifestation_ref[4:6]
-        / manifestation_ref
-        / filepath.replace("\\", "/")
+        pathlib.Path("/Volumes/LIB_WDL_SDB_STORE001") /
+        manifestation_ref[:2] /
+        manifestation_ref[2:4] /
+        manifestation_ref[4:6] /
+        manifestation_ref /
+        filepath.replace("\\", "/")
     )
 
 
-def _get_preservica_assets_from_s3(guid):
+def _get_preservica_asset_size_from_db(guid):
+    cursor = get_preservica_cursor()
+
+    cursor.execute("""SELECT FileSize FROM DigitalFile WHERE FileRef=?""", guid)
+
+    file_size,  = cursor.fetchone()
+
+    return file_size
+
+
+def _get_preservica_asset_size_from_euston_road(guid):
+    euston_road_path = _get_preservica_asset_path_from_euston_road(guid)
+    return os.stat(euston_road_path).st_size
+
+
+def _get_preservica_asset_size_from_s3(guid):
+    s3_client = read_only_client.s3_client()
+
+    try:
+        head_resp = s3_client.head_object(
+            Bucket="wdl-preservica",
+            Key=guid
+        )
+    except ClientError as err:
+        if err.args[0].startswith("An error occurred (404)"):
+            return None
+        else:
+            raise
+    else:
+        return head_resp["ContentLength"]
+
+
+def _get_preservica_asset_path_from_s3(guid):
     """
     Try to download a Preservica asset from the wdl-preservica bucket in S3.
     """
@@ -64,7 +113,11 @@ def _get_preservica_assets_from_s3(guid):
     _, path = tempfile.mkstemp()
 
     try:
-        s3_client.download_file(Bucket="wdl-preservica", Key=guid, Filename=path)
+        s3_client.download_file(
+            Bucket="wdl-preservica",
+            Key=guid,
+            Filename=path
+        )
     except ClientError as err:
         if err.args[0].startswith("An error occurred (404)"):
             return None
@@ -82,4 +135,7 @@ if __name__ == "__main__":
     except IndexError:
         sys.exit(f"Usage: {__file__} <PRESERVICA_GUID>")
 
-    print(get_preservica_asset(guid))
+    print(
+        get_preservica_asset_path(guid),
+        get_preservica_asset_size(guid)
+    )
