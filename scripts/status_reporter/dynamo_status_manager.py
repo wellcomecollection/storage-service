23f7833c15a1e2efbe85c06a960cd6f3529d7b38
Alex Chan <a.chan@wellcome.ac.uk>
2019-10-07 11:39:18 +0100

    Add a script for checking the existence of storage manifests

--- scripts/status_reporter/dynamo_status_manager.py
+++ scripts/status_reporter/dynamo_status_manager.py
@@ -1,7 +1,73 @@
+# -*- encoding: utf-8
+
 from boto3.dynamodb.types import TypeDeserializer
 from boto3.dynamodb.conditions import Attr
 
-import datetime as dt
+
+def get_all_statuses(table_name="storage-migration-status", first_bnumber=None):
+    from aws_client import read_only_client
+
+    deserializer = TypeDeserializer()
+
+    dynamo_client = read_only_client.dynamo_client()
+
+    pagination_kwargs = {
+        "TableName": table_name
+    }
+
+    if first_bnumber:
+        pagination_kwargs["ExclusiveStartKey"] = {"bnumber": {"S": first_bnumber}}
+
+    paginator = dynamo_client.get_paginator("scan")
+    all_pages = paginator.paginate(**pagination_kwargs)
+
+    for page in all_pages:
+        for row in page["Items"]:
+            yield {
+                key: deserializer.deserialize(value)
+                for key, value in row.items()
+            }
+
+
+class DynamoStatusUpdater:
+    def __enter__(self, table_name="storage-migration-status"):
+        from aws_client import dev_client
+        self.dynamo_table = dev_client.dynamo_table(table_name)
+
+        self._put_cache = []
+
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        # Drain any remaining items from the cache before returning.
+        with self.dynamo_table.batch_writer() as batch:
+            for item in self._put_cache:
+                batch.put_item(Item=item)
+
+    def _put_item(self, item):
+        # Rather than putting items one-at-a-time, we hold a buffer of items
+        # internally, and when it gets big enough, we pause to do a batch write
+        # to the table.
+        self._put_cache.append(item)
+
+        if len(self._put_cache) < 100:
+            return
+
+        with self.dynamo_table.batch_writer() as batch:
+            for item in self._put_cache:
+                batch.put_item(Item=item)
+
+        self._put_cache = []
+
+    def update_status(self, bnumber, *, status_name, success, last_modified=None):
+        item = self.dynamo_table.get_item(Key={"bnumber": bnumber})["Item"]
+
+        item[f"status-{status_name}"] = {
+            "success": success,
+            "last_modified": last_modified or dt.datetime.now().isoformat(),
+        }
+
+        self._put_item(item=item)
 
 
 class DynamoStatusManager:
@@ -19,10 +85,6 @@ class DynamoStatusManager:
     def _row_scan_generator(self, *args, **kwargs):
         paginator = self.dynamo_client.get_paginator("scan")
         all_pages = paginator.paginate(*args, **kwargs)
-
-        for page in all_pages:
-            for row in page["Items"]:
-                yield self._deserialize_row(row)
 
     def _get_raw_row(self, bnumber):
         resp = self.dynamo_table.get_item(Key={"bnumber": bnumber})
@@ -46,13 +108,3 @@ class DynamoStatusManager:
             TableName=self.table_name,
             ScanFilter={status_name: {"ComparisonOperator": "NULL"}},
         )
-
-    def update_status(self, bnumber, *, status_name, success, last_modified=None):
-        item = self._get_raw_row(bnumber)
-
-        item[f"status-{status_name}"] = {
-            "success": success,
-            "last_modified": last_modified or dt.datetime.now().isoformat(),
-        }
-
-        self.dynamo_table.put_item(Item=item)
---
+++ scripts/status_reporter/progress/check_storage_manifest_exists.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8
+
+import concurrent.futures
+import pathlib
+import sys
+
+import tqdm
+from wellcome_storage_service import BagNotFound
+
+sys.path.append(str(pathlib.Path(__file__).resolve().parent.parent))
+
+from aws_client import dev_client
+import dynamo_status_manager
+import helpers
+
+
+def needs_check(row):
+    bnumber = row["bnumber"]
+
+    if row.get("status-storage_manifest_created", {}).get("success"):
+        print(f"Already recorded storage manifest for {bnumber}")
+        return False
+
+    return True
+
+
+def run_check(status_updater, storage_client, row):
+    bnumber = row["bnumber"]
+
+    try:
+        response = storage_client.get_bag("digitised", bnumber)
+
+    except BagNotFound:
+        print(f"No bag in storage service for {bnumber}", file=sys.stderr)
+        return
+
+    manifest_date = response["createdDate"]
+
+    status_updater.update_status(
+        bnumber,
+        status_name="storage_manifest_created",
+        success=True,
+        last_modified=manifest_date
+    )
+
+    # print(f"Recorded storage manifest creation for {bnumber}")
+
+
+def get_statuses_for_updating():
+    for row in dynamo_status_manager.get_all_statuses(first_bnumber="b28476979"):
+        if needs_check(row):
+            yield row
+
+
+if __name__ == "__main__":
+    storage_client = helpers.create_storage_client()
+
+    futures = []
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        with dynamo_status_manager.DynamoStatusUpdater() as status_updater:
+            for row in get_statuses_for_updating():
+                futures.append(
+                    executor.submit(
+                        run_check, status_updater, storage_client, row
+                    )
+                )
+
+            for fut in tqdm.tqdm(
+                concurrent.futures.as_completed(futures), total=len(futures)
+            ):
+                fut.result()
