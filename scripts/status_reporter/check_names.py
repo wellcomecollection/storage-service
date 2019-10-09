e6f850ba6d9bc4a696f63eb456fa16e77b1ad742
Alex Chan <a.chan@wellcome.ac.uk>
2019-10-09 17:30:09 +0100

    Add names for two more checks

--- scripts/status_reporter/check_names.py
+++ scripts/status_reporter/check_names.py
@@ -1,18 +1,15 @@
 # -*- encoding: utf-8
 
+METS_EXISTS = "s3_mets_exists"
 STORAGE_MANIFESTS = "storage_manifest_created"
-
 DDS_SYNC = "dds_sync_complete"
-
-DLCS_ORIGIN_MATCH = "dlcs_origin_match"
-
 IIIF_MANIFESTS_CONTENTS = "iiif_manifest_contents_match"
-
 IIIF_MANIFESTS_FILE_SIZES = "iiif_manifest_file_sizes_match"
+DLCS_ORIGIN_MATCH = "dlcs_origin_match"
+DLCS_THUMBNAIL_RETRIEVE = "dlcs_thumbnail_retrieve"
+DLCS_ORIGIN_RETRIEVE = "dlcs_origin_retrieve"
 
 MANUAL_SKIP = "manual_skip"
-
-METS_EXISTS = "s3_mets_exists"
 
 ALL_CHECK_NAMES = [
     STORAGE_MANIFESTS,
@@ -22,4 +19,6 @@ ALL_CHECK_NAMES = [
     DLCS_ORIGIN_MATCH,
     MANUAL_SKIP,
     METS_EXISTS,
+    DLCS_THUMBNAIL_RETRIEVE,
+    DLCS_ORIGIN_RETRIEVE,
 ]
