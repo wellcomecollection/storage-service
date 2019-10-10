8ae2e74d1cb27aa456d01cf6b1b612d86b7cb4c5
Alex Chan <a.chan@wellcome.ac.uk>
2019-10-10 14:38:59 +0100

    Try to speed up the Preservica size lookups

--- scripts/status_reporter/preservica.py
+++ scripts/status_reporter/preservica.py
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8
 
+import collections
 import os
 import pathlib
 import tempfile
@@ -23,12 +24,38 @@ def get_preservica_asset_path(guid):
     return result
 
 
+LAST_CHECKS = collections.deque([], maxlen=5)
+
+
 def get_preservica_asset_size(guid):
     db_size = _get_preservica_asset_size_from_db(guid)
 
-    actual_size = _get_preservica_asset_size_from_s3(
-        guid
-    ) or _get_preservica_asset_size_from_euston_road(guid)
+    # The aim of this slightly fiddly looking code is to bias the Preservica
+    # size lookup.  All the files in a single bag will typically be in the
+    # same place: all in S3 or all in Euston Road.
+    #
+    # If this function is called repeatedly, it keeps track of the last few
+    # places it was asked to read a file from, and uses that to decide
+    # where to look next.
+    #
+    # At worst, we do an unnecessary filesystem call or S3 lookup, at best
+    # this function goes a bit faster.
+    if LAST_CHECKS.count("s3") > 3:
+        s3_size = _get_preservica_asset_size_from_s3(guid)
+        if s3_size is None:
+            actual_size = _get_preservica_asset_size_from_euston_road(guid)
+            LAST_CHECKS.append("euston_rd")
+        else:
+            actual_size = s3_size
+            LAST_CHECKS.append("s3")
+    else:
+        euston_rd_size = _get_preservica_asset_size_from_euston_road(guid)
+        if euston_rd_size is None:
+            actual_size = _get_preservica_asset_size_from_s3(guid)
+            LAST_CHECKS.append("s3_size")
+        else:
+            actual_size = euston_rd_size
+            LAST_CHECKS.append("euston_rd")
 
     if actual_size is None:
         raise RuntimeError(
@@ -86,21 +113,35 @@ def _get_preservica_asset_size_from_db(guid):
 
 def _get_preservica_asset_size_from_euston_road(guid):
     euston_road_path = _get_preservica_asset_path_from_euston_road(guid)
-    return os.stat(euston_road_path).st_size
+    try:
+        return os.stat(euston_road_path).st_size
+    except FileNotFoundError:
+        pass
+
+
+KNOWN_SIZES = {}
 
 
 def _get_preservica_asset_size_from_s3(guid):
-    s3_client = read_only_client.s3_client()
-
+    # To save us making loads of HeadObject requests, this function maintains
+    # a small internal cache of object sizes in S3.
     try:
-        head_resp = s3_client.head_object(Bucket="wdl-preservica", Key=guid)
-    except ClientError as err:
-        if err.args[0].startswith("An error occurred (404)"):
-            return None
-        else:
-            raise
-    else:
-        return head_resp["ContentLength"]
+        return KNOWN_SIZES[guid]
+    except KeyError:
+        s3_client = read_only_client.s3_client()
+
+        resp = s3_client.list_objects_v2(
+            Bucket="wdl-preservica",
+
+            # Chop a few chars off the end so we get the GUID we're looking for,
+            # if it's here.
+            StartAfter=guid[:-2]
+        )
+
+        for s3_obj in resp["Contents"]:
+            KNOWN_SIZES[s3_obj["Key"]] = s3_obj["Size"]
+
+        return KNOWN_SIZES.get(guid)
 
 
 def _get_preservica_asset_path_from_s3(guid):
@@ -129,4 +170,4 @@ if __name__ == "__main__":
     except IndexError:
         sys.exit(f"Usage: {__file__} <PRESERVICA_GUID>")
 
-    print(get_preservica_asset_path(guid), get_preservica_asset_size(guid))
+    print(get_preservica_asset_size(guid))
