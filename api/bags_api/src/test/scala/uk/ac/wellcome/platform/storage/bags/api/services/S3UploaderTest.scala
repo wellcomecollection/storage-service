632e98563f633902b3f6f907c5a90fc977fdc72c
Alex Chan <alex@alexwlchan.net>
2019-10-11 08:27:10 +0100

    Add a test for expiry of the S3 URLs

--- api/bags_api/src/test/scala/uk/ac/wellcome/platform/storage/bags/api/services/S3UploaderTest.scala
+++ api/bags_api/src/test/scala/uk/ac/wellcome/platform/storage/bags/api/services/S3UploaderTest.scala
@@ -1,19 +1,21 @@
 package uk.ac.wellcome.platform.storage.bags.api.services
 
+import java.io.IOException
 import java.net.URL
 import java.util
 
 import com.amazonaws.HttpMethod
 import com.amazonaws.services.s3.AmazonS3
-import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest
-import org.scalatest.{FunSpec, Matchers}
+import com.amazonaws.services.s3.model.{AmazonS3Exception, GeneratePresignedUrlRequest}
+import org.scalatest.{EitherValues, FunSpec, Matchers}
 import uk.ac.wellcome.storage.fixtures.S3Fixtures
 import uk.ac.wellcome.storage.store.s3.S3StreamStore
 import uk.ac.wellcome.storage.streaming.Codec._
 import uk.ac.wellcome.storage.streaming.InputStreamWithLengthAndMetadata
-import uk.ac.wellcome.storage.{ObjectLocation, ReadError, StorageError, StoreReadError}
+import uk.ac.wellcome.storage._
 
 import scala.concurrent.duration._
+import scala.io.Source
 import scala.util.{Failure, Success, Try}
 
 /** This class allows you to upload a string to an S3 bucket, and get a
@@ -71,31 +73,60 @@ class S3Uploader(implicit s3Client: AmazonS3) {
   }
 }
 
-class S3UploaderTest extends FunSpec with Matchers with S3Fixtures {
-  it("works") {
+class S3UploaderTest extends FunSpec with Matchers with S3Fixtures with EitherValues {
+  val uploader = new S3Uploader()
+
+  it("creates a pre-signed URL for an object") {
+    val content = randomAlphanumeric
+
     withLocalS3Bucket { bucket =>
-      s3Client.putObject(
-        bucket.name,
-        "digitised/b1234/v1.json",
-        "hello world"
-      )
+      val url = uploader.uploadAndGetURL(
+        location = createObjectLocationWith(bucket),
+        content = content,
+        expiryLength = 5.minutes
+      ).right.value
 
-      // Set the presigned URL to expire after an hour.
-      // Based on an example from the AWS SDK for Java docs:
-      // https://docs.aws.amazon.com/AmazonS3/latest/dev/ShareObjectPreSignedURLJavaSDK.html
-      val currentTime = new util.Date()
-      val currTimeMillis = currentTime.getTime
-      val expTimeMillis = currTimeMillis + 1000 * 60 * 60
-      val expTime = new util.Date(expTimeMillis)
-
-      // Generate the presigned URL
-      val request = new GeneratePresignedUrlRequest(bucket.name, "digitised/b1234/v1.json")
-        .withMethod(HttpMethod.GET)
-        .withExpiration(expTime)
-
-      val url = s3Client.generatePresignedUrl(request)
-
-      println(url)
+      getUrl(url) shouldBe content
     }
   }
+
+  it("expires the URL after the given duration") {
+    val content = randomAlphanumeric
+
+    withLocalS3Bucket { bucket =>
+      val url = uploader.uploadAndGetURL(
+        location = createObjectLocationWith(bucket),
+        content = content,
+        expiryLength = 2.seconds
+      ).right.value
+
+      getUrl(url) shouldBe content
+
+      Thread.sleep(3000)
+
+      val thrown = intercept[IOException] {
+        getUrl(url)
+      }
+
+      thrown.getMessage should startWith("Server returned HTTP response code: 403")
+    }
+  }
+
+  it("fails if it cannot upload to the bucket") {
+    val err = uploader.uploadAndGetURL(
+      location = createObjectLocationWith(createBucket),
+      content = randomAlphanumeric,
+      expiryLength = 5.minutes
+    ).left.value
+
+    err shouldBe a[StoreWriteError]
+    err.e shouldBe a[AmazonS3Exception]
+    err.e.getMessage should startWith("The specified bucket does not exist")
+  }
+
+  // TODO: Write a test for the case where generating the pre-signed URL fails.
+  // (I assume this is possible, I'm just not sure how.)
+
+  def getUrl(url: URL): String =
+    Source.fromURL(url).mkString
 }
