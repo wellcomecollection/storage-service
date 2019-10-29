ae056ebf903a2e1579922e1cf6436d9c559c9b40
Alex Chan <alex@alexwlchan.net>
2019-10-29 15:56:22 +0000

    Remove an upstreamed fix from DynamoIngestTracker
    The fixed version of max() is in the newest version of scala-storage,
    so we can delete all this code.

--- ingests_common/src/main/scala/uk/ac/wellcome/platform/archive/common/ingests/tracker/dynamo/DynamoIngestTracker.scala
+++ ingests_common/src/main/scala/uk/ac/wellcome/platform/archive/common/ingests/tracker/dynamo/DynamoIngestTracker.scala
@@ -9,23 +9,13 @@ import uk.ac.wellcome.platform.archive.common.ingests.models.IngestID._
 import uk.ac.wellcome.platform.archive.common.ingests.tracker.IngestTracker
 import uk.ac.wellcome.storage.dynamo._
 import uk.ac.wellcome.storage.store.VersionedStore
-import uk.ac.wellcome.storage.store.dynamo.DynamoHashStore
+import uk.ac.wellcome.storage.store.dynamo.DynamoSingleVersionStore
 
 class DynamoIngestTracker(config: DynamoConfig)(
   implicit client: AmazonDynamoDB
 ) extends IngestTracker
     with Logging {
 
-  // TODO: This should be upstreamed to the scala-storage library
-  private val hashStore = new DynamoHashStore[IngestID, Int, Ingest](config) {
-    override def max(hashKey: IngestID): Either[ReadError, Int] =
-      super.max(hashKey) match {
-        case Right(value)               => Right(value)
-        case Left(_: DoesNotExistError) => Left(NoMaximaValueError())
-        case Left(err)                  => Left(err)
-      }
-  }
-
   override val underlying: VersionedStore[IngestID, Int, Ingest] =
-    new VersionedStore[IngestID, Int, Ingest](hashStore)
+    new DynamoSingleVersionStore[IngestID, Ingest](config)
 }
