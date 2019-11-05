68c91296198449603ce48fa4e511c96872f664fc
Alex Chan <alex@alexwlchan.net>
2019-11-05 12:55:50 +0000

    Remove an unused reference to the Archivematica ingests bucket

--- terraform/infra/locals.tf
+++ terraform/infra/locals.tf
@@ -1,5 +1,3 @@
 locals {
   vpc_id = "${data.terraform_remote_state.infra_shared.storage_vpc_id}"
-
-  archivematica_ingests_bucket = "${data.terraform_remote_state.archivematica_infra.ingests_bucket}"
 }
