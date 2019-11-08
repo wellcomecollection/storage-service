ed3007103c58fc0246d3d74cf63c9587cb2e711a
Alex Chan <a.chan@wellcome.ac.uk>
2019-11-08 15:20:09 +0000

    Run terraform 0.12 in the infra stack

--- terraform/infra/terraform.tf
+++ terraform/infra/terraform.tf
@@ -9,23 +9,14 @@ terraform {
   }
 }
 
-provider "aws" {
-  assume_role {
-    role_arn = "arn:aws:iam::975596993436:role/storage-developer"
-  }
-
-  region  = "${var.aws_region}"
-  version = "1.60.0"
-}
-
 data "terraform_remote_state" "infra_shared" {
   backend = "s3"
 
-  config {
+  config = {
     role_arn = "arn:aws:iam::760097843905:role/platform-read_only"
-
-    bucket = "wellcomecollection-platform-infra"
-    key    = "terraform/shared_infra.tfstate"
-    region = "eu-west-1"
+    bucket   = "wellcomecollection-platform-infra"
+    key      = "terraform/shared_infra.tfstate"
+    region   = "eu-west-1"
   }
 }
+
