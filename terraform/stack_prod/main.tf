784f6c1c404fcbf3682aebf8ba37d4c488abc99c
Alex Chan <a.chan@wellcome.ac.uk>
2019-11-08 16:54:08 +0000

    Update the prod stack to terraform 0.12

--- terraform/stack_prod/main.tf
+++ terraform/stack_prod/main.tf
@@ -1,6 +1,6 @@
 resource "aws_api_gateway_base_path_mapping" "mapping_prod" {
-  api_id      = "${module.stack_prod.api_gateway_id}"
-  domain_name = "${local.domain_name}"
+  api_id      = module.stack_prod.api_gateway_id
+  domain_name = local.domain_name
   base_path   = "storage"
 }
 
@@ -9,49 +9,50 @@ module "stack_prod" {
 
   namespace = "${local.namespace}-prod"
 
-  api_url          = "${local.api_url}"
-  domain_name      = "${local.domain_name}"
-  cert_domain_name = "${local.cert_domain_name}"
+  api_url          = local.api_url
+  domain_name      = local.domain_name
+  cert_domain_name = local.cert_domain_name
 
   min_capacity = 1
   max_capacity = 10
 
-  vpc_id = "${local.vpc_id}"
+  vpc_id = local.vpc_id
 
-  private_subnets = "${local.private_subnets}"
+  private_subnets = local.private_subnets
 
-  dlq_alarm_arn = "${local.dlq_alarm_arn}"
+  dlq_alarm_arn = local.dlq_alarm_arn
 
-  cognito_user_pool_arn          = "${local.cognito_user_pool_arn}"
-  cognito_storage_api_identifier = "${local.cognito_storage_api_identifier}"
+  cognito_user_pool_arn          = local.cognito_user_pool_arn
+  cognito_storage_api_identifier = local.cognito_storage_api_identifier
 
   release_label = "prod"
-  nginx_image   = "${local.nginx_image}"
+  nginx_image   = local.nginx_image
 
-  replica_primary_bucket_name = "${data.terraform_remote_state.critical_prod.replica_primary_bucket_name}"
-  replica_glacier_bucket_name = "${data.terraform_remote_state.critical_prod.replica_glacier_bucket_name}"
+  replica_primary_bucket_name = data.terraform_remote_state.critical_prod.outputs.replica_primary_bucket_name
+  replica_glacier_bucket_name = data.terraform_remote_state.critical_prod.outputs.replica_glacier_bucket_name
 
-  static_content_bucket_name = "${data.terraform_remote_state.critical_prod.static_content_bucket_name}"
+  static_content_bucket_name = data.terraform_remote_state.critical_prod.outputs.static_content_bucket_name
 
-  vhs_manifests_bucket_name = "${data.terraform_remote_state.critical_prod.vhs_manifests_bucket_name}"
-  vhs_manifests_table_name  = "${data.terraform_remote_state.critical_prod.vhs_manifests_table_name}"
+  vhs_manifests_bucket_name = data.terraform_remote_state.critical_prod.outputs.vhs_manifests_bucket_name
+  vhs_manifests_table_name  = data.terraform_remote_state.critical_prod.outputs.vhs_manifests_table_name
 
-  vhs_manifests_readonly_policy  = "${data.terraform_remote_state.critical_prod.vhs_manifests_readonly_policy}"
-  vhs_manifests_readwrite_policy = "${data.terraform_remote_state.critical_prod.vhs_manifests_readwrite_policy}"
+  vhs_manifests_readonly_policy  = data.terraform_remote_state.critical_prod.outputs.vhs_manifests_readonly_policy
+  vhs_manifests_readwrite_policy = data.terraform_remote_state.critical_prod.outputs.vhs_manifests_readwrite_policy
 
-  versioner_versions_table_arn   = "${data.terraform_remote_state.critical_prod.versions_table_arn}"
-  versioner_versions_table_name  = "${data.terraform_remote_state.critical_prod.versions_table_name}"
-  versioner_versions_table_index = "${data.terraform_remote_state.critical_prod.versions_table_index}"
+  versioner_versions_table_arn   = data.terraform_remote_state.critical_prod.outputs.versions_table_arn
+  versioner_versions_table_name  = data.terraform_remote_state.critical_prod.outputs.versions_table_name
+  versioner_versions_table_index = data.terraform_remote_state.critical_prod.outputs.versions_table_index
 
-  alarm_topic_arn = "${local.gateway_server_error_alarm_arn}"
+  alarm_topic_arn = local.gateway_server_error_alarm_arn
 
-  ingests_table_name = "${data.terraform_remote_state.critical_prod.ingests_table_name}"
-  ingests_table_arn  = "${data.terraform_remote_state.critical_prod.ingests_table_arn}"
+  ingests_table_name = data.terraform_remote_state.critical_prod.outputs.ingests_table_name
+  ingests_table_arn  = data.terraform_remote_state.critical_prod.outputs.ingests_table_arn
 
-  replicas_table_arn  = "${data.terraform_remote_state.critical_prod.replicas_table_arn}"
-  replicas_table_name = "${data.terraform_remote_state.critical_prod.replicas_table_name}"
+  replicas_table_arn  = data.terraform_remote_state.critical_prod.outputs.replicas_table_arn
+  replicas_table_name = data.terraform_remote_state.critical_prod.outputs.replicas_table_name
 
-  workflow_bucket_name = "${local.workflow_bucket_name}"
+  workflow_bucket_name = local.workflow_bucket_name
 
-  archivematica_ingests_bucket = "${local.archivematica_ingests_bucket}"
+  archivematica_ingests_bucket = local.archivematica_ingests_bucket
 }
+
