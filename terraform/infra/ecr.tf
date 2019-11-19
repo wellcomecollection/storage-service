130c2d61f64b0be41bb01abdd2425820aaee03e7
Alex Chan <a.chan@wellcome.ac.uk>
2019-11-19 14:50:56 +0000

    Add the nginx Docker image to this repo

--- terraform/infra/ecr.tf
+++ terraform/infra/ecr.tf
@@ -70,3 +70,8 @@ module "ecr_repository_replica_aggregator" {
   namespace = "uk.ac.wellcome"
 }
 
+module "ecr_repository_nginx" {
+  source    = "git::https://github.com/wellcometrust/terraform.git//ecr?ref=v19.5.1"
+  id        = "nginx"
+  namespace = "uk.ac.wellcome"
+}
