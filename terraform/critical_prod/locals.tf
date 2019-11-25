801209e8da81fc6e1a783c40de3a15295393fc2a
alicefuzier <alicefuzier@gmail.com>
2019-11-25 11:14:53 +0000

    Allow mets adapter to read from storage service bucket

--- terraform/critical_prod/locals.tf
+++ terraform/critical_prod/locals.tf
@@ -3,9 +3,11 @@ locals {
 
   goobi_task_role_arn         = "arn:aws:iam::299497370133:role/goobi_task_role"
   archivematica_task_role_arn = "arn:aws:iam::299497370133:role/am-prod-storage-service_task_role"
+  catalogue_pipeline_task_role_arn = "arn:aws:iam::760097843905:role/mets_adapter_task_role"
 
   workflow_account_principal     = "arn:aws:iam::299497370133:root"
   digitisation_account_principal = "arn:aws:iam::404315009621:root"
+  catalogue_pipeline_account_principal = "arn:aws:iam::760097843905:root"
 
   digitisation_mediaconvert_role_arn = data.terraform_remote_state.digitisation_private.outputs.mediaconvert_role_arn
 }
