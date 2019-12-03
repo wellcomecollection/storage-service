ccc720a3fec4ef5c632831253b1527fe1539e735
Alex Chan <alex@alexwlchan.net>
2019-12-03 23:31:04 +0000

    Do not allow empty external identifiers

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/bagit/models/ExternalIdentifier.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/bagit/models/ExternalIdentifier.scala
@@ -3,8 +3,10 @@ package uk.ac.wellcome.platform.archive.common.bagit.models
 import org.scanamo.DynamoFormat
 import io.circe.{Decoder, Encoder, HCursor, Json}
 
-case class ExternalIdentifier(underlying: String) extends AnyVal {
+case class ExternalIdentifier(underlying: String) {
   override def toString: String = underlying
+
+  require(!underlying.isEmpty, "External identifier cannot be empty")
 }
 
 object ExternalIdentifier {
---
+++ common/src/test/scala/uk/ac/wellcome/platform/archive/common/bagit/models/ExternalIdentifierTest.scala
@@ -0,0 +1,17 @@
+package uk.ac.wellcome.platform.archive.common.bagit.models
+
+import org.scalatest.{FunSpec, Matchers}
+
+class ExternalIdentifierTest extends FunSpec with Matchers {
+  it("allows creating an external identifier") {
+    ExternalIdentifier("b12345678")
+  }
+
+  it("blocks creating an external identifier with an empty string") {
+    val err = intercept[IllegalArgumentException] {
+      ExternalIdentifier("")
+    }
+
+    err.getMessage shouldBe "requirement failed: External identifier cannot be empty"
+  }
+}
