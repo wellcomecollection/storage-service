ef4dd7ab25e26e1619c55c94e432031b338fb238
Alex Chan <a.chan@wellcome.ac.uk>
2019-12-04 11:32:40 +0000

    Remove some unused scripts [skip ci]

--- scripts/get_bag.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-BNUM="$(./get_location.sh $1)"
-./ss_get_bag.py 'digitised' ${BNUM} 2>/dev/null 1>/dev/null
-
-echo "$1,$BNUM,$?"
--- scripts/get_location.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-./ss_get_ingest.py $1 | sed -n 2p | awk '{print $2}' | awk -F'/' '{print $4}' | awk -F'.' '{print $1}'
