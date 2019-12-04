c4bc24d3073f49470e7d27c0abf412652cc51c04
Alex Chan <a.chan@wellcome.ac.uk>
2019-12-04 12:54:30 +0000

    Add some code for parsing a BagId from a string

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/bagit/models/BagId.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/bagit/models/BagId.scala
@@ -9,3 +9,22 @@ case class BagId(
   override def toString: String =
     s"$space/$externalIdentifier"
 }
+
+case object BagId {
+  // We can parse a bag ID this way because storage spaces never contain
+  // slashes, only the external identifier.
+  //
+  // The first slash can be treated as part of the bag ID, and any remaining
+  // slashes can be treated as part of the external identifier.
+  def apply(value: String): BagId =
+    value.split("/", 2) match {
+      case Array(space, externalIdentifier) =>
+        BagId(
+          space = StorageSpace(space),
+          externalIdentifier = ExternalIdentifier(externalIdentifier)
+        )
+
+      case _ =>
+        throw new IllegalArgumentException(s"Cannot create bag ID from $value")
+    }
+}
--- common/src/test/scala/uk/ac/wellcome/platform/archive/common/bagit/models/BagIdTest.scala
+++ common/src/test/scala/uk/ac/wellcome/platform/archive/common/bagit/models/BagIdTest.scala
@@ -3,9 +3,10 @@ package uk.ac.wellcome.platform.archive.common.bagit.models
 import org.scalatest.FunSpec
 import uk.ac.wellcome.json.JsonUtil._
 import uk.ac.wellcome.json.utils.JsonAssertions
+import uk.ac.wellcome.platform.archive.common.generators.BagIdGenerators
 import uk.ac.wellcome.platform.archive.common.storage.models.StorageSpace
 
-class BagIdTest extends FunSpec with JsonAssertions {
+class BagIdTest extends FunSpec with JsonAssertions with BagIdGenerators {
   it("serialises space and external identifier as strings") {
     val bagId =
       BagId(StorageSpace("digitised"), ExternalIdentifier("b1234567x"))
@@ -19,4 +20,42 @@ class BagIdTest extends FunSpec with JsonAssertions {
     assertJsonStringsAreEqual(toJson(bagId).get, expectedJson)
   }
 
+  it("can cast a BagId to a string and back again") {
+    val bagId = createBagId
+
+    BagId(bagId.toString) shouldBe bagId
+  }
+
+  it("can parse a slash in the string representation") {
+    val bagId = BagId(
+      space = StorageSpace("born-digital"),
+      externalIdentifier = ExternalIdentifier("PP/MIA/1")
+    )
+
+    BagId(bagId.toString) shouldBe bagId
+  }
+
+  it("fails if you try to create a BagId from a string without a slash") {
+    val err = intercept[IllegalArgumentException] {
+      BagId("no slashes here")
+    }
+
+    err.getMessage shouldBe "Cannot create bag ID from no slashes here"
+  }
+
+  it("fails if you try to create a BagId from a string with an empty storage space") {
+    val err = intercept[IllegalArgumentException] {
+      BagId("/b12345678")
+    }
+
+    err.getMessage shouldBe "requirement failed: Storage space cannot be empty"
+  }
+
+  it("fails if you try to create a BagId from a string with an empty external identifier") {
+    val err = intercept[IllegalArgumentException] {
+      BagId("digitised/")
+    }
+
+    err.getMessage shouldBe "requirement failed: External identifier cannot be empty"
+  }
 }
