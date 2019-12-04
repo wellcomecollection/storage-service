3143c094fcc2247112390eccea3c3334c79d3b27
Alex Chan <alex@alexwlchan.net>
2019-12-04 08:18:50 +0000

    Don't URL encode slashes in the replicator
    Because we're enforcing that the storage space should never contain a
    slash, we can now expand the external identifier to give a nice
    hierarchy in an unambiguous way:
    
        space: "alfa"
        identifier: "bravo/charlie"
    
         => S3 path: "alfa/bravo/charlie"

--- bag_replicator/src/main/scala/uk/ac/wellcome/platform/archive/bagreplicator/services/DestinationBuilder.scala
+++ bag_replicator/src/main/scala/uk/ac/wellcome/platform/archive/bagreplicator/services/DestinationBuilder.scala
@@ -1,7 +1,5 @@
 package uk.ac.wellcome.platform.archive.bagreplicator.services
 
-import java.net.URLEncoder
-import java.nio.charset.StandardCharsets
 import java.nio.file.Paths
 
 import uk.ac.wellcome.platform.archive.common.bagit.models.{
@@ -20,30 +18,10 @@ class DestinationBuilder(namespace: String) {
     namespace = namespace,
     path = Paths
       .get(
-        encode(storageSpace.toString),
-        encode(externalIdentifier.toString),
+        storageSpace.toString,
+        externalIdentifier.toString,
         version.toString
       )
       .toString
   )
-
-  // We encode the storage space and external identifier with
-  // a URL encoder so that:
-  //
-  //  - the path created is still human-readable
-  //  - slashes are escaped
-  //
-  // This means the replicator will never mix up:
-  //
-  //        space = "a/b"   identifier = "c"
-  // and
-  //        space = "a"     identifier = "b/c"
-  //
-  // They will get distinct paths.
-  //
-  // Names that only contain alphanumeric characters and hyphens
-  // (e.g. b1234, digitised-workflow) will be unmodified.
-  //
-  private def encode(s: String): String =
-    URLEncoder.encode(s, StandardCharsets.UTF_8.toString)
 }
