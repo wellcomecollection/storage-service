be54f46e417fb38a0786f9306b94d932f2e111df
Alex Chan <alex@alexwlchan.net>
2019-12-09 10:19:52 +0000

    Replicate objects as Standard-IA
    This is to save money: all of our objects are suitable for the IA access
    class, but the default is to store them as Standard.

--- bag_replicator/src/main/scala/uk/ac/wellcome/platform/archive/bagreplicator/replicator/s3/S3Replicator.scala
+++ bag_replicator/src/main/scala/uk/ac/wellcome/platform/archive/bagreplicator/replicator/s3/S3Replicator.scala
@@ -1,10 +1,11 @@
 package uk.ac.wellcome.platform.archive.bagreplicator.replicator.s3
 
 import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.model.StorageClass
 import uk.ac.wellcome.platform.archive.bagreplicator.replicator.Replicator
 import uk.ac.wellcome.storage.transfer.s3.S3PrefixTransfer
 
 class S3Replicator(implicit s3Client: AmazonS3) extends Replicator {
   implicit val prefixTransfer: S3PrefixTransfer =
-    S3PrefixTransfer()
+    S3PrefixTransfer(storageClass = StorageClass.StandardInfrequentAccess)
 }
