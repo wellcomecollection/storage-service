d2513d18bd966b50c989a5bdbdf82b02b9fc15a4
Robert Kenny <r.kenny@wellcome.ac.uk>
2020-01-13 11:38:55 +0000

    fix for another copy paste

--- terraform/critical_staging/main.tf
+++ terraform/critical_staging/main.tf
@@ -1,7 +1,7 @@
 module "critical" {
   source = "../modules/critical"
 
-  namespace = local.namespace
+  namespace = "${local.namespace}-staging"
 
   replica_primary_read_principals = [
     local.archivematica_task_role_arn,
