4ae0255d5ddc42be51ca12bf081d48a6a08890cf
Alex Chan <a.chan@wellcome.ac.uk>
2020-01-21 16:26:44 +0000

    more tweaks

--- cleanup_scripts/cleanup_alto.py
+++ cleanup_scripts/cleanup_alto.py
@@ -8,6 +8,7 @@ import os
 import re
 import sys
 import termcolor
+import uuid
 
 import tqdm
 from wellcome_storage_service import StorageServiceClient
@@ -79,88 +80,90 @@ def warn(s):
     print(termcolor.colored(s, "yellow"), file=sys.stderr)
 
 
-def log_event(s):  # pragma: no cover
-    with open(f"alto_cleanup_{NOW}.log", "a") as out_file:
+UUID = str(uuid.uuid4())
+
+with open(f"alto_cleanup_{NOW}.{UUID}.log", "a") as out_file:
+    def log_event(s):  # pragma: no cover
         out_file.write(s.rstrip() + "\n")
 
 
-# Trying to look for the b number in an ALTO filename, e.g.
-#
-#     b21020000_0001.xml
-#
-B_NUMBER_RE = re.compile(r"^(?P<b_number>b[0-9]{7}[0-9x])")
+    # Trying to look for the b number in an ALTO filename, e.g.
+    #
+    #     b21020000_0001.xml
+    #
+    B_NUMBER_RE = re.compile(r"^(?P<b_number>b[0-9]{7}[0-9x])")
 
 
-def get_info_blobs(paths):
-    for alto_name, alto_path in paths:
+    def get_info_blobs(paths):
+        for alto_name, alto_path in paths:
+            try:
+                b_number = B_NUMBER_RE.match(alto_name).group("b_number")
+            except AttributeError:
+                warn(f"Unable to identify b number in {alto_path!r}")
+                continue
+
+            files = get_xml_files(b_number)
+
+            try:
+                matching_file = files[alto_name]
+            except KeyError:
+                warn(f"Unable to find file in storage manifest for {alto_path!r}")
+                continue
+
+            yield {
+                "name": alto_name,
+                "path": alto_path,
+                "b_number": b_number,
+                "alto_size": size(alto_path),
+                "stored_size": matching_file["size"],
+                "alto_checksum": checksum(alto_path),
+                "stored_checksum": matching_file["checksum"],
+            }
+
+
+    def get_paths_for_deletion(info_blobs):
+        for info in info_blobs:
+            if info["alto_size"] != info["stored_size"]:
+                warn(
+                    f"Sizes don't match:\nPath       = %s\nMETS share = %10d\nStorage    = %10d"
+                    % (info["path"], info["alto_size"], info["stored_size"])
+                )
+                continue
+
+            if info["alto_checksum"] != info["stored_checksum"]:
+                warn(
+                    f"Checksums don't match:\nPath       = %s\nMETS share = %s\nStorage    = %s\n"
+                    % (info["path"], info["alto_checksum"], info["stored_checksum"])
+                )
+                continue
+
+            yield (info["path"], info["alto_checksum"], info["alto_size"])
+
+
+    def run_deleter(root):
+        paths = get_alto_paths(root)
+        info_blobs = get_info_blobs(paths)
+        paths_for_deletion = get_paths_for_deletion(info_blobs)
+
+        for path, checksum, size in tqdm.tqdm(paths_for_deletion):
+
+            # This allows us to gracefully stop the script, and it will refuse to
+            # run until we start it again.
+            if os.path.exists("stop.txt"):  # pragma: no cover
+                break
+
+            log_event(
+                json.dumps(
+                    {"event": "delete", "path": path, "checksum": checksum, "size": size,}
+                )
+            )
+            os.unlink(path)
+
+
+    if __name__ == "__main__":  # pragma: no cover
         try:
-            b_number = B_NUMBER_RE.match(alto_name).group("b_number")
-        except AttributeError:
-            warn(f"Unable to identify b number in {alto_path!r}")
-            continue
+            root = sys.argv[1]
+        except IndexError:
+            sys.exit(f"Usage: {__file__} <ROOT>")
 
-        files = get_xml_files(b_number)
-
-        try:
-            matching_file = files[alto_name]
-        except KeyError:
-            warn(f"Unable to find file in storage manifest for {alto_path!r}")
-            continue
-
-        yield {
-            "name": alto_name,
-            "path": alto_path,
-            "b_number": b_number,
-            "alto_size": size(alto_path),
-            "stored_size": matching_file["size"],
-            "alto_checksum": checksum(alto_path),
-            "stored_checksum": matching_file["checksum"],
-        }
-
-
-def get_paths_for_deletion(info_blobs):
-    for info in info_blobs:
-        if info["alto_size"] != info["stored_size"]:
-            warn(
-                f"Sizes don't match:\nPath       = %s\nMETS share = %10d\nStorage    = %10d"
-                % (info["path"], info["alto_size"], info["stored_size"])
-            )
-            continue
-
-        if info["alto_checksum"] != info["stored_checksum"]:
-            warn(
-                f"Checksums don't match:\nPath       = %s\nMETS share = %s\nStorage    = %s\n"
-                % (info["path"], info["alto_checksum"], info["stored_checksum"])
-            )
-            continue
-
-        yield (info["path"], info["alto_checksum"], info["alto_size"])
-
-
-def run_deleter(root):
-    paths = get_alto_paths(root)
-    info_blobs = get_info_blobs(paths)
-    paths_for_deletion = get_paths_for_deletion(info_blobs)
-
-    for path, checksum, size in tqdm.tqdm(paths_for_deletion):
-
-        # This allows us to gracefully stop the script, and it will refuse to
-        # run until we start it again.
-        if os.path.exists("stop.txt"):  # pragma: no cover
-            break
-
-        log_event(
-            json.dumps(
-                {"event": "delete", "path": path, "checksum": checksum, "size": size,}
-            )
-        )
-        os.unlink(path)
-
-
-if __name__ == "__main__":  # pragma: no cover
-    try:
-        root = sys.argv[1]
-    except IndexError:
-        sys.exit(f"Usage: {__file__} <ROOT>")
-
-    run_deleter(root)
+        run_deleter(root)
--- cleanup_scripts/status_report.py
+++ cleanup_scripts/status_report.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 
+import datetime
 import glob
 import json
+import subprocess
 
 import humanize
 import termcolor
 
 
 if __name__ == "__main__":
-    total_deleted = 0
-    total_size = 0
+    total_deleted = 1339218
+    total_size = 84395734645
+
+    processes = int(
+        subprocess.check_output(
+            "ps -eaf | grep python | grep cleanup_alto | wc -l", shell=True
+        )
+    ) - 1
 
     for log_path in glob.iglob("*.log"):
         for line in open(log_path):
@@ -22,10 +30,12 @@ if __name__ == "__main__":
             total_size += data["size"]
 
     print(
-        "Deleted %s (%s bytes) in %s files"
+        "Running %s processes; deleted %s (%s bytes) in %s files @ %s"
         % (
+            termcolor.colored(processes, "green"),
             termcolor.colored(humanize.naturalsize(total_size), "green"),
             humanize.intcomma(total_size),
             termcolor.colored(total_deleted, "green"),
+            datetime.datetime.now().isoformat(),
         )
     )
