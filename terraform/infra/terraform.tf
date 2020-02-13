64efcb658676b6566ad6d493d67c601e9b33111f
Alex Chan <a.chan@wellcome.ac.uk>
2020-02-13 09:19:05 +0000

    Use the new location for the shared tfstate file
    [skip ci]

--- terraform/infra/terraform.tf
+++ terraform/infra/terraform.tf
@@ -15,7 +15,7 @@ data "terraform_remote_state" "infra_shared" {
   config = {
     role_arn = "arn:aws:iam::760097843905:role/platform-read_only"
     bucket   = "wellcomecollection-platform-infra"
-    key      = "terraform/shared_infra.tfstate"
+    key      = "terraform/platform-infrastructure/shared.tfstate"
     region   = "eu-west-1"
   }
 }
