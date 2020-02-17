3a53ac7a3ddcce6798a80ec382467f77af1873c2
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2020-02-17 09:03:04 +0000

    Apply auto-formatting rules

--- terraform/critical_staging/locals.tf
+++ terraform/critical_staging/locals.tf
@@ -1,7 +1,7 @@
 locals {
   namespace = "storage"
 
-  goobi_task_role_arn         = "arn:aws:iam::299497370133:role/goobi_task_role"
+  goobi_task_role_arn = "arn:aws:iam::299497370133:role/goobi_task_role"
 
   shell_server_1_task_role = "arn:aws:iam::299497370133:role/shell_server_1_task_role"
   shell_server_2_task_role = "arn:aws:iam::299497370133:role/shell_server_2_task_role"
@@ -9,10 +9,10 @@ locals {
   shell_server_4_task_role = "arn:aws:iam::299497370133:role/shell_server_4_task_role"
 
   catalogue_pipeline_task_role_arn = "arn:aws:iam::760097843905:role/read_storage_s3_role"
-  archivematica_task_role_arn = "arn:aws:iam::299497370133:role/am-staging-storage-service_task_role"
+  archivematica_task_role_arn      = "arn:aws:iam::299497370133:role/am-staging-storage-service_task_role"
 
-  workflow_account_principal     = "arn:aws:iam::299497370133:root"
-  digitisation_account_principal = "arn:aws:iam::404315009621:root"
+  workflow_account_principal           = "arn:aws:iam::299497370133:root"
+  digitisation_account_principal       = "arn:aws:iam::404315009621:root"
   catalogue_pipeline_account_principal = "arn:aws:iam::760097843905:root"
 
   digitisation_mediaconvert_role_arn = data.terraform_remote_state.digitisation_private.outputs.mediaconvert_role_arn
