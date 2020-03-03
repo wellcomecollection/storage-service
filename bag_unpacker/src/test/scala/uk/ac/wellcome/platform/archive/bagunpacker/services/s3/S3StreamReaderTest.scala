6caf4de56bd77114e68191f348b3e16de88919b0
Alex Chan <alex@alexwlchan.net>
2020-03-03 09:55:33 +0000

    Add some tests for S3StreamReader
    These tests both fail, and catch the bug introduced in #448.

---
+++ bag_unpacker/src/test/scala/uk/ac/wellcome/platform/archive/bagunpacker/services/s3/S3StreamReaderTest.scala
@@ -0,0 +1,49 @@
+package uk.ac.wellcome.platform.archive.bagunpacker.services.s3
+
+import org.mockito.{Matchers => MockitoMatchers, Mockito}
+import org.scalatest.mockito.MockitoSugar
+import org.scalatest.{EitherValues, FunSpec, Matchers}
+import uk.ac.wellcome.platform.archive.common.fixtures.StorageRandomThings
+import uk.ac.wellcome.storage.fixtures.S3Fixtures
+import uk.ac.wellcome.storage.streaming.Codec._
+
+class S3StreamReaderTest extends FunSpec with Matchers with S3Fixtures with MockitoSugar with StorageRandomThings with EitherValues {
+  it("makes multiple GetObject requests") {
+    withLocalS3Bucket { bucket =>
+      val location = createObjectLocationWith(bucket)
+
+      s3Client.putObject(location.namespace, location.path, randomAlphanumericWithLength(length = 1000))
+
+      val spyClient = Mockito.spy(s3Client)
+      val reader = new S3StreamReader(bufferSize = 500)(s3Client = spyClient)
+
+      val result = reader.get(location)
+      result.right.value.identifiedT.read()
+
+      // We expect to see at least three calls to getObject:
+      //
+      //    - One to get the size of the object
+      //    - Two or more to read the object
+      //
+      Mockito
+        .verify(spyClient, Mockito.atLeast(3))
+        .getObject(MockitoMatchers.any())
+    }
+  }
+
+  it("joins multiple GetObject streams correctly") {
+    withLocalS3Bucket { bucket =>
+      val message = randomAlphanumericWithLength(length = 1000)
+      val location = createObjectLocationWith(bucket)
+
+      s3Client.putObject(location.namespace, location.path, message)
+
+      val reader = new S3StreamReader(bufferSize = 500)
+
+      val inputStream = reader.get(location).right.value.identifiedT
+      val s = stringCodec.fromStream(inputStream)
+      println(s)
+      stringCodec.fromStream(inputStream).right.value shouldBe message
+    }
+  }
+}
