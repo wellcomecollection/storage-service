335d28820ce41698d7181a0563779d84029b0e8b
Alex Chan <alex@alexwlchan.net>
2020-03-09 10:55:12 +0000

    Bump the version of scala-storage to get retries in S3Transfer

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object WellcomeDependencies {
     val json = "1.1.1"
     val messaging = "5.3.1"
     val monitoring = "2.3.0"
-    val storage = "7.25.0"
+    val storage = "7.25.1"
     val typesafe = "1.0.0"
   }
 
