d31dfb81d94d0afa2e448f4092e4d42928e8ed7f
Alex Chan <alex@alexwlchan.net>
2020-03-18 20:45:57 +0000

    Add some more happy path test cases

--- common/src/test/scala/uk/ac/wellcome/platform/archive/common/bagit/models/ManifestFileParserTest.scala
+++ common/src/test/scala/uk/ac/wellcome/platform/archive/common/bagit/models/ManifestFileParserTest.scala
@@ -15,10 +15,91 @@ class ManifestFileParserTest extends FunSpec with Matchers with EitherValues wit
         |""".stripMargin)
 
     val files = ManifestFileParser.createFileLists(sha256 = sha256)
-    
+
     files.success.value shouldBe Map(
       BagPath("file1.txt") -> VerifiableChecksum(sha256 = ChecksumValue("abc123")),
       BagPath("file2.txt") -> VerifiableChecksum(sha256 = ChecksumValue("def456")),
+    )
+  }
+
+  it("parses a SHA256 and an MD5 manifest") {
+    val md5 = asStream(
+      """
+        |aaa111   file1.txt
+        |aaa222   file2.txt
+        |""".stripMargin)
+
+    val sha256 = asStream(
+      """
+        |bbb111   file1.txt
+        |bbb222   file2.txt
+        |""".stripMargin)
+
+    val files = ManifestFileParser.createFileLists(md5 = Some(md5), sha256 = sha256)
+
+    files.success.value shouldBe Map(
+      BagPath("file1.txt") -> VerifiableChecksum(
+        md5 = Some(ChecksumValue("aaa111")),
+        sha256 = ChecksumValue("bbb111")
+      ),
+      BagPath("file2.txt") -> VerifiableChecksum(
+        md5 = Some(ChecksumValue("aaa222")),
+        sha256 = ChecksumValue("bbb222")
+      ),
+    )
+  }
+
+  it("parses a SHA256 and an SHA1 manifest") {
+    val sha1 = asStream(
+      """
+        |aaa111   file1.txt
+        |aaa222   file2.txt
+        |""".stripMargin)
+
+    val sha256 = asStream(
+      """
+        |bbb111   file1.txt
+        |bbb222   file2.txt
+        |""".stripMargin)
+
+    val files = ManifestFileParser.createFileLists(sha1 = Some(sha1), sha256 = sha256)
+
+    files.success.value shouldBe Map(
+      BagPath("file1.txt") -> VerifiableChecksum(
+        sha1 = Some(ChecksumValue("aaa111")),
+        sha256 = ChecksumValue("bbb111")
+      ),
+      BagPath("file2.txt") -> VerifiableChecksum(
+        sha1 = Some(ChecksumValue("aaa222")),
+        sha256 = ChecksumValue("bbb222")
+      ),
+    )
+  }
+
+  it("parses a SHA256 and an SHA512 manifest") {
+    val sha512 = asStream(
+      """
+        |aaa111   file1.txt
+        |aaa222   file2.txt
+        |""".stripMargin)
+
+    val sha256 = asStream(
+      """
+        |bbb111   file1.txt
+        |bbb222   file2.txt
+        |""".stripMargin)
+
+    val files = ManifestFileParser.createFileLists(sha512 = Some(sha512), sha256 = sha256)
+
+    files.success.value shouldBe Map(
+      BagPath("file1.txt") -> VerifiableChecksum(
+        sha512 = Some(ChecksumValue("aaa111")),
+        sha256 = ChecksumValue("bbb111")
+      ),
+      BagPath("file2.txt") -> VerifiableChecksum(
+        sha512 = Some(ChecksumValue("aaa222")),
+        sha256 = ChecksumValue("bbb222")
+      ),
     )
   }
 
