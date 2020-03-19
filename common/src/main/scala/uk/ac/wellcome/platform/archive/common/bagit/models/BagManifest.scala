6c454809395b6e1e1e824dae8df53606f69c315c
Alex Chan <alex@alexwlchan.net>
2020-03-19 10:12:31 +0000

    Create a Map[BagPath, ChecksumValue] inside the BagManifest parser
    The BagManifest parser gets some more robust tests, and takes
    responsibility for noticing duplicate entries in a manifest file (which
    are not allowed).
    
    This is a stepping stone to using this map everywhere else.

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/bagit/models/BagManifest.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/bagit/models/BagManifest.scala
@@ -23,60 +23,106 @@ case class BagManifest(
   def paths: Seq[BagPath] = entries.keys.toSeq
 }
 
+/** Each line in a manifest file _manifest-algorithm.txt_ is of the form
+  *
+  *     checksum filepath
+  *
+  * where _checksum_ is a hex-encoded checksum for the file at _filepath_ created
+  * using _algorithm_ (e.g. md5, sha256).  The payload manifest lists every
+  * file in the data/ directory; the tag manifest lists every file in the
+  * top directory.
+  *
+  * This class parses the contents of a manifest file.
+  *
+  * See https://tools.ietf.org/html/rfc8493#section-2.1.3
+  *     https://tools.ietf.org/html/rfc8493#section-2.2.1
+  *
+  */
 object BagManifest {
-
-  // Intended to match BagIt `manifest-algorithm.txt` file format:
-  // https://tools.ietf.org/html/draft-kunze-bagit-17#section-2.1.3
-
-  val LINE_REGEX: Regex = """(.+?)\s+(.+)""".r
 
   def create(
     inputStream: InputStream,
     algorithm: HashingAlgorithm
   ): Try[BagManifest] = {
-
     val bufferedReader = new BufferedReader(
       new InputStreamReader(inputStream)
     )
 
+    // Read the lines one by one, either getting a (filepath -> checksum) association,
+    // or returning the line if it doesn't parse correctly.
     val lines = Iterator
       .continually(bufferedReader.readLine())
       .takeWhile { _ != null }
       .filter { _.nonEmpty }
       .toList
 
-    val eitherFiles = lines.map(createBagFile(_, None, algorithm))
+    val parsedLines = lines.map { parseSingleLine }
 
-    val errorStrings = eitherFiles.collect {
-      case Left(errorString) => errorString
+    val unparseableLines = parsedLines.collect {
+      case Left(line) => line
     }
 
-    val files = eitherFiles.collect {
-      case Right(bagFile) => bagFile
-    }
+    // The BagIt spec says a manifest cannot refer to the same file more than once;
+    // for example, this is illegal (even though the checksums match):
+    //
+    //    abcdef  myfile.txt
+    //    abcdef  myfile.txt
+    //
+    // This looks for duplicate paths in the manifest file.
+    val paths = parsedLines
+      .collect { case Right(assoc) => assoc }
+      .flatMap { _.keys }
 
-    if (errorStrings.isEmpty) {
-      Success(BagManifest(algorithm, files))
+    val duplicatePaths: List[BagPath] = paths
+      .groupBy { identity }
+      .mapValues { _.size }
+      .filter { case (_, count) => count > 1 }
+      .map { case (bagPath, _) => bagPath }
+      .toList
+
+    val associations: Map[BagPath, ChecksumValue] = parsedLines
+      .collect { case Right(assoc) => assoc }
+      .foldLeft(Map[BagPath, ChecksumValue]()) { _ ++ _ }
+
+    if (unparseableLines.nonEmpty) {
+      Failure(
+        new RuntimeException(
+          s"Failed to parse the following lines: $unparseableLines"
+        )
+      )
+    } else if (duplicatePaths.nonEmpty) {
+      Failure(
+        new RuntimeException(
+          s"Manifest contains duplicate paths: $duplicatePaths"
+        )
+      )
     } else {
-      Failure(new RuntimeException(s"Failed to parse: ${lines}"))
+      val bagFiles = associations.map { case (path, checksumValue) =>
+        BagFile(
+          checksum = Checksum(
+            algorithm = algorithm,
+            value = checksumValue
+          ),
+          path = path
+        )
+      }
+      Success(BagManifest(checksumAlgorithm = algorithm, files = bagFiles.toSeq))
     }
   }
 
-  private def createBagFile(
-    line: String,
-    maybeRoot: Option[String],
-    algorithm: HashingAlgorithm
-  ): Either[String, BagFile] = line match {
-    case LINE_REGEX(checksumString, itemPathString) =>
-      Right(
-        BagFile(
-          Checksum(
-            algorithm,
-            ChecksumValue.create(checksumString)
-          ),
-          BagPath.create(itemPathString)
-        )
-      )
-    case l => Left(l)
+  // Represents a single line in a manifest file, e.g.
+  //
+  //    3532b221585eaa6d2520068ec1938928a1069a72fc50a8df9f3472da3f458037  data/b12345678.xml
+  //
+  // Note: checksums can be upper or lowercase.
+  //
+  private val LINE_REGEX: Regex = """([0-9a-fA-F]+?)\s+(.+)""".r
+
+  private def parseSingleLine(
+                               line: String
+                             ): Either[String, Map[BagPath, ChecksumValue]] = line match {
+    case LINE_REGEX(checksum, filepath) =>
+      Right(Map(BagPath.create(filepath) -> ChecksumValue.create(checksum)))
+    case _ => Left(line)
   }
 }
---
+++ common/src/test/scala/uk/ac/wellcome/platform/archive/common/bagit/models/BagManifestTest.scala
@@ -0,0 +1,114 @@
+package uk.ac.wellcome.platform.archive.common.bagit.models
+
+import java.io.InputStream
+
+import org.scalatest.{EitherValues, FunSpec, Matchers, TryValues}
+import uk.ac.wellcome.platform.archive.common.verify.{Checksum, ChecksumValue, MD5, SHA256}
+import uk.ac.wellcome.storage.streaming.Codec._
+
+class BagManifestTest extends FunSpec with Matchers with EitherValues with TryValues {
+  it("parses a valid manifest") {
+    val inputStream = toInputStream("""
+      |abc123   file1.txt
+      |def456   file2.txt
+      |""".stripMargin)
+
+    val manifest = BagManifest.create(inputStream, algorithm = SHA256)
+
+    manifest.success.value shouldBe BagManifest(
+      checksumAlgorithm = SHA256,
+      files = Seq(
+        BagFile(
+          checksum = Checksum(algorithm = SHA256, value = ChecksumValue("abc123")),
+          path = BagPath("file1.txt")
+        ),
+        BagFile(
+          checksum = Checksum(algorithm = SHA256, value = ChecksumValue("def456")),
+          path = BagPath("file2.txt")
+        )
+      )
+    )
+  }
+
+  it("sets the checksum algorithm correctly") {
+    val inputStream = toInputStream("""
+                                      |abc123   file1.txt
+                                      |def456   file2.txt
+                                      |""".stripMargin)
+
+    val manifest = BagManifest.create(inputStream, algorithm = MD5)
+
+    manifest.success.value shouldBe BagManifest(
+      checksumAlgorithm = MD5,
+      files = Seq(
+        BagFile(
+          checksum = Checksum(algorithm = MD5, value = ChecksumValue("abc123")),
+          path = BagPath("file1.txt")
+        ),
+        BagFile(
+          checksum = Checksum(algorithm = MD5, value = ChecksumValue("def456")),
+          path = BagPath("file2.txt")
+        )
+      )
+    )
+  }
+
+  describe("catching errors in the manifest file") {
+    it("spots duplicate entries") {
+      val inputStream = toInputStream("""
+                              |aaa111   file1.txt
+                              |aaa111   file1.txt
+                              |aaa222   file2.txt
+                              |""".stripMargin)
+
+      val result = BagManifest.create(inputStream, algorithm = SHA256)
+
+      val err = result.failure.exception
+
+      err shouldBe a[RuntimeException]
+      err.getMessage should startWith("Manifest contains duplicate paths:")
+    }
+
+    it("if a manifest has a line which isn't `checksum filepath`") {
+      val inputStream = toInputStream("""
+                              |notavalidline
+                              |aaa111   file1.txt
+                              |""".stripMargin)
+
+      val result = BagManifest.create(inputStream, algorithm = SHA256)
+
+      val err = result.failure.exception
+
+      err shouldBe a[RuntimeException]
+      err.getMessage should startWith("Failed to parse the following lines:")
+    }
+
+    it("if the checksum is not hex-encoded") {
+      val inputStream = toInputStream("""
+                              |nothex   badfile.txt
+                              |aaa111   file1.txt
+                              |""".stripMargin)
+
+      val result = BagManifest.create(inputStream, algorithm = SHA256)
+
+      val err = result.failure.exception
+
+      err shouldBe a[RuntimeException]
+      err.getMessage should startWith("Failed to parse the following lines:")
+    }
+
+    it("if the manifest does not contain any valid entries") {
+      val inputStream = toInputStream("neroifuighsdfiug")
+
+      val result = BagManifest.create(inputStream, algorithm = SHA256)
+
+      val err = result.failure.exception
+
+      err shouldBe a[RuntimeException]
+      err.getMessage should startWith("Failed to parse the following lines:")
+    }
+  }
+
+  private def toInputStream(s: String): InputStream =
+    stringCodec.toStream(s).right.value
+}
