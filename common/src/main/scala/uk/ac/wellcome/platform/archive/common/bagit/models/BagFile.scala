cf6ecc3f85519cf6a952f92fd7711380f9cd436c
Alex Chan <alex@alexwlchan.net>
2020-03-19 09:46:58 +0000

    Remove the now-unused BagLocation trait
    It's only being used on a single class, and it's a name that doesn't
    really map to anything in BagIt terms.  We can remove it and clean up
    the namespace.

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/bagit/models/BagFile.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/bagit/models/BagFile.scala
@@ -2,11 +2,7 @@ package uk.ac.wellcome.platform.archive.common.bagit.models
 
 import uk.ac.wellcome.platform.archive.common.verify.Checksum
 
-sealed trait BagLocation {
-  val path: BagPath
-}
-
 case class BagFile(
   checksum: Checksum,
   path: BagPath
-) extends BagLocation
+)
