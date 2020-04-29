835b9605361ce01a6a1d0097e756917d765a820f
Robert Kenny <kenoir@users.noreply.github.com>
2020-04-29 17:45:19 +0100

    Add GET /ingest to ingest_tracker (#491)
    * Add healthcheck and get endpoints
    
    * Apply auto-formatting rules
    
    Co-authored-by: Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>

--- ingests_tracker/src/main/scala/uk/ac/wellcome/platform/storage/ingests_tracker/IngestsTrackerApi.scala
+++ ingests_tracker/src/main/scala/uk/ac/wellcome/platform/storage/ingests_tracker/IngestsTrackerApi.scala
@@ -1,14 +1,22 @@
 package uk.ac.wellcome.platform.storage.ingests_tracker
 
 import akka.actor.ActorSystem
-import uk.ac.wellcome.platform.archive.common.ingests.tracker.IngestTracker
+import uk.ac.wellcome.platform.archive.common.ingests.tracker.{
+  IngestDoesNotExistError,
+  IngestTracker
+}
 import uk.ac.wellcome.typesafe.Runnable
 import akka.http.scaladsl.model._
-import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Directives.{get, _}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
 import grizzled.slf4j.Logging
+import uk.ac.wellcome.platform.archive.common.ingests.models.IngestID
+import uk.ac.wellcome.storage.Identified
+
+import de.heikoseeberger.akkahttpcirce.ErrorAccumulatingCirceSupport._
+import uk.ac.wellcome.json.JsonUtil._
 
 import scala.concurrent.Future
 
@@ -27,9 +35,19 @@ trait IngestsTrackerApi extends Runnable with Logging {
     concat(
       get {
         pathPrefix("ingest" / JavaUUID) { id =>
-          // val result: ingestTracker.Result = ingestTracker.get(IngestID(id))
-
-          complete(StatusCodes.NotFound)
+          ingestTracker.get(IngestID(id)) match {
+            case Left(IngestDoesNotExistError(_)) =>
+              complete(StatusCodes.NotFound)
+            case Left(_) =>
+              complete(StatusCodes.InternalServerError)
+            case Right(Identified(_, ingest)) =>
+              complete(ingest)
+          }
+        }
+      },
+      get {
+        pathPrefix("healthcheck") {
+          complete(StatusCodes.OK)
         }
       }
     )
--- ingests_tracker/src/test/scala/uk/ac/wellcome/platform/storage/ingests_tracker/IngestsTrackerApiFeatureTest.scala
+++ ingests_tracker/src/test/scala/uk/ac/wellcome/platform/storage/ingests_tracker/IngestsTrackerApiFeatureTest.scala
@@ -1,5 +1,7 @@
 package uk.ac.wellcome.platform.storage.ingests_tracker
 
+import java.time.Instant
+
 import akka.http.scaladsl.model.StatusCodes
 import org.scalatest.concurrent.IntegrationPatience
 import org.scalatest.{FunSpec, Matchers}
@@ -9,6 +11,8 @@ import uk.ac.wellcome.platform.archive.common.fixtures.{
   StorageRandomThings
 }
 import uk.ac.wellcome.platform.storage.ingests_tracker.fixtures.IngestsTrackerApiFixture
+import uk.ac.wellcome.json.JsonUtil._
+import uk.ac.wellcome.platform.archive.common.ingests.models.Ingest
 
 class IngestsTrackerApiFeatureTest
     extends FunSpec
@@ -19,12 +23,54 @@ class IngestsTrackerApiFeatureTest
     with IntegrationPatience
     with StorageRandomThings {
 
-  it("starts") {
-    withConfiguredApp() { _ =>
-      val path = s"http://localhost:8080/ingests/id"
+  describe("GET /healthcheck") {
+    it("responds OK") {
+      withConfiguredApp() { _ =>
+        val path = s"http://localhost:8080/healthcheck"
 
-      whenGetRequestReady(path) { result =>
-        result.status shouldBe StatusCodes.NotFound
+        whenGetRequestReady(path) { result =>
+          result.status shouldBe StatusCodes.OK
+        }
+      }
+    }
+  }
+
+  describe("GET /ingest/:id") {
+
+    val ingest = createIngestWith(
+      createdDate = Instant.now(),
+      events = Seq(createIngestEvent, createIngestEvent).sortBy {
+        _.createdDate
+      },
+      version = None
+    )
+
+    val notRecordedIngestId = createIngestID
+
+    it("responds NotFound when there is no ingest") {
+      withConfiguredApp(Seq(ingest)) { _ =>
+        val path = s"http://localhost:8080/ingest/${notRecordedIngestId}"
+
+        whenGetRequestReady(path) { result =>
+          result.status shouldBe StatusCodes.NotFound
+        }
+      }
+    }
+
+    it("responds OK with an Ingest when available") {
+      withConfiguredApp(Seq(ingest)) { _ =>
+        val path = s"http://localhost:8080/ingest/${ingest.id}"
+
+        whenGetRequestReady(path) { result =>
+          result.status shouldBe StatusCodes.OK
+
+          // We are interested in whether we can serialise/de-serialise
+          // what the JSON looks like is not interesting, so we
+          // do not need to test in detail as with the external API
+          withStringEntity(result.entity) { jsonString =>
+            fromJson[Ingest](jsonString).get shouldBe ingest
+          }
+        }
       }
     }
   }
