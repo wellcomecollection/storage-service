1675e43fcf6bcf1c5dc49ed4ef1482ff8584fb17
Alex Chan <alex@alexwlchan.net>
2020-04-30 10:07:13 +0100

    Get a working set of akka-http libraries

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,10 +3,10 @@ import sbt._
 object WellcomeDependencies {
   lazy val versions = new {
     val fixtures = "1.2.0"
-    val json = "2.0.1"
-    val messaging = "9.0.1"
+    val json = "2.3.0"
+    val messaging = "9.1.0"
     val monitoring = "4.0.0"
-    val storage = "8.0.0"
+    val storage = "8.1.0"
     val typesafe = "2.0.0"
   }
 
@@ -63,9 +63,9 @@ object WellcomeDependencies {
 
 object ExternalDependencies {
   lazy val versions = new {
-    val akkaHttp = "10.1.10"
-    val akkaHttpCirce = "1.21.1"
-    val akkaStreamAlpakka = "0.20"
+
+
+
     val apacheLogging = "2.8.2"
     val commonsCompress = "1.5"
     val commonsIO = "2.6"
@@ -79,7 +79,36 @@ object ExternalDependencies {
 
     // This should match the version of circe used in scala-json; see
     // https://github.com/wellcomecollection/scala-json/blob/master/project/Dependencies.scala
-    val circeOptics = "0.11.0"
+    val circeOptics = "0.13.0"
+
+    // Getting the akka-http dependencies right can be fiddly and takes some work.
+    // In particular you need to use the same version of akka-http everywhere, or you
+    // get errors (from LArgeResponsesTest) like:
+    //
+    //      Detected possible incompatible versions on the classpath. Please note that
+    //      a given Akka HTTP version MUST be the same across all modules of Akka HTTP
+    //      that you are using, e.g. if you use [10.1.10] all other modules that are
+    //      released together MUST be of the same version.
+    //
+    //      Make sure you're using a compatible set of libraries.
+    //
+    // To work this out:
+    //
+    //   1. Look at the version of alpakka-streams used by scala-messaging:
+    //      https://github.com/wellcomecollection/scala-messaging/blob/master/project/Dependencies.scala
+    //      (At time of writing, v1.1.2)
+    //
+    //   2. Look at the corresponding akka-http dependency in alpakka:
+    //      https://github.com/akka/alpakka/blob/master/project/Dependencies.scala
+    //      (At time of writing, alpakka v1.1.2 pulls in akka-http 10.1.10)
+    //
+    //   3. Look at versions of akka-http-json.  Browse the Git tags until you find
+    //      one that uses the same version of akka-http and a compatible Circe:
+    //      https://github.com/hseeberger/akka-http-json/blob/master/build.sbt
+    //
+    val akkaStreamAlpakka = "1.1.2"
+    val akkaHttp = "10.1.10"
+    val akkaHttpCirce = "1.29.1"
   }
 
   val logbackDependencies = Seq(
