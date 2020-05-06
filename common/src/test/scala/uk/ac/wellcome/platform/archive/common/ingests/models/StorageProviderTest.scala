aba5c8d37628632d37a44eaf1a71306d71bd9e9a
Alex Chan <alex@alexwlchan.net>
2020-05-06 07:53:22 +0100

    Add some tests for StorageProvider

---
+++ common/src/test/scala/uk/ac/wellcome/platform/archive/common/ingests/models/StorageProviderTest.scala
@@ -0,0 +1,29 @@
+package uk.ac.wellcome.platform.archive.common.ingests.models
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
+
+class StorageProviderTest extends AnyFunSpec with Matchers with TableDrivenPropertyChecks {
+  val providerPairs = Table(
+    ("id", "provider"),
+    ("aws-s3-standard", StandardStorageProvider),
+    ("aws-s3-ia", InfrequentAccessStorageProvider),
+    ("aws-s3-glacier", GlacierStorageProvider),
+  )
+
+  it("creates the correct storage provider from an ID") {
+    forAll(providerPairs) {
+      case (id: String, storageProvider: StorageProvider) =>
+        StorageProvider(id) shouldBe storageProvider
+    }
+  }
+
+  it("throws an error if it gets an unrecognised ID") {
+    val thrown = intercept[IllegalArgumentException] {
+      StorageProvider("not-a-storage-provider")
+    }
+
+    thrown.getMessage shouldBe "Unrecognised storage provider ID: not-a-storage-provider"
+  }
+}
