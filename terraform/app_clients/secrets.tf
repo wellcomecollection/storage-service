e7a2c26c32d301dc93937950f2bf9475dbdc1365
Alex Chan <alex@alexwlchan.net>
2020-05-15 10:39:53 +0100

    Add some credentials for the end-to-end tester

--- terraform/app_clients/secrets.tf
+++ terraform/app_clients/secrets.tf
@@ -23,3 +23,33 @@ resource "aws_secretsmanager_secret_version" "catalogue_client_secret" {
   secret_id     = aws_secretsmanager_secret.catalogue_client_secret.id
   secret_string = module.catalogue_client.secret
 }
+
+# Store the client credentials in KMS for the end-to-end tester
+
+resource "aws_secretsmanager_secret" "end_to_end_client_id" {
+  provider = aws.storage
+
+  name        = "end_to_end_bag_tester/client_id"
+  description = local.description
+}
+
+resource "aws_secretsmanager_secret_version" "end_to_end_client_id" {
+  provider = aws.storage
+
+  secret_id     = aws_secretsmanager_secret.end_to_end_client_id.id
+  secret_string = module.end_to_end_client.id
+}
+
+resource "aws_secretsmanager_secret" "end_to_end_client_secret" {
+  provider = aws.storage
+
+  name        = "end_to_end_bag_tester/client_secret"
+  description = local.description
+}
+
+resource "aws_secretsmanager_secret_version" "end_to_end_client_secret" {
+  provider = aws.storage
+
+  secret_id     = aws_secretsmanager_secret.end_to_end_client_secret.id
+  secret_string = module.end_to_end_client.secret
+}
