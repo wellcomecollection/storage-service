c23045b242784b87f35757b5400f74807827f3b2
Alex Chan <alex@alexwlchan.net>
2020-05-15 07:32:55 +0100

    Use the module for the catalogue client

--- terraform/app_clients/main.tf
+++ terraform/app_clients/main.tf
@@ -2,27 +2,6 @@ locals {
   wc_user_pool_id = "eu-west-1_h3qKmdYyD"
 }
 
-resource "aws_cognito_user_pool_client" "catalogue" {
-  name = "Catalogue"
-
-  user_pool_id = local.wc_user_pool_id
-
-  allowed_oauth_flows = [
-    "client_credentials",
-  ]
-
-  allowed_oauth_flows_user_pool_client = true
-
-  allowed_oauth_scopes = [
-    "https://api.wellcomecollection.org/storage/v1/bags",
-    "https://api.wellcomecollection.org/storage/v1/ingests",
-  ]
-
-  supported_identity_providers = [
-    "COGNITO",
-  ]
-}
-
 module "alex_home_imac_client" {
   source = "../modules/app_client"
 
@@ -30,5 +9,15 @@ module "alex_home_imac_client" {
   user_pool_id = local.wc_user_pool_id
 
   allow_bags_access    = false
+  allow_ingests_access = true
+}
+
+module "catalogue_client" {
+  source = "../modules/app_client"
+
+  name         = "Catalogue"
+  user_pool_id = local.wc_user_pool_id
+
+  allow_bags_access    = true
   allow_ingests_access = true
 }
