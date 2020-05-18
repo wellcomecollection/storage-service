89eae5f7665210f3bd34c92870ccc7bb584916f8
Robert Kenny <r.kenny@wellcome.ac.uk>
2020-05-18 13:38:53 +0100

    bag indexer feature test

--- indexer/bag_indexer/src/main/scala/uk/ac/wellcome/platform/archive/indexer/bags/Main.scala
+++ indexer/bag_indexer/src/main/scala/uk/ac/wellcome/platform/archive/indexer/bags/Main.scala
@@ -7,7 +7,6 @@ import com.typesafe.config.Config
 import software.amazon.awssdk.services.sqs.SqsAsyncClient
 import uk.ac.wellcome.messaging.typesafe.{AlpakkaSqsWorkerConfigBuilder, CloudwatchMonitoringClientBuilder, SQSBuilder}
 import uk.ac.wellcome.messaging.worker.monitoring.metrics.cloudwatch.CloudwatchMetricsMonitoringClient
-import uk.ac.wellcome.platform.archive.common.ingests.models.Ingest
 import uk.ac.wellcome.platform.archive.indexer.bags.models.IndexedStorageManifest
 import uk.ac.wellcome.platform.archive.indexer.elasticsearch.{ElasticsearchIndexCreator, IndexerWorker}
 import uk.ac.wellcome.platform.archive.indexer.elasticsearch.config.ElasticClientBuilder
--- indexer/bag_indexer/src/test/scala/uk/ac/wellcome/platform/archive/indexer/bag/BagIndexerFeatureTest.scala
+++ indexer/bag_indexer/src/test/scala/uk/ac/wellcome/platform/archive/indexer/bag/BagIndexerFeatureTest.scala
@@ -1,10 +1,57 @@
 package uk.ac.wellcome.platform.archive.indexer.bag
 
+import com.sksamuel.elastic4s.Index
+import org.scalatest.EitherValues
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
+import uk.ac.wellcome.platform.archive.common.generators.StorageManifestGenerators
+import uk.ac.wellcome.platform.archive.common.storage.models.StorageManifest
+import uk.ac.wellcome.platform.archive.indexer.bags.{BagIndexer, BagsIndexConfig}
+import uk.ac.wellcome.platform.archive.indexer.bags.models.IndexedStorageManifest
+import uk.ac.wellcome.platform.archive.indexer.elasticsearch.Indexer
+import uk.ac.wellcome.platform.archive.indexer.fixtures.IndexerFixtures
+import uk.ac.wellcome.json.JsonUtil._
 
-class BagIndexerFeatureTest extends AnyFunSpec with Matchers {
-  it("asserts truth") {
-    true shouldBe true
-  }
+import scala.concurrent.ExecutionContext.Implicits.global
+
+
+class BagIndexerFeatureTest
+  extends AnyFunSpec
+  with Matchers
+  with EitherValues
+  with IndexerFixtures[StorageManifest, IndexedStorageManifest]
+  with StorageManifestGenerators {
+
+    def convertIndexedT(t: StorageManifest): IndexedStorageManifest = IndexedStorageManifest(t)
+    def createT: (StorageManifest, String) = {
+      val storageManifest = createStorageManifest
+
+      (storageManifest, storageManifest.id.toString)
+    }
+
+    override def createIndexer(index: Index): Indexer[StorageManifest, IndexedStorageManifest] =
+      new BagIndexer(
+        client = elasticClient,
+        index = index
+      )
+
+    it("processes a single message") {
+      withLocalElasticsearchIndex(BagsIndexConfig.mapping) { index =>
+        withLocalSqsQueue() { queue =>
+          withIndexerWorker(index, queue) { worker =>
+            val (t, id) = createT
+
+            worker.run()
+
+            sendNotificationToSQS(queue, t)
+
+            eventually {
+              val storedIndexedT = getT[IndexedStorageManifest](index, id)
+
+             storedIndexedT shouldBe convertIndexedT(t)
+            }
+          }
+        }
+      }
+    }
 }
