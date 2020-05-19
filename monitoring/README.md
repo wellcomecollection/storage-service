0aa1b87377dd41188c3e633e37aada3e90c51d97
Alex Chan <alex@alexwlchan.net>
2020-05-19 09:15:22 +0100

    Remove automatic runs for now

--- monitoring/README.md
+++ monitoring/README.md
@@ -3,6 +3,6 @@
 This directory contains some stuff for monitoring the storage service.
 
 *   `end_to_end_bag_test` is a Lambda that sends a new bag for ingest into the storage service.
-    It runs once a day, or on demand, as a check that the storage service is behaving correctly.
+    It can be run on demand as a check that the storage service is behaving correctly.
 
 *   `test_bags` are some tiny bags to use when testing the storage service.
--- terraform/monitoring/end_to_end_test_lambda/cloudwatch_trigger.tf
+++ terraform/monitoring/end_to_end_test_lambda/cloudwatch_trigger.tf
@@ -1,3 +1,7 @@
+# This allows you to set up a rule that triggers a new bag ingest every day
+# at 9pm.  It's currently disabled, but you can enable it by changing
+# `count` to 1.
+
 resource "aws_cloudwatch_event_rule" "every_day_at_9pm" {
   name                = "trigger-${var.name}"
   schedule_expression = "cron(* 21 * * ? *)"
@@ -12,7 +16,8 @@ resource "aws_lambda_permission" "allow_cloudwatch_trigger" {
 }
 
 resource "aws_cloudwatch_event_target" "event_trigger" {
+  count = 0
+
   rule  = aws_cloudwatch_event_rule.every_day_at_9pm.name
   arn   = module.lambda.arn
 }
-
