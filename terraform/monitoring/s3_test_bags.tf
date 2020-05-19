47803f51e509e94bb2d76edd59aac0ee6fd4d523
Alex Chan <alex@alexwlchan.net>
2020-05-19 07:43:51 +0100

    Let the bag unpacker read the test bag

--- terraform/monitoring/s3_test_bags.tf
+++ terraform/monitoring/s3_test_bags.tf
@@ -1,3 +1,7 @@
+# Upload a series of test bags to an S3 bucket.  We manage the bags in Git and
+# Terraform rather than by hand, so we can track changes and know what version
+# of a test bag was in use at a given time.
+
 locals {
   infra_bucket    = "wellcomecollection-storage-infra"
   test_bag_prefix = "test_bags/"
@@ -9,4 +13,23 @@ resource "aws_s3_bucket_object" "bag_with_one_text_file" {
   source = "${path.module}/../../monitoring/test_bags/bag_with_one_text_file.tar.gz"
 
   etag = filemd5("${path.module}/../../monitoring/test_bags/bag_with_one_text_file.tar.gz")
+}
+
+# Give the unpacker tasks permission to read all of the test bags.
+
+data "aws_iam_policy_document" "read_test_bags" {
+  statement {
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${local.infra_bucket}/${local.test_bag_prefix}*"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "allow_staging_read_test_bags" {
+  role   = data.terraform_remote_state.stack_staging.outputs.unpacker_task_role_name
+  policy = data.aws_iam_policy_document.read_test_bags.json
 }
--- terraform/monitoring/terraform.tf
+++ terraform/monitoring/terraform.tf
@@ -8,3 +8,14 @@ terraform {
     region         = "eu-west-1"
   }
 }
+
+data "terraform_remote_state" "stack_staging" {
+  backend = "s3"
+
+  config = {
+    role_arn = "arn:aws:iam::975596993436:role/storage-read_only"
+    bucket   = "wellcomecollection-storage-infra"
+    key      = "terraform/storage-service/stack_staging.tfstate"
+    region   = "eu-west-1"
+  }
+}
