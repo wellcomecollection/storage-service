fdc59de77b9ede519450e41e383f4a93e2da145a
Alex Chan <alex@alexwlchan.net>
2020-05-21 14:07:48 +0100

    Fix the tests in the display library

--- display/src/test/scala/uk/ac/wellcome/platform/archive/display/DisplayProviderTest.scala
+++ display/src/test/scala/uk/ac/wellcome/platform/archive/display/DisplayProviderTest.scala
@@ -15,13 +15,19 @@ class DisplayProviderTest
 
   val providerPairs = Table(
     ("display", "internal"),
-    (DisplayProvider("aws-s3-standard"), StandardStorageProvider),
-    (DisplayProvider("aws-s3-ia"), InfrequentAccessStorageProvider),
-    (DisplayProvider("aws-s3-glacier"), GlacierStorageProvider)
+    (DisplayProvider("amazon-s3"), AmazonS3StorageProvider),
+    (DisplayProvider("azure-blob-storage"), AzureBlobStorageProvider),
+  )
+
+  val deprecatedProviderPairs = Table(
+    ("display", "internal"),
+    (DisplayProvider("aws-s3-standard"), AmazonS3StorageProvider),
+    (DisplayProvider("aws-s3-ia"), AmazonS3StorageProvider),
+    (DisplayProvider("aws-s3-glacier"), AmazonS3StorageProvider),
   )
 
   it("turns a DisplayProvider into a StorageProvider") {
-    forAll(providerPairs) {
+    forAll(providerPairs ++ deprecatedProviderPairs) {
       case (
           displayProvider: DisplayProvider,
           storageProvider: StorageProvider
@@ -49,25 +55,21 @@ class DisplayProviderTest
 
     thrown.getMessage shouldBe (
       "Unrecognised storage provider ID: not-a-storage-provider; " +
-        "valid values are: aws-s3-standard, aws-s3-ia, aws-s3-glacier"
+        "valid values are: amazon-s3, azure-blob-storage"
     )
   }
 
   describe("JSON encoding/decoding") {
     val jsonPairs = Table(
-      ("provider", "json"),
+      ("provider", "jsonString"),
       (
-        DisplayProvider("aws-s3-standard"),
-        """{"id": "aws-s3-standard", "type": "Provider"}"""
+        DisplayProvider(AmazonS3StorageProvider),
+        """{"id": "amazon-s3", "type": "Provider"}""",
       ),
       (
-        DisplayProvider("aws-s3-ia"),
-        """{"id": "aws-s3-ia", "type": "Provider"}"""
+        DisplayProvider(AzureBlobStorageProvider),
+        """{"id": "azure-blob-storage", "type": "Provider"}""",
       ),
-      (
-        DisplayProvider("aws-s3-glacier"),
-        """{"id": "aws-s3-glacier", "type": "Provider"}"""
-      )
     )
 
     it("decodes correctly") {
@@ -84,6 +86,19 @@ class DisplayProviderTest
             toJson[DisplayProvider](displayProvider).get,
             jsonString
           )
+      }
+    }
+
+    val deprecatedJsonIds = Table(
+      "json",
+      """{"id": "aws-s3-standard", "type": "Provider"}""",
+      """{"id": "aws-s3-ia", "type": "Provider"}""",
+      """{"id": "aws-s3-glacier", "type": "Provider"}""",
+    )
+
+    it("decodes deprecated storage IDs") {
+      forAll(deprecatedJsonIds) { jsonString =>
+        fromJson[DisplayProvider](jsonString).get.toStorageProvider shouldBe AmazonS3StorageProvider
       }
     }
   }
