f933bb53e95503f10f9f2eb6773521c1595fd721
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2020-05-28 08:38:00 +0000

    Apply auto-formatting rules

--- indexer/bag_indexer/src/test/scala/uk/ac/wellcome/platform/archive/indexer/bag/services/FileSuffixCounterTest.scala
+++ indexer/bag_indexer/src/test/scala/uk/ac/wellcome/platform/archive/indexer/bag/services/FileSuffixCounterTest.scala
@@ -2,7 +2,10 @@ package uk.ac.wellcome.platform.archive.indexer.bag.services
 
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
-import uk.ac.wellcome.platform.archive.common.generators.{IngestGenerators, StorageManifestGenerators}
+import uk.ac.wellcome.platform.archive.common.generators.{
+  IngestGenerators,
+  StorageManifestGenerators
+}
 import uk.ac.wellcome.platform.archive.indexer.bags.services.FileSuffixCounter
 
 class FileSuffixCounterTest
@@ -11,7 +14,15 @@ class FileSuffixCounterTest
     with IngestGenerators
     with StorageManifestGenerators {
 
-  val names = Seq("foo.TXT", "bar.txt", "bat.png", "mystery file", "bad..bmp", "worse.jif.", "file.with.full.stops.scala")
+  val names = Seq(
+    "foo.TXT",
+    "bar.txt",
+    "bat.png",
+    "mystery file",
+    "bad..bmp",
+    "worse.jif.",
+    "file.with.full.stops.scala"
+  )
   val files = names.map { createStorageManifestFileWith(_) }
 
   it("tallies the file suffixes correctly") {
