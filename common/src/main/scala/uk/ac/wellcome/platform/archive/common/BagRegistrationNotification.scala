4da600b6d22c4a00928b3a174d5d47abac2a7991
Alex Chan <alex@alexwlchan.net>
2020-06-03 11:36:18 +0100

    Use a string to store the bag version

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/BagRegistrationNotification.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/BagRegistrationNotification.scala
@@ -6,12 +6,25 @@ import uk.ac.wellcome.platform.archive.common.storage.models.StorageSpace
 
 /** This notification is sent by the storage service to notify another system
   * (which may be entirely separate from the storage service, e.g. the catalogue)
-  * that a bag has been registered.
+  * that a bag has been registered.  You should be able to use this information to
+  * get a bag from the external API.
+  *
+  * We encode a version as a string (v1, v2, v3) rather than a number because that's
+  * what the external API expects.
   *
   */
 case class BagRegistrationNotification(
   space: StorageSpace,
   externalIdentifier: ExternalIdentifier,
-  version: BagVersion,
+  version: String,
   @JsonKey("type") ontologyType: String = "RegisteredBagNotification"
 )
+
+case object BagRegistrationNotification {
+  def apply(space: StorageSpace, externalIdentifier: ExternalIdentifier, version: BagVersion): BagRegistrationNotification =
+    BagRegistrationNotification(
+      space = space,
+      externalIdentifier = externalIdentifier,
+      version = version.toString
+    )
+}
