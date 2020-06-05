edc22bcb3635b2dfd1555402878b29fec6e75f0c
Alex Chan <alex@alexwlchan.net>
2020-06-05 15:43:05 +0100

    Remove an unused object

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/verify/Verifiable.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/verify/Verifiable.scala
@@ -4,15 +4,6 @@ trait Verifiable[T] {
   def create(t: T): Either[VerifiableGenerationFailure, Seq[VerifiableLocation]]
 }
 
-object Verifiable {
-  implicit class Convert[T](t: T)(implicit verifiable: Verifiable[T]) {
-    def toVerifiable
-      : Either[VerifiableGenerationFailure, Seq[VerifiableLocation]] = {
-      verifiable.create(t)
-    }
-  }
-}
-
 sealed trait VerifiableGenerationFailure {
   val msg: String
 }
