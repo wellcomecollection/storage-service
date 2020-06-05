19ed6202913f1828e688533dd2db58df7a495179
Robert Kenny <r.kenny@wellcome.ac.uk>
2020-06-05 10:41:17 +0100

    Update reindex bag script to new format

--- indexer/reindex_bags_backlog.py
+++ indexer/reindex_bags_backlog.py
@@ -68,29 +68,22 @@ def create_client(service_name, role_arn):
     return session.client(service_name)
 
 
-# The bag indexer only cares about space, externalIdentifier & version
-def fake_known_replicas_payload(space, externalIdentifier, version):
-    return {
-        "context": {
-            "ingestId": "8368b576-8206-4552-870c-005186f0264a",
-            "ingestType": {"id": "create"},
-            "storageSpace": space,
-            "ingestDate": "2069-11-30T03:09:31.986Z",
-            "externalIdentifier": externalIdentifier,
-        },
-        "version": version,
-        "knownReplicas": {
-            "location": {
-                "provider": {"type": "AmazonS3StorageProvider"},
-                "prefix": {
-                    "namespace": f"sent by {__file__}",
-                    "path": f"sent by {__file__}",
-                },
-            },
-            "replicas": [],
-        },
-    }
+# case class BagRegistrationNotification(
+#   space: StorageSpace,
+#   externalIdentifier: ExternalIdentifier,
+#   version: String,
+#   @JsonKey("type") ontologyType: String = "RegisteredBagNotification"
+# )
 
+
+# The bag indexer only cares about space, externalIdentifier & version
+def fake_notification(space, externalIdentifier, version):
+    return {
+        "space": space,
+        "externalIdentifier": externalIdentifier,
+        "version": f"v{version}",
+        "type:": "RegisteredBagNotification"
+    }
 
 def get_total_bags(dynamodb_client, table_name):
     resp = dynamodb_client.describe_table(TableName=table_name)
@@ -129,7 +122,7 @@ def publish_bags(sns_client, topic_arn, bags, dry_run=False):
     payloads = []
     for (dynamo_id, version) in tqdm(bags.items(), total=unique_bags):
         space, external_id = dynamo_id.split("/", 1)
-        payload = fake_known_replicas_payload(space, external_id, version)
+        payload = fake_notification(space, external_id, version)
         payloads.append(payload)
 
     print(f"Prepared notifications for {len(payloads)} bags.\n")
