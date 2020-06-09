36fbc3367bad78f599fd6d9749d0352500a8cb43
Alex Chan <alex@alexwlchan.net>
2020-06-09 08:18:06 +0100

    Add a test for the new FileFixityCouldNotGetChecksum error
    Testing with coverage showed that we were never checking the case where
    we couldn't get a checksum from an InputStream.  This test checks we're
    handling this test correctly.

--- bag_verifier/src/test/scala/uk/ac/wellcome/platform/archive/bagverifier/fixity/FixityCheckerTests.scala
+++ bag_verifier/src/test/scala/uk/ac/wellcome/platform/archive/bagverifier/fixity/FixityCheckerTests.scala
@@ -1,5 +1,6 @@
 package uk.ac.wellcome.platform.archive.bagverifier.fixity
 
+import java.io.FilterInputStream
 import java.net.URI
 
 import org.scalatest.funspec.AnyFunSpec
@@ -7,8 +8,9 @@ import org.scalatest.matchers.should.Matchers
 import uk.ac.wellcome.platform.archive.bagverifier.fixity.memory.MemoryFixityChecker
 import uk.ac.wellcome.platform.archive.bagverifier.generators.FixityGenerators
 import uk.ac.wellcome.platform.archive.common.storage.{LocateFailure, LocationParsingError}
-import uk.ac.wellcome.storage.ObjectLocation
-import uk.ac.wellcome.storage.store.memory.MemoryStreamStore
+import uk.ac.wellcome.storage.{Identified, ObjectLocation}
+import uk.ac.wellcome.storage.store.memory.{MemoryStore, MemoryStreamStore}
+import uk.ac.wellcome.storage.streaming.InputStreamWithLength
 
 class FixityCheckerTests extends AnyFunSpec with Matchers with FixityGenerators {
   override def resolve(location: ObjectLocation): URI =
@@ -25,6 +27,33 @@ class FixityCheckerTests extends AnyFunSpec with Matchers with FixityGenerators 
 
       val expectedFileFixity = createExpectedFileFixity
       brokenChecker.check(expectedFileFixity) shouldBe a[FileFixityCouldNotRead]
+    }
+
+    it("handles an error when trying to checksum the object") {
+      val badStream = new FilterInputStream(randomInputStream()) {
+        override def read(b: Array[Byte], off: Int, len: Int): Int =
+          throw new Throwable("BOOM!")
+      }
+
+      val closedStream = new InputStreamWithLength(
+        badStream,
+        length = randomInt(from = 1, to = 10)
+      )
+
+      closedStream.close()
+
+      val streamStore = new MemoryStreamStore[ObjectLocation](
+        memoryStore = new MemoryStore[ObjectLocation, Array[Byte]](initialEntries = Map.empty)
+      ) {
+        override def get(location: ObjectLocation): this.ReadEither =
+          Right(Identified(location, closedStream))
+      }
+
+      val expectedFileFixity = createExpectedFileFixityWith(length = None)
+
+      val checker = new MemoryFixityChecker(streamStore)
+
+      checker.check(expectedFileFixity) shouldBe a[FileFixityCouldNotGetChecksum]
     }
   }
 }
