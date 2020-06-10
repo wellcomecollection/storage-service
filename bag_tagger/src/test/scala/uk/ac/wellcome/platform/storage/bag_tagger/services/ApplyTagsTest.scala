22fae196f721bd2c4a8bdef0c3287c33b99393c6
Alex Chan <alex@alexwlchan.net>
2020-06-10 12:24:26 +0100

    Start filling out the test cases

--- bag_tagger/src/test/scala/uk/ac/wellcome/platform/storage/bag_tagger/services/ApplyTagsTest.scala
+++ bag_tagger/src/test/scala/uk/ac/wellcome/platform/storage/bag_tagger/services/ApplyTagsTest.scala
@@ -1,11 +1,51 @@
 package uk.ac.wellcome.platform.storage.bag_tagger.services
 
+import org.scalatest.TryValues
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
+import uk.ac.wellcome.platform.archive.common.generators.StorageManifestGenerators
+import uk.ac.wellcome.platform.archive.common.ingests.models.AmazonS3StorageProvider
+import uk.ac.wellcome.platform.archive.common.storage.models.PrimaryStorageLocation
+import uk.ac.wellcome.storage.fixtures.S3Fixtures
+import uk.ac.wellcome.storage.tags.s3.S3Tags
 
-class ApplyTagsTest extends AnyFunSpec with Matchers {
-  describe("it applies tags") {
+import scala.util.Success
+
+class ApplyTagsTest extends AnyFunSpec with Matchers with TryValues with S3Fixtures with StorageManifestGenerators {
+  val s3Tags = new S3Tags()
+  val applyTags = new ApplyTags(s3Tags = s3Tags)
+
+  describe("it updates tags") {
     it("to objects in S3") {
+      withLocalS3Bucket { bucket =>
+        val prefix = createObjectLocationPrefixWith(bucket.name)
+
+        val file = createStorageManifestFileWith(
+          pathPrefix = "space/externalIdentifier/v1",
+          name = "b1234.mxf"
+        )
+
+        val location = prefix.asLocation(file.path)
+
+        s3Client.putObject(location.namespace, location.path, "<MXF file contents>")
+
+        val result = applyTags.applyTags(
+          storageLocations = Seq(
+            PrimaryStorageLocation(
+              provider = AmazonS3StorageProvider,
+              prefix = prefix
+            )
+          ),
+          tagsToApply = Map(file -> Map("Content-Type" -> "application/mxf"))
+        )
+
+        result shouldBe Success(())
+
+        s3Tags.get(location).right.value shouldBe Map("Content-Type" -> "application/mxf")
+      }
+    }
+
+    it("by appending to the existing tags") {
       true shouldBe false
     }
   }
