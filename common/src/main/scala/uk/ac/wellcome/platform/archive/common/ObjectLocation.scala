753e0ef2b22f17f6e132424832d8009b305e5a2d
Alex Chan <alex@alexwlchan.net>
2020-06-22 21:44:34 +0100

    Add a backport of ObjectLocation
    This will eventually be removed, but it means the codebase will compile
    and I can _gradually_ migrate to the new classes.

---
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/ObjectLocation.scala
@@ -0,0 +1,24 @@
+package uk.ac.wellcome.storage
+
+import java.nio.file.Paths
+
+case class ObjectLocation(namespace: String, path: String) {
+  override def toString = s"$namespace/$path"
+
+  def join(parts: String*): ObjectLocation = this.copy(
+    path = Paths.get(this.path, parts: _*).normalize().toString
+  )
+
+  def asPrefix: ObjectLocationPrefix =
+    ObjectLocationPrefix(
+      namespace = namespace,
+      path = path
+    )
+}
+
+case class ObjectLocationPrefix(namespace: String, path: String) {
+  override def toString = s"$namespace/$path"
+
+  def asLocation(parts: String*): ObjectLocation =
+    ObjectLocation(namespace, path).join(parts: _*)
+}
