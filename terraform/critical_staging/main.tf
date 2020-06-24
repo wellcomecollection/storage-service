2f2ec036dde1a7c3f109c2f16e671f5f55fe7092
Robert Kenny <r.kenny@wellcome.ac.uk>
2020-06-24 17:01:15 +0100

    example migration script

--- terraform/critical_staging/main.tf
+++ terraform/critical_staging/main.tf
@@ -29,3 +29,23 @@ module "critical" {
   # we don't need S3 versioning.
   enable_s3_versioning = false
 }
+
+resource "aws_dynamodb_table" "new_vhs_table" {
+  name             = "vhs-storage-staging-manifests-test"
+  hash_key         = "id"
+  range_key        = "version"
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
+
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "version"
+    type = "N"
+  }
+}
