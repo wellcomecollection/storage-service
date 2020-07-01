ddfd88e3e71ca44aa4b28816a05afe8b45a6335a
Alex Chan <alex@alexwlchan.net>
2020-07-01 13:10:34 +0100

    Get the tests in the notifier compiling again

--- notifier/src/test/scala/uk/ac/wellcome/platform/archive/notifier/services/CallbackUrlServiceTest.scala
+++ notifier/src/test/scala/uk/ac/wellcome/platform/archive/notifier/services/CallbackUrlServiceTest.scala
@@ -122,8 +122,8 @@ class CallbackUrlServiceTest
              |      "type": "Provider",
              |      "id": "amazon-s3"
              |    },
-             |    "bucket": "${ingest.sourceLocation.location.namespace}",
-             |    "path": "${ingest.sourceLocation.location.path}"
+             |    "bucket": "${ingest.sourceLocation.prefix.namespace}",
+             |    "path": "${ingest.sourceLocation.prefix.path}"
              |  },
              |  "callback": {
              |    "type": "Callback",
