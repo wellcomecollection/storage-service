5a1784f7b04f923252fdce68a53b5f074b2753ab
Alex Chan <alex@alexwlchan.net>
2020-07-15 10:38:30 +0100

    Remove some unneeded Terraform interpolation quotes

--- terraform/modules/critical/outputs.tf
+++ terraform/modules/critical/outputs.tf
@@ -1,59 +1,59 @@
 # Storage manifests VHS
 
 output "vhs_manifests_bucket_name" {
-  value = "${module.vhs_manifests.bucket_name}"
+  value = module.vhs_manifests.bucket_name
 }
 
 output "vhs_manifests_bucket_arn" {
-  value = "${module.vhs_manifests.bucket_arn}"
+  value = module.vhs_manifests.bucket_arn
 }
 
 output "vhs_manifests_table_name" {
-  value = "${module.vhs_manifests.table_name}"
+  value = module.vhs_manifests.table_name
 }
 
 output "vhs_manifests_table_arn" {
-  value = "${module.vhs_manifests.table_arn}"
+  value = module.vhs_manifests.table_arn
 }
 
 output "vhs_manifests_readonly_policy" {
-  value = "${module.vhs_manifests.read_policy}"
+  value = module.vhs_manifests.read_policy
 }
 
 output "vhs_manifests_readwrite_policy" {
-  value = "${module.vhs_manifests.full_access_policy}"
+  value = module.vhs_manifests.full_access_policy
 }
 
 #
 
 output "ingests_table_name" {
-  value = "${aws_dynamodb_table.ingests.name}"
+  value = aws_dynamodb_table.ingests.name
 }
 
 output "ingests_table_arn" {
-  value = "${aws_dynamodb_table.ingests.arn}"
+  value = aws_dynamodb_table.ingests.arn
 }
 
 output "static_content_bucket_name" {
-  value = "${aws_s3_bucket.static_content.bucket}"
+  value = aws_s3_bucket.static_content.bucket
 }
 
 # Replica buckets
 
 output "replica_primary_bucket_name" {
-  value = "${aws_s3_bucket.replica_primary.bucket}"
+  value = aws_s3_bucket.replica_primary.bucket
 }
 
 output "replica_primary_bucket_arn" {
-  value = "${aws_s3_bucket.replica_primary.arn}"
+  value = aws_s3_bucket.replica_primary.arn
 }
 
 output "replica_glacier_bucket_name" {
-  value = "${aws_s3_bucket.replica_glacier.bucket}"
+  value = aws_s3_bucket.replica_glacier.bucket
 }
 
 output "replica_glacier_bucket_arn" {
-  value = "${aws_s3_bucket.replica_glacier.arn}"
+  value = aws_s3_bucket.replica_glacier.arn
 }
 
 output "replica_azure_container_name" {
@@ -63,21 +63,21 @@ output "replica_azure_container_name" {
 # DynamoDB tables
 
 output "replicas_table_arn" {
-  value = "${aws_dynamodb_table.replicas_table.arn}"
+  value = aws_dynamodb_table.replicas_table.arn
 }
 
 output "replicas_table_name" {
-  value = "${aws_dynamodb_table.replicas_table.name}"
+  value = aws_dynamodb_table.replicas_table.name
 }
 
 output "versions_table_arn" {
-  value = "${aws_dynamodb_table.versioner_versions_table.arn}"
+  value = aws_dynamodb_table.versioner_versions_table.arn
 }
 
 output "versions_table_name" {
-  value = "${aws_dynamodb_table.versioner_versions_table.name}"
+  value = aws_dynamodb_table.versioner_versions_table.name
 }
 
 output "versions_table_index" {
-  value = "${local.versioner_versions_table_index}"
+  value = local.versioner_versions_table_index
 }
