485f1ac6787bae3948e4832c2b47e9f37c47c29e
Alex Chan <alex@alexwlchan.net>
2020-07-22 13:10:37 +0100

    Do away with BagReplicationRequest

--- bag_replicator/src/main/scala/uk/ac/wellcome/platform/archive/bagreplicator/models/ReplicaType.scala
+++ bag_replicator/src/main/scala/uk/ac/wellcome/platform/archive/bagreplicator/models/ReplicaType.scala
@@ -1,5 +1,14 @@
 package uk.ac.wellcome.platform.archive.bagreplicator.models
 
+// We distinguish between primary and secondary replicas.
+//
+//  - The primary replica is the "warm" copy, intended for frequent access
+//    e.g. an S3 bucket with Standard-IA
+//
+//  - Secondary replicas are the "cold" copies, that give us an extra layer
+//    of backup but aren't intended for frequent access
+//    e.g. an S3 Glacier bucket
+
 sealed trait ReplicaType
 
 case object PrimaryReplica extends ReplicaType
