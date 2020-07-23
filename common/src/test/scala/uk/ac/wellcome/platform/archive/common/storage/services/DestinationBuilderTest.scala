d504fc925852877bbf2e321e5939814860093fac
Alex Chan <alex@alexwlchan.net>
2020-07-23 09:24:29 +0100

    Remove an unused buildDestination method in DestinationBuilder

--- common/src/test/scala/uk/ac/wellcome/platform/archive/common/storage/services/DestinationBuilderTest.scala
+++ common/src/test/scala/uk/ac/wellcome/platform/archive/common/storage/services/DestinationBuilderTest.scala
@@ -15,49 +15,28 @@ class DestinationBuilderTest
     with ExternalIdentifierGenerators
     with StorageSpaceGenerators {
 
-  def createNamespace: String =
-    randomAlphanumeric
-
-  it("uses the given namespace") {
-    val namespace = createNamespace
-
-    val location = DestinationBuilder.buildDestination(
-      namespace = namespace,
-      storageSpace = createStorageSpace,
-      externalIdentifier = createExternalIdentifier,
-      version = createBagVersion
-    )
-
-    location.namespace shouldBe namespace
-  }
-
   it("constructs the correct path") {
-    val namespace = createNamespace
-
     val storageSpace = createStorageSpace
     val externalIdentifier = createExternalIdentifier
     val version = createBagVersion
 
-    val location = DestinationBuilder.buildDestination(
-      namespace = namespace,
+    val path = DestinationBuilder.buildPath(
       storageSpace = storageSpace,
       externalIdentifier = externalIdentifier,
       version = version
     )
 
-    location.path shouldBe s"${storageSpace.underlying}/${externalIdentifier.toString}/$version"
+    path shouldBe s"${storageSpace.underlying}/${externalIdentifier.toString}/$version"
   }
 
   it("uses slashes in the external identifier to build a hierarchy") {
-    val namespace = createNamespace
     val version = createBagVersion
-    val location = DestinationBuilder.buildDestination(
-      namespace = namespace,
+    val path = DestinationBuilder.buildPath(
       storageSpace = StorageSpace("alfa"),
       externalIdentifier = ExternalIdentifier("bravo/charlie"),
       version = version
     )
 
-    location.path shouldBe s"alfa/bravo/charlie/$version"
+    path shouldBe s"alfa/bravo/charlie/$version"
   }
 }
