e1b36e50f20bf71ac6367b2e8e345726d2e302f4
Alex Chan <alex@alexwlchan.net>
2020-07-24 15:31:42 +0100

    Remove the vendored storage lib

--- common/src/main/scala/uk/ac/wellcome/storage/listing/s3/NewS3Listing.scala
+++
@@ -1,7 +0,0 @@
-package uk.ac.wellcome.storage.listing.s3
-
-import uk.ac.wellcome.storage.S3ObjectLocationPrefix
-import uk.ac.wellcome.storage.listing.Listing
-
-trait NewS3Listing[ListingResult]
-    extends Listing[S3ObjectLocationPrefix, ListingResult]
