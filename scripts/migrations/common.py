91f66fafb0456238e94792fcfdb540db8c545d4e
Alex Chan <alex@alexwlchan.net>
2020-07-24 09:49:41 +0100

    Add a script for migrating the SourceLocation objects

--- scripts/migrations/common.py
+++ scripts/migrations/common.py
@@ -34,35 +34,28 @@ def scan_table(*, TableName, **kwargs):
         yield from page["Items"]
 
 
-def get_aws_resource(resource, role_arn):
-    # Taken from https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_use_switch-role-api.html
+sts_client = boto3.client("sts")
 
-    # The calls to AWS STS AssumeRole must be signed with the access key ID
-    # and secret access key of an existing IAM user or by using existing temporary
-    # credentials such as those from another role. (You cannot call AssumeRole
-    # with the access key for the root account.) The credentials can be in
-    # environment variables or in a configuration file and will be discovered
-    # automatically by the boto3.client() function. For more information, see the
-    # Python SDK documentation:
-    # http://boto3.readthedocs.io/en/latest/reference/services/sts.html#client
 
-    # create an STS client object that represents a live connection to the
-    # STS service
-    sts_client = boto3.client("sts")
-
-    # Call the assume_role method of the STSConnection object and pass the role
-    # ARN and a role session name.
+def get_aws_resource(resource, *, role_arn):
     assumed_role_object = sts_client.assume_role(
         RoleArn=role_arn, RoleSessionName="AssumeRoleSession1"
     )
-
-    # From the response that contains the assumed role, get the temporary
-    # credentials that can be used to make subsequent API calls
     credentials = assumed_role_object["Credentials"]
-
-    # Use the temporary credentials that AssumeRole returns to make a
-    # connection to Amazon S3
     return boto3.resource(
+        resource,
+        aws_access_key_id=credentials["AccessKeyId"],
+        aws_secret_access_key=credentials["SecretAccessKey"],
+        aws_session_token=credentials["SessionToken"],
+    )
+
+
+def get_aws_client(resource, *, role_arn):
+    assumed_role_object = sts_client.assume_role(
+        RoleArn=role_arn, RoleSessionName="AssumeRoleSession1"
+    )
+    credentials = assumed_role_object["Credentials"]
+    return boto3.client(
         resource,
         aws_access_key_id=credentials["AccessKeyId"],
         aws_secret_access_key=credentials["SecretAccessKey"],
