241c36837bcda45ffab60ca6eb98ca178853ebbd
Donald Gray <donald.gray@digirati.com>
2020-07-29 09:56:27 +0100

    Grant s3 access to digirati account, rather than roles

--- terraform/modules/critical/s3_replica_primary.tf
+++ terraform/modules/critical/s3_replica_primary.tf
@@ -93,11 +93,6 @@ data "aws_iam_policy_document" "replica_primary_read" {
         # Beta version of the DLCS orchestrator.
         # See https://wellcome.slack.com/archives/CBT40CMKQ/p1573742247457800
         "AROAZQI22QHWTHLN4QHJU:*",
-
-        # Dashboard for iiif-builder staging + staging-prd + prod
-        "AROAZQI22QHW3RRRIYDN3:*",
-        "AROAZQI22QHWQI2FAIMQ5:*",
-        "AROAZQI22QHWZ755Z5O5K:*",
       ]
     }
 
