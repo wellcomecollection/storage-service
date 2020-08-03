2913a313f77de3a2667a4a6df7e38f34dbece909
alicefuzier <alicefuzier@gmail.com>
2020-08-03 12:10:56 +0100

    Add AzureBagReader

---
+++ common/src/test/scala/uk/ac/wellcome/platform/archive/common/bagit/services/azure/AzureBagReaderTest.scala
@@ -0,0 +1,36 @@
+package uk.ac.wellcome.platform.archive.common.bagit.services.azure
+
+import uk.ac.wellcome.fixtures.TestWith
+import uk.ac.wellcome.platform.archive.common.bagit.services.{BagReader, BagReaderTestCases}
+import uk.ac.wellcome.platform.archive.common.fixtures.azure.AzureBagBuilder
+import uk.ac.wellcome.storage.azure.{AzureBlobLocation, AzureBlobLocationPrefix}
+import uk.ac.wellcome.storage.fixtures.AzureFixtures.Container
+import uk.ac.wellcome.storage.store.TypedStore
+import uk.ac.wellcome.storage.store.azure.{AzureStreamStore, AzureTypedStore}
+
+class AzureBagReaderTest extends BagReaderTestCases[
+  Unit,
+  Container,
+  AzureBlobLocation,
+  AzureBlobLocationPrefix
+]
+  with AzureBagBuilder {
+  override def asLocation(root: AzureBlobLocationPrefix, path: String): AzureBlobLocation = root.asLocation(path)
+
+  override def withContext[R](testWith: TestWith[Unit, R]): R = testWith(())
+
+  override def withTypedStore[R](testWith: TestWith[TypedStore[AzureBlobLocation, String], R])(implicit context: Unit): R = {
+    implicit val streamStore = new AzureStreamStore()
+    testWith(new AzureTypedStore[String]())
+  }
+
+  override def withBagReader[R](testWith: TestWith[BagReader[AzureBlobLocation, AzureBlobLocationPrefix], R])(implicit context: Unit): R = testWith(new AzureBagReader())
+
+  override def withNamespace[R](testWith: TestWith[Container, R]): R = withAzureContainer{ container =>
+    testWith(container)
+  }
+
+  override def deleteFile(root: AzureBlobLocationPrefix, path: String)(implicit context: Unit): Unit = {
+    azureClient.getBlobContainerClient(root.container).getBlobClient(s"${root.namePrefix}/$path").delete()
+  }
+}
---
+++ common/src/test/scala/uk/ac/wellcome/platform/archive/common/fixtures/azure/AzureBagBuilder.scala
@@ -0,0 +1,24 @@
+package uk.ac.wellcome.platform.archive.common.fixtures.azure
+
+import uk.ac.wellcome.platform.archive.common.bagit.models.{BagVersion, ExternalIdentifier}
+import uk.ac.wellcome.platform.archive.common.fixtures.{BagBuilder, PayloadEntry}
+import uk.ac.wellcome.platform.archive.common.storage.models.StorageSpace
+import uk.ac.wellcome.storage.azure.{AzureBlobLocation, AzureBlobLocationPrefix}
+import uk.ac.wellcome.storage.fixtures.AzureFixtures
+import uk.ac.wellcome.storage.fixtures.AzureFixtures.Container
+
+import scala.util.Random
+
+trait AzureBagBuilder extends BagBuilder[AzureBlobLocation, AzureBlobLocationPrefix, Container]
+  with AzureFixtures{
+  override def createBagRoot(space: StorageSpace, externalIdentifier: ExternalIdentifier, version: BagVersion)(implicit container: Container): AzureBlobLocationPrefix = AzureBlobLocationPrefix(container.name, createBagRootPath(space, externalIdentifier, version))
+
+  override def createBagLocation(bagRoot: AzureBlobLocationPrefix, path: String): AzureBlobLocation = AzureBlobLocation(bagRoot.container, path)
+
+  override protected def buildFetchEntryLine(entry: PayloadEntry)(implicit container: Container): String = {
+    val displaySize =
+      if (Random.nextBoolean()) entry.contents.getBytes.length.toString else "-"
+
+    s"""https://myaccount.blob.core.windows.net/${container.name}/${entry.path} $displaySize ${entry.bagPath}"""
+  }
+}
