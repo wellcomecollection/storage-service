c854c116cdae3e8dfba2c1b2e75aa81a64d59bf8
alicefuzier <alicefuzier@gmail.com>
2020-08-04 14:31:50 +0100

    Starting to add a AzureFixityChecker

---
+++ bag_verifier/src/main/scala/uk/ac/wellcome/platform/archive/bagverifier/fixity/azure/AzureFixityChecker.scala
@@ -0,0 +1,20 @@
+package uk.ac.wellcome.platform.archive.bagverifier.fixity.azure
+
+import java.net.URI
+
+import com.azure.storage.blob.BlobServiceClient
+import uk.ac.wellcome.platform.archive.bagverifier.fixity.FixityChecker
+import uk.ac.wellcome.platform.archive.bagverifier.storage.Locatable
+import uk.ac.wellcome.platform.archive.common.storage.services.SizeFinder
+import uk.ac.wellcome.storage.azure.{AzureBlobLocation, AzureBlobLocationPrefix}
+import uk.ac.wellcome.storage.store.StreamStore
+import uk.ac.wellcome.storage.store.azure.AzureStreamStore
+import uk.ac.wellcome.storage.tags.Tags
+import uk.ac.wellcome.storage.tags.azure.AzureBlobMetadata
+
+class AzureFixityChecker(implicit blobClient: BlobServiceClient) extends FixityChecker[AzureBlobLocation, AzureBlobLocationPrefix] {
+  override protected val streamStore: StreamStore[AzureBlobLocation] = new AzureStreamStore()
+  override protected val sizeFinder: SizeFinder[AzureBlobLocation] = ???
+  override val tags: Tags[AzureBlobLocation] = new AzureBlobMetadata()
+  override implicit val locator: Locatable[AzureBlobLocation, AzureBlobLocationPrefix, URI] = ???
+}
---
+++ bag_verifier/src/test/scala/uk/ac/wellcome/platform/archive/bagverifier/fixity/azure/AzureFixityCheckerTest.scala
@@ -0,0 +1,43 @@
+package uk.ac.wellcome.platform.archive.bagverifier.fixity.azure
+
+import java.net.URI
+
+import uk.ac.wellcome.fixtures.TestWith
+import uk.ac.wellcome.platform.archive.bagverifier.fixity.{
+  FixityChecker,
+  FixityCheckerTestCases
+}
+import uk.ac.wellcome.platform.archive.bagverifier.storage.azure.AzureResolvable
+import uk.ac.wellcome.storage.azure.{AzureBlobLocation, AzureBlobLocationPrefix}
+import uk.ac.wellcome.storage.fixtures.AzureFixtures
+import uk.ac.wellcome.storage.fixtures.AzureFixtures.Container
+import uk.ac.wellcome.storage.store.azure.{AzureStreamStore, AzureTypedStore}
+
+class AzureFixityCheckerTest extends FixityCheckerTestCases[
+  AzureBlobLocation,
+  AzureBlobLocationPrefix,
+  Container,
+  Unit,
+  AzureStreamStore
+]
+  with AzureFixtures {
+
+  implicit val streamStore: AzureStreamStore = new AzureStreamStore()
+  val azureTypedStore = new AzureTypedStore[String]()
+
+  override def withContext[R](testWith: TestWith[Unit, R]): R = testWith(())
+
+  override def putString(location: AzureBlobLocation, contents: String)(implicit context: Unit): Unit = azureTypedStore.put(location)(contents)
+
+  override def withFixityChecker[R](streamStore: AzureStreamStore)(testWith: TestWith[FixityChecker[AzureBlobLocation, AzureBlobLocationPrefix], R])(implicit context: Unit): R = testWith(new AzureFixityChecker())
+
+  override def withStreamStore[R](testWith: TestWith[AzureStreamStore, R])(implicit context: Unit): R = testWith(streamStore)
+
+  override def resolve(location: AzureBlobLocation): URI = new AzureResolvable().resolve(location)
+
+  override def withNamespace[R](testWith: TestWith[Container, R]): R = withAzureContainer{container =>
+    testWith(container)
+  }
+
+  override def createId(implicit container: Container): AzureBlobLocation = createAzureBlobLocationWith(container)
+}
