9539d64d7176726413d86da0dfdae4b8533f706f
Alex Chan <alex@alexwlchan.net>
2020-08-05 10:43:38 +0100

    Define our test bags in unpacked form
    This makes it easier to see what those bags contain, and to change them
    if we need to add new files -- a script handles the checksums rather
    than a human.

---
+++ monitoring/test_bags/upload_test_bags.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import os
+
+import bagit
+import boto3
+import tarfile
+
+
+def find_bag_dirs(root):
+    for dir_entry in os.listdir(root):
+        if os.path.isdir(dir_entry):
+            yield dir_entry
+
+
+def create_bag(dirname):
+    bag = bagit.Bag(dirname)
+    bag.algorithms = ["sha256", "sha512"]
+    bag.save(manifests=True)
+
+
+def create_tar_gz(bag_dir):
+    archive_name = f"{bag_dir}.tar.gz"
+
+    with tarfile.open(f"{bag_dir}.tar.gz", "w:gz") as tf:
+        for dirpath, _, filenames in os.walk(bag_dir):
+            for f in filenames:
+                name = os.path.join(dirpath, f)
+                arcname = os.path.relpath(name, start=bag_dir)
+                tf.add(name=name, arcname=arcname)
+
+    return archive_name
+
+
+def upload_tar_gz(tar_gz_name):
+    s3 = boto3.client("s3")
+
+    s3.upload_file(
+        Bucket="wellcomecollection-storage-infra",
+        Filename=tar_gz_name,
+        Key=f"test_bags/{tar_gz_name}"
+    )
+
+
+if __name__ == "__main__":
+    s3 = boto3.client("s3")
+
+    for bag_dir in find_bag_dirs("."):
+        print(f"Working on {bag_dir}")
+        create_bag(bag_dir)
+        tar_gz_name = create_tar_gz(bag_dir)
+        upload_tar_gz(tar_gz_name)
