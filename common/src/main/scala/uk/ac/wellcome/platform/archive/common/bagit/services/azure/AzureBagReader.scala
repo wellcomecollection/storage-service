55b05adc3e096dbc92022c8292f51625a663fe4a
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2020-08-11 10:14:47 +0000

    Apply auto-formatting rules

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/bagit/services/azure/AzureBagReader.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/bagit/services/azure/AzureBagReader.scala
@@ -7,6 +7,9 @@ import uk.ac.wellcome.storage.store
 import uk.ac.wellcome.storage.store.azure.AzureStreamStore
 import uk.ac.wellcome.storage.streaming.InputStreamWithLength
 
-class AzureBagReader(implicit blobClient: BlobServiceClient) extends BagReader[AzureBlobLocation, AzureBlobLocationPrefix]{
-  override implicit val readable: store.Readable[AzureBlobLocation, InputStreamWithLength] = new AzureStreamStore()
+class AzureBagReader(implicit blobClient: BlobServiceClient)
+    extends BagReader[AzureBlobLocation, AzureBlobLocationPrefix] {
+  override implicit val readable
+    : store.Readable[AzureBlobLocation, InputStreamWithLength] =
+    new AzureStreamStore()
 }
--- common/src/test/scala/uk/ac/wellcome/platform/archive/common/fixtures/azure/AzureBagBuilder.scala
+++ common/src/test/scala/uk/ac/wellcome/platform/archive/common/fixtures/azure/AzureBagBuilder.scala
@@ -1,7 +1,13 @@
 package uk.ac.wellcome.platform.archive.common.fixtures.azure
 
-import uk.ac.wellcome.platform.archive.common.bagit.models.{BagVersion, ExternalIdentifier}
-import uk.ac.wellcome.platform.archive.common.fixtures.{BagBuilder, PayloadEntry}
+import uk.ac.wellcome.platform.archive.common.bagit.models.{
+  BagVersion,
+  ExternalIdentifier
+}
+import uk.ac.wellcome.platform.archive.common.fixtures.{
+  BagBuilder,
+  PayloadEntry
+}
 import uk.ac.wellcome.platform.archive.common.storage.models.StorageSpace
 import uk.ac.wellcome.storage.azure.{AzureBlobLocation, AzureBlobLocationPrefix}
 import uk.ac.wellcome.storage.fixtures.AzureFixtures
@@ -9,17 +15,27 @@ import uk.ac.wellcome.storage.fixtures.AzureFixtures.Container
 
 import scala.util.Random
 
-trait AzureBagBuilder extends BagBuilder[AzureBlobLocation, AzureBlobLocationPrefix, Container]
-  with AzureFixtures{
-  override def createBagRoot(space: StorageSpace, externalIdentifier: ExternalIdentifier, version: BagVersion)(implicit container: Container): AzureBlobLocationPrefix =
+trait AzureBagBuilder
+    extends BagBuilder[AzureBlobLocation, AzureBlobLocationPrefix, Container]
+    with AzureFixtures {
+  override def createBagRoot(
+    space: StorageSpace,
+    externalIdentifier: ExternalIdentifier,
+    version: BagVersion
+  )(implicit container: Container): AzureBlobLocationPrefix =
     AzureBlobLocationPrefix(
       container = container.name,
       namePrefix = createBagRootPath(space, externalIdentifier, version)
     )
 
-  override def createBagLocation(bagRoot: AzureBlobLocationPrefix, path: String): AzureBlobLocation = AzureBlobLocation(bagRoot.container, path)
+  override def createBagLocation(
+    bagRoot: AzureBlobLocationPrefix,
+    path: String
+  ): AzureBlobLocation = AzureBlobLocation(bagRoot.container, path)
 
-  override protected def buildFetchEntryLine(entry: PayloadEntry)(implicit container: Container): String = {
+  override protected def buildFetchEntryLine(
+    entry: PayloadEntry
+  )(implicit container: Container): String = {
     val displaySize =
       if (Random.nextBoolean()) entry.contents.getBytes.length.toString else "-"
 
