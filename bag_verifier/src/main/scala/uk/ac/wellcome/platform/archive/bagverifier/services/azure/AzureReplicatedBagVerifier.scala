4447415e1fe7fd9aae0788d9a22cf9f103a77e00
Alex Chan <alex@alexwlchan.net>
2020-08-12 10:25:29 +0100

    Tidy up the definition of AzureReplicatedBagVerifier

--- bag_verifier/src/main/scala/uk/ac/wellcome/platform/archive/bagverifier/services/azure/AzureReplicatedBagVerifier.scala
+++ bag_verifier/src/main/scala/uk/ac/wellcome/platform/archive/bagverifier/services/azure/AzureReplicatedBagVerifier.scala
@@ -2,34 +2,29 @@ package uk.ac.wellcome.platform.archive.bagverifier.services.azure
 
 import com.amazonaws.services.s3.AmazonS3
 import com.azure.storage.blob.BlobServiceClient
-import uk.ac.wellcome.platform.archive.bagverifier.fixity.FixityChecker
 import uk.ac.wellcome.platform.archive.bagverifier.fixity.azure.AzureFixityChecker
 import uk.ac.wellcome.platform.archive.bagverifier.services.ReplicatedBagVerifier
-import uk.ac.wellcome.platform.archive.bagverifier.storage.Resolvable
 import uk.ac.wellcome.platform.archive.bagverifier.storage.azure.AzureResolvable
-import uk.ac.wellcome.platform.archive.common.bagit.services.BagReader
 import uk.ac.wellcome.platform.archive.common.bagit.services.azure.AzureBagReader
 import uk.ac.wellcome.storage.azure.{AzureBlobLocation, AzureBlobLocationPrefix}
-import uk.ac.wellcome.storage.listing.Listing
 import uk.ac.wellcome.storage.listing.azure.AzureBlobLocationListing
-import uk.ac.wellcome.storage.s3.S3ObjectLocation
-import uk.ac.wellcome.storage.store.StreamStore
 import uk.ac.wellcome.storage.store.azure.AzureStreamStore
-import uk.ac.wellcome.storage.store.s3.S3StreamStore
 
 class AzureReplicatedBagVerifier(val primaryBucket: String)(
   implicit val s3Client: AmazonS3,
   implicit val blobClient: BlobServiceClient
 ) extends ReplicatedBagVerifier[AzureBlobLocation, AzureBlobLocationPrefix] {
 
-  override val srcStreamStore: StreamStore[S3ObjectLocation] =
-    new S3StreamStore()
-  override implicit val bagReader: BagReader[AzureBlobLocation, AzureBlobLocationPrefix] = new AzureBagReader()
-  override implicit val listing: Listing[AzureBlobLocationPrefix, AzureBlobLocation] = AzureBlobLocationListing()
-  override implicit val resolvable: Resolvable[AzureBlobLocation] = new AzureResolvable()
-  override implicit val fixityChecker: FixityChecker[AzureBlobLocation, AzureBlobLocationPrefix] = new AzureFixityChecker()
+  override implicit val bagReader: AzureBagReader = new AzureBagReader()
 
-  override def getRelativePath(root: AzureBlobLocationPrefix, location: AzureBlobLocation): String = location.name.replace(root.namePrefix, "")
+  override implicit val listing: AzureBlobLocationListing = AzureBlobLocationListing()
 
-  override val replicaStreamStore: StreamStore[AzureBlobLocation] = new AzureStreamStore()
+  override implicit val resolvable: AzureResolvable = new AzureResolvable()
+
+  override implicit val fixityChecker: AzureFixityChecker = new AzureFixityChecker()
+
+  override def getRelativePath(root: AzureBlobLocationPrefix, location: AzureBlobLocation): String =
+    location.name.replace(root.namePrefix, "")
+
+  override val replicaStreamStore: AzureStreamStore = new AzureStreamStore()
 }
