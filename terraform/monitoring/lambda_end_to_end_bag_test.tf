f3c2a45faa28e6cc886cfec36ec390273b16f275
Alex Chan <alex@alexwlchan.net>
2020-08-12 12:43:30 +0100

    Test bags are no longer stored as tarballs

--- terraform/monitoring/lambda_end_to_end_bag_test.tf
+++ terraform/monitoring/lambda_end_to_end_bag_test.tf
@@ -5,8 +5,8 @@ module "end_to_end_bag_tester_stage" {
   description = "Send a bag to test the staging storage service"
 
   environment = {
-    BUCKET = module.bag_with_fetch_file_stage.bucket
-    KEY    = module.bag_with_fetch_file_stage.key
+    BUCKET = local.infra_bucket
+    KEY    = "test_bags/bag_with_fetch_file_stage.tar.gz"
 
     EXTERNAL_IDENTIFIER = "test_bag"
 
@@ -25,8 +25,8 @@ module "end_to_end_bag_tester_prod" {
   description = "Send a bag to test the storage service"
 
   environment = {
-    BUCKET = module.bag_with_fetch_file_prod.bucket
-    KEY    = module.bag_with_fetch_file_prod.key
+    BUCKET = local.infra_bucket
+    KEY    = "test_bags/bag_with_fetch_file_prod.tar.gz"
 
     EXTERNAL_IDENTIFIER = "test_bag"
 
