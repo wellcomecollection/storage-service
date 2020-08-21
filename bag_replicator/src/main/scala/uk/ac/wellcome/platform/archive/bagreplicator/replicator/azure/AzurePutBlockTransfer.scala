3149e2fc04960f02c6b5971a1a235b9fc39c7233
Alex Chan <alex@alexwlchan.net>
2020-08-21 15:50:52 +0100

    Get AzurePutBlockTransfer working

--- bag_replicator/src/main/scala/uk/ac/wellcome/platform/archive/bagreplicator/replicator/azure/AzurePutBlockTransfer.scala
+++ bag_replicator/src/main/scala/uk/ac/wellcome/platform/archive/bagreplicator/replicator/azure/AzurePutBlockTransfer.scala
@@ -1,13 +1,17 @@
 package uk.ac.wellcome.platform.archive.bagreplicator.replicator.azure
 
-import java.net.URL
+import java.nio.charset.StandardCharsets
+import java.util
+import java.util.Base64
 
 import com.amazonaws.services.s3.AmazonS3
 import com.azure.storage.blob.BlobServiceClient
+import com.azure.storage.blob.models.BlobRange
+import grizzled.slf4j.Logging
 import uk.ac.wellcome.platform.archive.common.storage.services.s3.S3Uploader
 import uk.ac.wellcome.storage.azure.AzureBlobLocation
 import uk.ac.wellcome.storage.s3.S3ObjectLocation
-import uk.ac.wellcome.storage.transfer.{Transfer, TransferDestinationFailure, TransferPerformed, TransferSourceFailure}
+import uk.ac.wellcome.storage.transfer.{Transfer, TransferDestinationFailure, TransferPerformed}
 
 import scala.concurrent.duration._
 import scala.util.{Failure, Success, Try}
@@ -16,29 +20,79 @@ class AzurePutBlockTransfer(
   implicit
   s3Client: AmazonS3,
   blobServiceClient: BlobServiceClient
-) extends Transfer[S3ObjectLocation, AzureBlobLocation] {
+) extends Transfer[S3ObjectLocation, AzureBlobLocation] with Logging {
 
   implicit val s3Uploader: S3Uploader = new S3Uploader()
 
-  private def singleTransfer(src: S3ObjectLocation, dst: AzureBlobLocation): TransferEither =
-    for {
-      s3PresignedUrl: URL <- s3Uploader.getPresignedGetURL(
-        location = src,
-        expiryLength = 6.hour
-      ) match {
-        case Right(url) => Right(url)
-        case Left(err)  => Left(TransferSourceFailure(src, dst, err.e))
+  private class RangeEnumeration(
+                                     contentLength: Long,
+                                     bufferSize: Long
+  ) extends util.Enumeration[BlobRange] {
+    var currentPosition = 0L
+    val totalLength: Long = contentLength
+
+    override def hasMoreElements: Boolean =
+      currentPosition < totalLength
+
+    override def nextElement(): BlobRange = {
+      val r = if (currentPosition + bufferSize >= totalLength) {
+        new BlobRange(currentPosition)
+      } else {
+        new BlobRange(currentPosition, bufferSize)
+      }
+      currentPosition += bufferSize
+      r
+    }
+  }
+
+  private def singleTransfer(src: S3ObjectLocation, dst: AzureBlobLocation): TransferEither = {
+    val size = s3Client.getObject(src.bucket, src.key).getObjectMetadata.getContentLength
+    println(s"Size of $src is $size")
+
+    val s3PresignedUrl = s3Uploader.getPresignedGetURL(
+      location = src,
+      expiryLength = 6.hour
+    ).right.get
+
+    val blockClient = blobServiceClient
+      .getBlobContainerClient(dst.container)
+      .getBlobClient(dst.name)
+      .getBlockBlobClient
+
+    import scala.collection.JavaConverters._
+
+    val ranges = new RangeEnumeration(contentLength = size, bufferSize = 100000000L).asScala.toSeq
+    println(ranges)
+
+    val blockIdLength = ranges.length.toString.length
+
+    val identifiedRanges: Seq[(String, BlobRange)] = ranges.zipWithIndex.map { case (range, i) =>
+      // https://gauravmantri.com/2013/05/18/windows-azure-blob-storage-dealing-with-the-specified-blob-or-block-content-is-invalid-error/
+      val idNumber = ("0" * blockIdLength + (i + 1).toString).takeRight(blockIdLength)
+
+      Base64.getEncoder.encodeToString(idNumber.getBytes(StandardCharsets.UTF_8)) -> range
+    }
+    debug(s"@@AWLC ranges for $src: $identifiedRanges")
+
+    Try {
+      identifiedRanges.foreach { case (blockId, range) =>
+        debug(s"@@AWLC uploading to $dst with range $range / block Id $blockId")
+        val s = blockClient.stageBlock()
+        val s = blockClient.stageBlockFromUrl(
+          blockId,
+          s3PresignedUrl.toString,
+          range
+        )
+        debug(s"@@AWLC reuslt of stageBlockFromURL for $blockId=$range is $s")
+        println(s)
       }
 
-      blobClient = blobServiceClient
-        .getBlobContainerClient(dst.container)
-        .getBlobClient(dst.name)
-
-      result <- Try { blobClient.copyFromUrl(s3PresignedUrl.toString) } match {
-        case Success(_)   => Right(TransferPerformed(src, dst))
-        case Failure(err) => Left(TransferDestinationFailure(src, dst, err))
-      }
-    } yield result
+      blockClient.commitBlockList(identifiedRanges.map { case (blockId, _) => blockId }.toList.asJava)
+    } match {
+      case Success(_)   => Right(TransferPerformed(src, dst))
+      case Failure(err) => Left(TransferDestinationFailure(src, dst, err))
+    }
+  }
 
   override protected def transferWithCheckForExisting(
     src: S3ObjectLocation,
