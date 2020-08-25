09afcdc18fb68aea2f995b60f37fc7b875af34d5
Alex Chan <alex@alexwlchan.net>
2020-08-25 11:53:23 +0100

    Use the new Azure large stream reader in the bag verifier

--- bag_verifier/src/main/scala/uk/ac/wellcome/platform/archive/bagverifier/fixity/azure/AzureFixityChecker.scala
+++ bag_verifier/src/main/scala/uk/ac/wellcome/platform/archive/bagverifier/fixity/azure/AzureFixityChecker.scala
@@ -3,14 +3,23 @@ package uk.ac.wellcome.platform.archive.bagverifier.fixity.azure
 import com.azure.storage.blob.BlobServiceClient
 import uk.ac.wellcome.platform.archive.bagverifier.fixity.FixityChecker
 import uk.ac.wellcome.platform.archive.bagverifier.storage.azure.AzureLocatable
-import uk.ac.wellcome.platform.archive.common.storage.services.azure.AzureSizeFinder
+import uk.ac.wellcome.platform.archive.common.storage.services.azure.{AzureLargeStreamReader, AzureSizeFinder}
 import uk.ac.wellcome.storage.azure.{AzureBlobLocation, AzureBlobLocationPrefix}
-import uk.ac.wellcome.storage.store.azure.AzureStreamStore
+import uk.ac.wellcome.storage.store.Readable
+import uk.ac.wellcome.storage.streaming.InputStreamWithLength
 
 class AzureFixityChecker(implicit blobClient: BlobServiceClient)
     extends FixityChecker[AzureBlobLocation, AzureBlobLocationPrefix] {
-  override protected val streamReader =
-    new AzureStreamStore()
+
+  // The verifier gets 4GB of memory, and may be processing up to ten objects at once.
+  // Allowing a GB of overhead for other functions, let it read 200MB at a time.
+  //
+  // This means reading a 166GB object (the largest so far in the storage service) will
+  // require 830 Read calls.
+  //
+  // In practice most objects are well under this threshhold, so it won't be maxing out.
+  override protected val streamReader: Readable[AzureBlobLocation, InputStreamWithLength] =
+    new AzureLargeStreamReader(bufferSize = 200 * 1000 * 1000)
 
   override protected val sizeFinder =
     new AzureSizeFinder()
