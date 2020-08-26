5472fa7d15e38600b339f09e5cd65138c79cbc7b
alicefuzier <alicefuzier@gmail.com>
2020-08-26 13:42:45 +0100

    Print errors and move on + some extraction in functions

--- scripts/migrations/2020-08-update_migration_created_date.py
+++ scripts/migrations/2020-08-update_migration_created_date.py
@@ -13,6 +13,12 @@ vhs_table = "vhs-storage-staging-manifests-2020-07-24"
 
 backfill_vhs_table = "vhs-storage-staging-manifests-2020-08-19"
 
+errors = {}
+
+
+def record_error(id, version, err):
+    print(f"Error while updating {id}/{version}: {err}")
+    errors[f'{id}/{version}'] = err
 
 def get_bucket_key(item):
     try:
@@ -23,61 +29,80 @@ def get_bucket_key(item):
             bucket = item["payload"]["bucket"]
             key = item["payload"]["key"]
         except:
-            raise RuntimeError(f"Find s3 bucket, key in {item}")
+            record_error(item["id"], item["version"], f"Cannot find s3 bucket, key in {item}")
         else:
             return bucket, key
     else:
         return bucket, key
 
 
-def get_vhs_json(bucket, key):
-    return json.loads(s3.Object(bucket, key).get()["Body"].read().decode("utf-8"))
+def get_vhs_json(id, version, bucket, key):
+    try:
+        return json.loads(s3.Object(bucket, key).get()["Body"].read().decode("utf-8"))
+    except ClientError as e:
+        record_error(id, version, f"Cannot read s3 entry for {id}: {item}: {e}")
 
 
-def put_vhs_json(bucket, key, content):
+def put_vhs_json(id, version, bucket, content):
+    bb = uuid.uuid4()
+    key = f"{id}/{version}/{bb}"
     try:
         s3object = s3.Object(bucket, key)
         # s3object.put(
         #     Body=(bytes(json.dumps(content).encode('UTF-8')))
         # )
     except ClientError as e:
-        raise RuntimeError(f"Error updating backfill vhs object s3://{bucket}/{key}",e)
-    except:
-        raise RuntimeError(f"Unknown error when updating backfill vhs object s3://{bucket}/{key}")
+        record_error(id, version, f"Error updating backfill vhs object s3://{bucket}/{key}")
+
+
+def get_backfill_item(id, version):
+    try:
+        return dynamodb.Table(backfill_vhs_table).get_item(
+            Key={"id": id, "version": version}
+        )["Item"]
+    except KeyError:
+        record_error(id, version, f"Cannot find backfill storage manifest!!!!")
+
+
+def is_expected_diff(id, version, current_json, backfill_json):
+    diff = DeepDiff(current_json, backfill_json, ignore_order=True)
+
+    if diff:
+        values_changed = diff.pop("values_changed", None)
+        items_added = diff.pop("iterable_item_added", None)
+        if values_changed and values_changed.keys()-["root['createdDate']"]:
+            record_error(id, version, f"Unexpected values changed in {values_changed}!")
+            return False
+        if items_added and items_added.keys() - ["root['replicaLocations'][1]"]:
+            record_error(id, version, f"Unexpected values changed in {items_added}!")
+            return False
+        if diff:
+            record_error(id, version, f"Unexpected values changed in {diff}!")
+            return False
+    return True
 
 
 for item in scan_table(TableName=vhs_table):
     id = item["id"]
     version = item["version"]
     bucket, key = get_bucket_key(item)
-    try:
-        vhs_content = get_vhs_json(bucket, key)
-    except:
-        raise RuntimeError(f"Cannot read s3 entry for {id}: {item}")
-    else:
-        created_date = vhs_content["createdDate"]
-        try:
-            backfilled_item = dynamodb.Table(backfill_vhs_table).get_item(
-                Key={"id": id, "version": version}
-            )["Item"]
-        except KeyError:
-            print(f"Cannot find backfill storage manifest for {id}:{version}!!!!")
-        else:
-            backfilled_bucket = backfilled_item["payload"]["bucket"]
-            backfilled_key = backfilled_item["payload"]["key"]
-            backfilled_json = get_vhs_json(backfilled_bucket, backfilled_key)
+    if bucket and key:
+        vhs_content = get_vhs_json(id, version, bucket, key)
+        if vhs_content:
+            created_date = vhs_content["createdDate"]
+            backfilled_item = get_backfill_item(id, version)
+            if backfilled_item:
+                backfilled_bucket = backfilled_item["payload"]["bucket"]
+                backfilled_key = backfilled_item["payload"]["key"]
+                backfilled_json = get_vhs_json(id, version, backfilled_bucket, backfilled_key)
+                if backfilled_json:
+                    if is_expected_diff(id,version,vhs_content, backfilled_json):
+                        backfilled_json["createdDate"] = created_date
+                        put_vhs_json(id, backfilled_bucket, backfilled_key, backfilled_json)
 
-            diff = DeepDiff(vhs_content, backfilled_json, ignore_order=True)
-
-            if diff:
-                values_changed = diff.pop("values_changed", None)
-                items_added = diff.pop("iterable_item_added", None)
-                if values_changed and values_changed.keys()-["root['createdDate']"]:
-                    raise RuntimeError(f"Unexpected values changed in {values_changed} for {id}:{version}!")
-                if items_added and items_added.keys() - ["root['replicaLocations'][1]"]:
-                    raise RuntimeError(f"Unexpected values changed in {items_added} for {id}:{version}!")
-                if diff:
-                    raise RuntimeError(f"Unexpected values changed in {diff} for {id}:{version}!")
-
-            backfilled_json["createdDate"] = created_date
-            put_vhs_json(backfilled_bucket, backfilled_key, backfilled_json)
+if errors:
+    print(errors)
+    exit(1)
+else:
+    print("Finished with no errors!")
+    exit(0)
