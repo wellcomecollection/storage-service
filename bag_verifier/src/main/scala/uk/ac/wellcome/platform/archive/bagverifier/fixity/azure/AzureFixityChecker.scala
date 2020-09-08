63318850c3361137eb20454465d2f8ed689131a5
Alex Chan <alex@alexwlchan.net>
2020-09-08 10:27:57 +0100

    Check for tags in the AzureFixityChecker tests

--- bag_verifier/src/main/scala/uk/ac/wellcome/platform/archive/bagverifier/fixity/azure/AzureFixityChecker.scala
+++ bag_verifier/src/main/scala/uk/ac/wellcome/platform/archive/bagverifier/fixity/azure/AzureFixityChecker.scala
@@ -10,6 +10,7 @@ import uk.ac.wellcome.platform.archive.common.storage.services.azure.{
 import uk.ac.wellcome.storage.azure.{AzureBlobLocation, AzureBlobLocationPrefix}
 import uk.ac.wellcome.storage.store.Readable
 import uk.ac.wellcome.storage.streaming.InputStreamWithLength
+import uk.ac.wellcome.storage.tags.azure.AzureBlobMetadata
 
 class AzureFixityChecker(implicit blobClient: BlobServiceClient)
     extends FixityChecker[AzureBlobLocation, AzureBlobLocationPrefix] {
@@ -36,13 +37,7 @@ class AzureFixityChecker(implicit blobClient: BlobServiceClient)
   override protected val sizeFinder =
     new AzureSizeFinder()
 
-  /**
-    * The FixityChecker tags objects with their checksum so that,
-    * if they are lifecycled to cold storage, we don't need to read them to know the checksum.
-    * This is useful for files referenced in the fetch file. However, references in the fetch file
-    * will always point to the primary bucket in S3, never Azure, so there's no need to tag in the AzureFixityChecker
-    */
-  override val tags = None
+  override val tags = Some(new AzureBlobMetadata())
 
   // e.g. ContentMD5, ContentSHA256
   // We can't include a hyphen in the name because Azure metadata names have to be
