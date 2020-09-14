070e4bf57cb685327fa8b602a8a38a7b6c139546
Alex Chan <a.chan@wellcome.ac.uk>
2020-09-14 08:34:45 +0100

    Add a helper for creating S3ObjectSummary from a Location

--- bag_replicator/src/test/scala/uk/ac/wellcome/platform/archive/bagreplicator/storage/azure/AzurePutBlockFromURLTransferTest.scala
+++ bag_replicator/src/test/scala/uk/ac/wellcome/platform/archive/bagreplicator/storage/azure/AzurePutBlockFromURLTransferTest.scala
@@ -1,19 +1,18 @@
 package uk.ac.wellcome.platform.archive.bagreplicator.storage.azure
 
-import com.amazonaws.services.s3.iterable.S3Objects
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 import uk.ac.wellcome.storage.fixtures.{AzureFixtures, S3Fixtures}
 import uk.ac.wellcome.storage.store.azure.AzureTypedStore
 import uk.ac.wellcome.storage.store.s3.S3TypedStore
 import uk.ac.wellcome.storage.transfer.{TransferNoOp, TransferOverwriteFailure}
-import scala.collection.convert.ImplicitConversions._
 
 class AzurePutBlockFromURLTransferTest
     extends AnyFunSpec
     with Matchers
     with S3Fixtures
-    with AzureFixtures {
+    with AzureFixtures
+    with AzureTransferFixtures {
   val srcStore: S3TypedStore[String] = S3TypedStore[String]
   val dstStore: AzureTypedStore[String] = AzureTypedStore[String]
 
@@ -27,8 +26,9 @@ class AzurePutBlockFromURLTransferTest
           val dst = createAzureBlobLocationWith(dstContainer)
 
           srcStore.put(src)("Hello world") shouldBe a[Right[_, _]]
-          val srcSummary =
-            S3Objects.withPrefix(s3Client, src.bucket, src.key).head
+          val srcSummary = createS3ObjectSummaryFrom(
+            src, size = "Hello world".getBytes().length
+          )
 
           dstStore.put(dst)("Hello world") shouldBe a[Right[_, _]]
 
@@ -47,8 +47,9 @@ class AzurePutBlockFromURLTransferTest
           val dst = createAzureBlobLocationWith(dstContainer)
 
           srcStore.put(src)("hello world") shouldBe a[Right[_, _]]
-          val srcSummary =
-            S3Objects.withPrefix(s3Client, src.bucket, src.key).head
+          val srcSummary = createS3ObjectSummaryFrom(
+            src, size = "hello world".getBytes().length
+          )
           dstStore.put(dst)("HELLO WORLD") shouldBe a[Right[_, _]]
 
           transfer
@@ -66,8 +67,9 @@ class AzurePutBlockFromURLTransferTest
           val dst = createAzureBlobLocationWith(dstContainer)
 
           srcStore.put(src)("Hello world") shouldBe a[Right[_, _]]
-          val srcSummary =
-            S3Objects.withPrefix(s3Client, src.bucket, src.key).head
+          val srcSummary = createS3ObjectSummaryFrom(
+            src, size = "Hello world".getBytes().length
+          )
           dstStore.put(dst)("Greetings, humans") shouldBe a[Right[_, _]]
 
           transfer
