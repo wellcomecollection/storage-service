43eafc89512862158daf88cdf29bb81ba7bad1ea
Alex Chan <a.chan@wellcome.ac.uk>
2020-09-15 07:52:07 +0100

    Add a test that we can transfer weird keys

--- bag_replicator/src/test/scala/uk/ac/wellcome/platform/archive/bagreplicator/storage/azure/AzurePutBlockFromURLTransferTest.scala
+++ bag_replicator/src/test/scala/uk/ac/wellcome/platform/archive/bagreplicator/storage/azure/AzurePutBlockFromURLTransferTest.scala
@@ -2,7 +2,10 @@ package uk.ac.wellcome.platform.archive.bagreplicator.storage.azure
 
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
+import uk.ac.wellcome.storage.azure.AzureBlobLocation
 import uk.ac.wellcome.storage.fixtures.{AzureFixtures, S3Fixtures}
+import uk.ac.wellcome.storage.s3.S3ObjectLocation
 import uk.ac.wellcome.storage.store.azure.AzureTypedStore
 import uk.ac.wellcome.storage.store.s3.S3TypedStore
 import uk.ac.wellcome.storage.transfer.{TransferNoOp, TransferOverwriteFailure}
@@ -12,7 +15,8 @@ class AzurePutBlockFromURLTransferTest
     with Matchers
     with S3Fixtures
     with AzureFixtures
-    with AzureTransferFixtures {
+    with AzureTransferFixtures
+    with TableDrivenPropertyChecks {
   val srcStore: S3TypedStore[String] = S3TypedStore[String]
   val dstStore: AzureTypedStore[String] = AzureTypedStore[String]
 
@@ -76,6 +80,32 @@ class AzurePutBlockFromURLTransferTest
             .transfer(srcSummary, dst, checkForExisting = true)
             .left
             .value shouldBe a[TransferOverwriteFailure[_, _]]
+        }
+      }
+    }
+  }
+
+  it("handles weird keys") {
+    val weirdKeys = Table(
+      "key",
+      "born-digital/SATSY/1821/4/v1/data/objects/Disc_4/Outreach_-_misc/PFFD_enquiry_-Dawn_G.",
+    )
+
+    withLocalS3Bucket { bucket =>
+      withAzureContainer { container =>
+        forAll(weirdKeys) { key =>
+          val src = S3ObjectLocation(bucket.name, key)
+          val dst = AzureBlobLocation(container.name, key)
+
+          srcStore.put(src)("Hello world") shouldBe a[Right[_, _]]
+
+          transfer.transfer(
+            src = createS3ObjectSummaryFrom(src),
+            dst = dst,
+            checkForExisting = true
+          ) shouldBe a[Right[_, _]]
+
+          dstStore.get(dst).right.value.identifiedT shouldBe "Hello world"
         }
       }
     }
