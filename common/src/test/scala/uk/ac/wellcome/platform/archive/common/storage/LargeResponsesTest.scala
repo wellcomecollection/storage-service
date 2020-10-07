2e5049bf5b4950d2484421720e1b37f14f2e27f2
Alex Chan <alex@alexwlchan.net>
2020-10-07 10:49:36 +0100

    LargeResponsesTest doesn't need to extend RandomThings

--- common/src/test/scala/uk/ac/wellcome/platform/archive/common/storage/LargeResponsesTest.scala
+++ common/src/test/scala/uk/ac/wellcome/platform/archive/common/storage/LargeResponsesTest.scala
@@ -20,7 +20,6 @@ import uk.ac.wellcome.akka.fixtures.Akka
 import uk.ac.wellcome.fixtures.TestWith
 import uk.ac.wellcome.storage.fixtures.S3Fixtures
 import uk.ac.wellcome.storage.fixtures.S3Fixtures.Bucket
-import uk.ac.wellcome.storage.generators.RandomThings
 import uk.ac.wellcome.storage.s3.S3ObjectLocationPrefix
 import uk.ac.wellcome.storage.services.s3.S3Uploader
 
@@ -31,7 +30,6 @@ import scala.concurrent.{Await, Future}
 class LargeResponsesTest
     extends AnyFunSpec
     with S3Fixtures
-    with RandomThings
     with Akka {
 
   private val converter = StreamConverters.asInputStream()
