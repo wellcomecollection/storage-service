bef4e2a7d65ed7c9cc24019095815a4a30913d01
Alex Chan <alex@alexwlchan.net>
2020-10-21 18:42:00 +0100

    Add concurrency to the file reindex script

--- indexer/reindex_files_backlog.py
+++ indexer/reindex_files_backlog.py
@@ -4,6 +4,7 @@ This script will send every bag in the storage service to the file indexer
 to be re-indexed in Elasticsearch.
 """
 
+import concurrent.futures
 import json
 
 import click
@@ -26,6 +27,8 @@ def main(env):
     else:
         topic_arn = "arn:aws:sns:eu-west-1:975596993436:storage_staging_file_reindexer_output"
 
+    messages = []
+
     for item in tqdm.tqdm(scan_table(dynamodb_client, TableName=table_name)):
         space, externalIdentifier = item["id"]["S"].split("/", 1)
         version = int(item["version"]["N"])
@@ -36,7 +39,41 @@ def main(env):
             "type": "RegisteredBagNotification",
         }
 
-        sns_client.publish(TopicArn=topic_arn, Message=json.dumps(notification))
+        messages.append(json.dumps(notification))
+
+    def publish(message):
+        return sns_client.publish(TopicArn=topic_arn, Message=message)
+
+    message_count = len(messages)
+    messages = iter(messages)
+
+    print(f"\nPublishing {message_count} notifications to {topic_arn}")
+    with tqdm.tqdm(total=message_count) as progress_bar:
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            # Schedule the first N futures.  We don't want to schedule them all
+            # at once, to avoid consuming excessive amounts of memory.
+            futures = {
+                executor.submit(publish, msg)
+                for msg in itertools.islice(messages, max_parallel_notifications)
+            }
+
+            while futures:
+                # Wait for the next future to complete.
+                done, futures = concurrent.futures.wait(
+                    futures, return_when=concurrent.futures.FIRST_COMPLETED
+                )
+
+                for fut in done:
+                    fut.result()
+
+                progress_bar.update(len(done))
+
+                # Schedule the next set of futures.  We don't want more than N futures
+                # in the pool at a time, to keep memory consumption down.
+                for msg in itertools.islice(messages, len(done)):
+                    futures.add(executor.submit(publish, msg))
+
+    print(f"Published notifications for {message_count} bags.\n")
 
 
 if __name__ == "__main__":
