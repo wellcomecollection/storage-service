94f548c99b4bfe0961ef6cfa5518eac8ca700a1e
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2020-11-17 11:22:10 +0000

    Apply auto-formatting rules

--- scripts/helpers/__init__.py
+++ scripts/helpers/__init__.py
@@ -4,7 +4,4 @@ Helpers shared between our storage service scripts.
 
 from .secrets import read_secret, write_secret
 
-__all__ = [
-    "read_secret",
-    "write_secret",
-]
+__all__ = ["read_secret", "write_secret"]
