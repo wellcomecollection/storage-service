87d283f8be042ec11adee6ccbbf5f61dcd5d1f7e
Alex Chan <a.chan@wellcome.org>
2020-11-18 13:46:44 +0000

    Check the locations we're about to delete

--- scripts/ss_delete_bag.py
+++ scripts/ss_delete_bag.py
@@ -7,6 +7,8 @@ This script will delete a bag from the storage service.  Start by running:
 and follow the instructions from there.
 """
 
+import json
+import os
 import sys
 
 import click
@@ -56,6 +58,10 @@ def main(ingest_id):
         space=space, external_identifier=external_identifier, version=version
     )
 
+    # Do various other checks that this bag is correctly formed before we go
+    # ahead and start deleting stuff.  The deletion should be as close to atomic
+    # as possible -- we shouldn't get halfway through deleting the bag and then
+    # discover something is wrong; we should catch problems upfront.
     storage_client = RequestsOAuthStorageServiceClient.from_path(api_url=api_url)
 
     _confirm_is_latest_version_of_bag(
@@ -63,6 +69,16 @@ def main(ingest_id):
         space=space,
         external_identifier=external_identifier,
         version=version,
+    )
+
+    bag = storage_client.get_bag(
+        space=space, external_identifier=external_identifier, version=version,
+    )
+
+    locations = _confirm_user_wants_to_delete_locations(bag)
+    assert all(
+        loc["prefix"].endswith(f"/{version}")
+        for loc in locations["s3"] + [locations["azure"]]
     )
 
 
@@ -155,6 +171,97 @@ def _confirm_is_latest_version_of_bag(
             f"Something is wrong -- the bags API only knows about {latest_version}, "
             f"which is older than {version}"
         )
+
+
+def _confirm_user_wants_to_delete_locations(bag):
+    """
+    Now get the list of locations/prefixes we're going to delete from permanent
+    storage.  This is another place for the user to intervene if something seems
+    wrong.  It presents a prompt of the following form:
+
+          This bag is stored in 3 locations:
+          - s3://wc-storage-staging/testing/test_bag/v132
+          - s3://wc-storage-staging-replica-ireland/testing/test_bag/v132
+          - azure://wc-storage-staging-replica-netherlands/testing/test_bag/v132
+
+          Does this look right? [y/N]: y
+
+    """
+    click.echo("")
+    click.echo("Checking the locations to delete...")
+
+    location = bag["location"]
+    if location["provider"]["id"] != "amazon-s3":
+        abort(
+            "Something is wrong: the primary location of the bag isn't S3:\n"
+            + json.dumps(location, indent=2, sort_keys=True)
+        )
+
+    replicas = bag["replicaLocations"]
+    replica_providers = {r["provider"]["id"] for r in replicas}
+
+    if len(replicas) != 2 or replica_providers != {"amazon-s3", "azure-blob-storage"}:
+        abort(
+            "This script only knows how to deal with exactly two replicas: one S3, "
+            f"one Azure.  This bag has {len(replicas)}:\n"
+            + json.dumps(replicas, indent=2, sort_keys=True)
+        )
+
+    s3_replica = next(r for r in replicas if r["provider"]["id"] == "amazon-s3")
+    azure_replica = next(
+        r for r in replicas if r["provider"]["id"] == "azure-blob-storage"
+    )
+
+    # All the paths should be the same in each bucket/container; if they're not,
+    # something is dodgy about this bag.
+    prefixes = {location["path"], s3_replica["path"], azure_replica["path"]}
+    if len(prefixes) != 1:
+        abort(
+            "All the replicas should have the same prefix.  This bag has different "
+            "prefixes in different buckets/containers:\n"
+            + json.dumps(
+                {"location": location, "replicas": replicas}, indent=2, sort_keys=True
+            )
+        )
+
+    # The 'prefix' returned in the locations block on a bag refers to *all* versions
+    # of a bag, but when doing deletions we only want to delete a single version.
+    common_prefix = os.path.join(prefixes.pop(), bag["version"])
+
+    try:
+        azure_account = {
+            "wellcomecollection-storage-staging-replica-netherlands": "wecostoragestage",
+            "wellcomecollection-storage-replica-netherlands": "wecostorageprod",
+        }[azure_replica["bucket"]]
+    except KeyError:
+        abort(
+            "Unrecognised Azure container in the Azure replica location:\n"
+            + json.dumps(azure_replica, indent=2, sort_keys=True)
+        )
+
+    locations = {
+        "s3": [
+            {"bucket": location["bucket"], "prefix": common_prefix},
+            {"bucket": s3_replica["bucket"], "prefix": common_prefix},
+        ],
+        "azure": {
+            "account": azure_account,
+            "container": azure_replica["bucket"],
+            "prefix": common_prefix,
+        },
+    }
+
+    loc_uris = [f"s3://{loc['bucket']}/{loc['prefix']}" for loc in locations["s3"]] + [
+        f"azure://{locations['azure']['container']}/{locations['azure']['prefix']}"
+    ]
+
+    click.echo(f"This bag is stored in {hilight(len(loc_uris))} locations:")
+    for loc in loc_uris:
+        click.echo(f"- {hilight(loc)}")
+    click.echo("")
+    click.confirm("Does this look right?", abort=True)
+
+    return locations
 
 
 if __name__ == "__main__":
