e795faf10ecce671e6c864cc494f53ec06cb7987
Alex Chan <a.chan@wellcome.org>
2020-12-04 10:19:55 +0000

    Add some tests for getting the dev status in the daily reporter

---
+++ monitoring/daily_reporter/src/test_ingests.py
@@ -0,0 +1,68 @@
+import datetime
+
+import pytest
+
+from ingests import get_dev_status
+
+
+@pytest.mark.parametrize(
+    "ingest, expected_dev_status",
+    [
+        ({"status": "succeeded"}, "succeeded"),
+        (
+            {
+                "status": "accepted",
+                "createdDate": datetime.datetime.now() - datetime.timedelta(seconds=5),
+            },
+            "accepted",
+        ),
+        (
+            {
+                "status": "accepted",
+                "createdDate": datetime.datetime.now() - datetime.timedelta(days=5),
+            },
+            "stalled",
+        ),
+        (
+            {
+                "status": "processing",
+                "createdDate": datetime.datetime.now() - datetime.timedelta(seconds=5),
+            },
+            "processing",
+        ),
+        (
+            {
+                "status": "processing",
+                "createdDate": datetime.datetime.now() - datetime.timedelta(days=5),
+            },
+            "stalled",
+        ),
+        (
+            {
+                "status": "failed",
+                "events": [{"description": "Assigning bag version failed"}],
+            },
+            "failed (unknown reason)",
+        ),
+        (
+            {
+                "status": "failed",
+                "events": [
+                    {
+                        "description": "Assigning bag version failed - newer version of bag etc."
+                    }
+                ],
+            },
+            "failed (user error)",
+        ),
+        (
+            {
+                "status": "failed",
+                "events": [{"description": "Unpacking failed - the bag doesn't exist"}],
+            },
+            "failed (user error)",
+        ),
+    ],
+)
+def test_get_dev_status(ingest, expected_dev_status):
+    assert get_dev_status(ingest) == expected_dev_status
