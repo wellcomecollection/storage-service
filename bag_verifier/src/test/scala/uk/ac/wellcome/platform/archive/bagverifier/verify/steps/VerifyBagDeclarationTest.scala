afb6726142f63396ab9f62bae845e3ca14cc3f87
Alex Chan <a.chan@wellcome.org>
2021-01-04 10:52:41 +0000

    Add more test cases for VerifyBagDeclaration

--- bag_verifier/src/test/scala/uk/ac/wellcome/platform/archive/bagverifier/verify/steps/VerifyBagDeclarationTest.scala
+++ bag_verifier/src/test/scala/uk/ac/wellcome/platform/archive/bagverifier/verify/steps/VerifyBagDeclarationTest.scala
@@ -89,4 +89,89 @@ class VerifyBagDeclarationTest extends AnyFunSpec with Matchers with EitherValue
     err.userMessage.get shouldBe "Unable to read Bag Declaration from bag (bagit.txt)"
     err.e shouldBe expectedErr
   }
+
+  it("fails if the bagit.txt has no BagIt-Version line") {
+    val store = MemoryStreamStore[MemoryLocation]()
+
+    val root = createMemoryLocationPrefix
+
+    store.put(root.asLocation("bagit.txt"))(
+      stringCodec.toStream("Tag-File-Character-Encoding: UTF-8\n").value
+    )
+
+    val verifier = new VerifyBagDeclaration[MemoryLocation, MemoryLocationPrefix] {
+      override protected val srcReader: Readable[MemoryLocation, InputStreamWithLength] = store
+    }
+
+    val err = verifier.verifyBagDeclaration(createMemoryLocationPrefix).left.value
+    err.userMessage.get shouldBe "The Bag Declaration is missing BagIt-Version (bagit.txt)"
+  }
+
+  it("fails if the bagit.txt has no Tag-File-Character-Encoding line") {
+    val store = MemoryStreamStore[MemoryLocation]()
+
+    val root = createMemoryLocationPrefix
+
+    store.put(root.asLocation("bagit.txt"))(
+      stringCodec.toStream("BagIt-Version: 0.97\n").value
+    )
+
+    val verifier = new VerifyBagDeclaration[MemoryLocation, MemoryLocationPrefix] {
+      override protected val srcReader: Readable[MemoryLocation, InputStreamWithLength] = store
+    }
+
+    val err = verifier.verifyBagDeclaration(createMemoryLocationPrefix).left.value
+    err.userMessage.get shouldBe "The Bag Declaration is missing Tag-File-Character-Encoding (bagit.txt)"
+  }
+
+  it("fails if the bagit.txt is empty") {
+    val store = MemoryStreamStore[MemoryLocation]()
+
+    val root = createMemoryLocationPrefix
+
+    store.put(root.asLocation("bagit.txt"))(
+      stringCodec.toStream("BagIt-Version: 0.97\n").value
+    )
+
+    val verifier = new VerifyBagDeclaration[MemoryLocation, MemoryLocationPrefix] {
+      override protected val srcReader: Readable[MemoryLocation, InputStreamWithLength] = store
+    }
+
+    val err = verifier.verifyBagDeclaration(createMemoryLocationPrefix).left.value
+    err.userMessage.get shouldBe "The Bag Declaration is missing BagIt-Version and Tag-File-Character-Encoding (bagit.txt)"
+  }
+
+  it("fails if the bagit.txt has an unwanted key") {
+    val store = MemoryStreamStore[MemoryLocation]()
+
+    val root = createMemoryLocationPrefix
+
+    store.put(root.asLocation("bagit.txt"))(
+      stringCodec.toStream("BagIt-Version: 1.0\nTag-File-Character-Encoding: UTF-8\nExtra-Key: ShouldNotBeHere").value
+    )
+
+    val verifier = new VerifyBagDeclaration[MemoryLocation, MemoryLocationPrefix] {
+      override protected val srcReader: Readable[MemoryLocation, InputStreamWithLength] = store
+    }
+
+    val err = verifier.verifyBagDeclaration(createMemoryLocationPrefix).left.value
+    err.userMessage.get shouldBe "The Bag Declaration has an unwanted key: Extra-Key (bagit.txt)"
+  }
+
+  it("fails if the bagit.txt has unwanted keys") {
+    val store = MemoryStreamStore[MemoryLocation]()
+
+    val root = createMemoryLocationPrefix
+
+    store.put(root.asLocation("bagit.txt"))(
+      stringCodec.toStream("BagIt-Version: 1.0\nTag-File-Character-Encoding: UTF-8\nExtra-Key1: ShouldNotBeHere\nExtra-Key2: ShouldNotBeHere").value
+    )
+
+    val verifier = new VerifyBagDeclaration[MemoryLocation, MemoryLocationPrefix] {
+      override protected val srcReader: Readable[MemoryLocation, InputStreamWithLength] = store
+    }
+
+    val err = verifier.verifyBagDeclaration(createMemoryLocationPrefix).left.value
+    err.userMessage.get shouldBe "The Bag Declaration has unwanted keys: Extra-Key1, Extra-Key2 (bagit.txt)"
+  }
 }
