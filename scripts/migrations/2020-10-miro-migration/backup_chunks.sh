244610f0a917d887662306f4f6d204d7ddeed999
EC2 Default User <ec2-user@ip-172-42-2-139.eu-west-1.compute.internal>
2021-01-22 17:39:33 +0000

    update backup chunks

--- scripts/migrations/2020-10-miro-migration/backup_chunks.sh
+++ scripts/migrations/2020-10-miro-migration/backup_chunks.sh
@@ -5,10 +5,14 @@ cd /data/storage-service/scripts/migrations/2020-10-miro-migration
 python3 main.py save-index --index-name chunks -o
 python3 main.py save-index --index-name chunks_movies_and_corporate -o
 python3 main.py save-index --index-name chunks_no_miro_id -o
+python3 main.py save-index --index-name chunks_derivatives -o
+
 python3 main.py save-index --index-name transfers -o
 
 aws s3 cp _cache/index_chunks.json s3://wellcomecollection-platform-infra/miro-migration/index_chunks.json --profile platform
 aws s3 cp _cache/index_chunks_movies_and_corporate.json s3://wellcomecollection-platform-infra/miro-migration/index_chunks_movies_and_corporate.json --profile platform
 aws s3 cp _cache/index_chunks_no_miro_id.json s3://wellcomecollection-platform-infra/miro-migration/index_chunks_no_miro_id.json --profile platform
+aws s3 cp _cache/index_chunks_derivatives.json s3://wellcomecollection-platform-infra/miro-migration/index_chunks_derivatives.json --profile platform
+
 aws s3 cp _cache/index_transfers.json s3://wellcomecollection-platform-infra/miro-migration/index_transfers.json --profile platform
 
