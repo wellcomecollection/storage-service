2692b44f3187cf4515c57b241decb711637d31c2
Alex Chan <a.chan@wellcome.org>
2021-04-08 11:35:57 +0100

    Add the image/tiff tag to TIFs in the digitised space

--- bag_tagger/src/main/scala/uk/ac/wellcome/platform/storage/bag_tagger/services/TagRules.scala
+++ bag_tagger/src/main/scala/uk/ac/wellcome/platform/storage/bag_tagger/services/TagRules.scala
@@ -16,6 +16,15 @@ object TagRules {
   ): Map[StorageManifestFile, Map[String, String]] =
     createContentTypeTags(manifest)
 
+  /** Create tags of the form Map("Content-Type" -> [MIME type])
+    *
+    * Our tags are based on the official list of MIME types from
+    * http://www.iana.org/assignments/media-types/media-types.xhtml
+    *
+    * We prefer to tag with MIME types because these are standardised tags
+    * that might be useful elsewhere, rather than inventing an unnecessary
+    * tagging scheme solely for the storage service.
+    */
   private def createContentTypeTags(
     manifest: StorageManifest
   ): Map[StorageManifestFile, Map[String, String]] = {
@@ -30,6 +39,14 @@ object TagRules {
         // Services like DLCS will use the MP4 to serve videos on the web.
         case f if manifest.space == StorageSpace("digitised") && f.hasExtension(".mxf") =>
           f -> "application/mxf"
+
+        // In our digitised manuscripts workflow, we keep both the original TIFF
+        // and the edited JP2 from LayoutWizzard.
+        //
+        // We apply a Content-Type tag so the TIFF can be lifecycled to a
+        // cold storage tier, because DLCS will use the JP2 to serve images on the web.
+        case f if manifest.space == StorageSpace("digitised") && f.hasExtension(".tif", ".tiff") =>
+          f -> "image/tiff"
       }
 
     contentTypes
@@ -38,10 +55,13 @@ object TagRules {
   }
 
   implicit class FileOps(f: StorageManifestFile) {
-    def hasExtension(ext: String): Boolean = {
-      assert(ext.startsWith("."))
-      assert(ext.toLowerCase == ext)
-      f.path.toLowerCase.endsWith(ext )
+    def hasExtension(exts: String*): Boolean = {
+      exts.foreach { e =>
+        assert(e.startsWith("."))
+        assert(e.toLowerCase == e)
+      }
+
+      exts.exists(f.path.toLowerCase.endsWith)
     }
   }
 }
--- bag_tagger/src/test/scala/uk/ac/wellcome/platform/storage/bag_tagger/services/TagRulesTest.scala
+++ bag_tagger/src/test/scala/uk/ac/wellcome/platform/storage/bag_tagger/services/TagRulesTest.scala
@@ -58,4 +58,67 @@ class TagRulesTest
       }
     }
   }
+
+  describe("Content-Type=application/tif for TIFF manuscripts masters") {
+    describe("it applies a tag") {
+      it("for TIFFs in the digitised space") {
+        val tifFile = createStorageManifestFileWith(name = "b1842700_0001.tif")
+
+        val manifest = createStorageManifestWith(
+          space = StorageSpace("digitised"),
+          files = Seq(tifFile)
+        )
+
+        TagRules.chooseTags(manifest) shouldBe Map(
+          tifFile -> Map("Content-Type" -> "image/tiff")
+        )
+      }
+
+      it("for TIFFs that use two 'f's in the extension") {
+        val tiffFile = createStorageManifestFileWith(name = "b1842700_0001.tiff")
+
+        val manifest = createStorageManifestWith(
+          space = StorageSpace("digitised"),
+          files = Seq(tiffFile)
+        )
+
+        TagRules.chooseTags(manifest) shouldBe Map(
+          tiffFile -> Map("Content-Type" -> "image/tiff")
+        )
+      }
+
+      it("whose file extension is uppercase") {
+        val tifFile = createStorageManifestFileWith(name = "b1842700_0001.TIF")
+
+        val manifest = createStorageManifestWith(
+          space = StorageSpace("digitised"),
+          files = Seq(tifFile)
+        )
+
+        TagRules.chooseTags(manifest) shouldBe Map(
+          tifFile -> Map("Content-Type" -> "image/tiff")
+        )
+      }
+    }
+
+    describe("it does not apply a tag") {
+      it("to non-TIFF files") {
+        val manifest = createStorageManifestWith(
+          space = StorageSpace("digitised"),
+          files = Seq(createStorageManifestFileWith(name = "b1842700_0001.jp2"))
+        )
+
+        TagRules.chooseTags(manifest) shouldBe empty
+      }
+
+      it("to TIFF files outside the digitised space") {
+        val manifest = createStorageManifestWith(
+          space = StorageSpace("born-digital"),
+          files = Seq(createStorageManifestFileWith(name = "330-19_19a.tif"))
+        )
+
+        TagRules.chooseTags(manifest) shouldBe empty
+      }
+    }
+  }
 }
