25eb0e5aaceab11ba8511135137950467e299d93
Alex Chan <a.chan@wellcome.org>
2021-05-13 17:15:25 +0100

    Use TypedString for ChecksumValue

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/verify/ChecksumValue.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/verify/ChecksumValue.scala
@@ -1,26 +1,15 @@
 package uk.ac.wellcome.platform.archive.common.verify
 
-import grizzled.slf4j.Logging
-import io.circe.{Decoder, Encoder, HCursor, Json}
-import org.scanamo.DynamoFormat
+import weco.json.{TypedString, TypedStringOps}
 
-case class ChecksumValue(value: String) {
-  override def toString: String = value
+class ChecksumValue(val value: String) extends TypedString[ChecksumValue] {
+  val underlying: String = value
 }
 
-object ChecksumValue extends Logging {
+object ChecksumValue extends TypedStringOps[ChecksumValue] {
+  override def apply(value: String): ChecksumValue =
+    new ChecksumValue(value)
+
   def create(raw: String): ChecksumValue =
     ChecksumValue(raw.trim)
-
-  implicit val encoder: Encoder[ChecksumValue] = (value: ChecksumValue) =>
-    Json.fromString(value.toString)
-
-  implicit val decoder: Decoder[ChecksumValue] = (cursor: HCursor) =>
-    cursor.value.as[String].map(ChecksumValue(_))
-
-  implicit def format: DynamoFormat[ChecksumValue] =
-    DynamoFormat.iso[ChecksumValue, String](
-      ChecksumValue(_),
-      _.toString
-    )
 }
