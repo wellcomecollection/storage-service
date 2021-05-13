6100e0edeb3fe4ef391135d50d82f2a32a819fb3
Alex Chan <a.chan@wellcome.org>
2021-05-13 17:10:41 +0100

    Use TypedString for ExternalIdentifier

--- common/src/main/scala/uk/ac/wellcome/platform/archive/common/bagit/models/ExternalIdentifier.scala
+++ common/src/main/scala/uk/ac/wellcome/platform/archive/common/bagit/models/ExternalIdentifier.scala
@@ -1,11 +1,10 @@
 package uk.ac.wellcome.platform.archive.common.bagit.models
 
-import org.scanamo.DynamoFormat
-import io.circe.{Decoder, Encoder, HCursor, Json}
+import uk.ac.wellcome.storage.TypedStringScanamoOps
+import weco.json.TypedString
 
-class ExternalIdentifier(val underlying: String) {
-  override def toString: String = underlying
-
+class ExternalIdentifier(val underlying: String)
+    extends TypedString[ExternalIdentifier] {
   require(underlying.nonEmpty, "External identifier cannot be empty")
 
   // When you want to see all versions of a bag in the bags API, you call
@@ -76,39 +75,9 @@ class ExternalIdentifier(val underlying: String) {
     underlying.matches(regex),
     s"External identifier must match regex: $regex"
   )
-
-  // Normally we use case classes for immutable data, which provide these
-  // methods for us.
-  //
-  // We deliberately don't use case classes here so we skip automatic
-  // case class derivation for JSON encoding/DynamoDB in Scanamo,
-  // and force callers to intentionally import the implicits below.
-  def canEqual(a: Any): Boolean = a.isInstanceOf[ExternalIdentifier]
-
-  override def equals(that: Any): Boolean =
-    that match {
-      case that: ExternalIdentifier =>
-        that.canEqual(this) && this.underlying == that.underlying
-      case _ => false
-    }
-
-  override def hashCode: Int = underlying.hashCode
 }
 
-object ExternalIdentifier {
+object ExternalIdentifier extends TypedStringScanamoOps[ExternalIdentifier] {
   def apply(underlying: String): ExternalIdentifier =
     new ExternalIdentifier(underlying)
-
-  implicit val encoder: Encoder[ExternalIdentifier] =
-    (value: ExternalIdentifier) => Json.fromString(value.toString)
-
-  implicit val decoder: Decoder[ExternalIdentifier] = (cursor: HCursor) =>
-    cursor.value.as[String].map(ExternalIdentifier(_))
-
-  implicit def evidence: DynamoFormat[ExternalIdentifier] =
-    DynamoFormat
-      .coercedXmap[ExternalIdentifier, String, IllegalArgumentException](
-        ExternalIdentifier(_),
-        _.underlying
-      )
 }
