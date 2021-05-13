f5e43fb630add03e9acecb7740afde893cfd6999
Alex Chan <a.chan@wellcome.org>
2021-05-13 17:13:17 +0100

    Use TypedString for ReplicaPath

--- replica_aggregator/src/main/scala/uk/ac/wellcome/platform/storage/replica_aggregator/models/ReplicaPath.scala
+++ replica_aggregator/src/main/scala/uk/ac/wellcome/platform/storage/replica_aggregator/models/ReplicaPath.scala
@@ -1,31 +1,19 @@
 package uk.ac.wellcome.platform.storage.replica_aggregator.models
 
-import io.circe.{Decoder, Encoder, HCursor, Json}
-import org.scanamo.DynamoFormat
 import uk.ac.wellcome.storage.azure.AzureBlobLocationPrefix
 import uk.ac.wellcome.storage.s3.S3ObjectLocationPrefix
-import uk.ac.wellcome.storage.{Location, Prefix}
+import uk.ac.wellcome.storage.{Location, Prefix, TypedStringScanamoOps}
+import weco.json.TypedString
 
-case class ReplicaPath(underlying: String) extends AnyVal {
-  override def toString: String = underlying
-}
+class ReplicaPath(val underlying: String) extends TypedString[ReplicaPath]
 
-case object ReplicaPath {
+object ReplicaPath extends TypedStringScanamoOps[ReplicaPath] {
+  override def apply(underlying: String): ReplicaPath =
+    new ReplicaPath(underlying)
+
   def apply(prefix: Prefix[_ <: Location]): ReplicaPath =
     prefix match {
       case S3ObjectLocationPrefix(_, keyPrefix)   => ReplicaPath(keyPrefix)
       case AzureBlobLocationPrefix(_, namePrefix) => ReplicaPath(namePrefix)
     }
-
-  implicit val encoder: Encoder[ReplicaPath] =
-    (value: ReplicaPath) => Json.fromString(value.toString)
-
-  implicit val decoder: Decoder[ReplicaPath] = (cursor: HCursor) =>
-    cursor.value.as[String].map(ReplicaPath(_))
-
-  implicit def evidence: DynamoFormat[ReplicaPath] =
-    DynamoFormat.iso[ReplicaPath, String](
-      ReplicaPath(_),
-      _.underlying
-    )
 }
