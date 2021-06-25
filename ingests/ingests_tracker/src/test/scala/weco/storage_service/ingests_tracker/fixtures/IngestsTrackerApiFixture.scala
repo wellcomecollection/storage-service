68f98f3b864905a19d71b186c89a66ccf27e7246
Alex Chan <a.chan@wellcome.org>
2021-06-25 18:03:23 +0100

    Get the ingest tracker tests passing

--- ingests/ingests_tracker/src/test/scala/weco/storage_service/ingests_tracker/fixtures/IngestsTrackerApiFixture.scala
+++ ingests/ingests_tracker/src/test/scala/weco/storage_service/ingests_tracker/fixtures/IngestsTrackerApiFixture.scala
@@ -4,25 +4,25 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.HttpMethods.{GET, POST}
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse, RequestEntity}
 import org.scalatest.concurrent.ScalaFutures.whenReady
+import org.scalatest.concurrent.{IntegrationPatience, PatienceConfiguration}
+import org.scalatest.time.{Seconds, Span}
 import weco.akka.fixtures.Akka
 import weco.fixtures.TestWith
 import weco.messaging.memory.MemoryMessageSender
+import weco.storage.maxima.memory.MemoryMaxima
+import weco.storage.store.memory.{MemoryStore, MemoryVersionedStore}
+import weco.storage.{StoreReadError, StoreWriteError, UpdateWriteError, Version}
 import weco.storage_service.generators.IngestGenerators
 import weco.storage_service.ingests.models.{Ingest, IngestID, IngestUpdate}
 import weco.storage_service.ingests_tracker.IngestsTrackerApi
-import weco.storage_service.ingests_tracker.services.{
-  CallbackNotificationService,
-  MessagingService
-}
+import weco.storage_service.ingests_tracker.services.{CallbackNotificationService, MessagingService}
 import weco.storage_service.ingests_tracker.tracker.IngestStoreUnexpectedError
 import weco.storage_service.ingests_tracker.tracker.memory.MemoryIngestTracker
-import weco.storage.{StoreReadError, StoreWriteError, UpdateWriteError, Version}
-import weco.storage.maxima.memory.MemoryMaxima
-import weco.storage.store.memory.{MemoryStore, MemoryVersionedStore}
 
 trait IngestsTrackerApiFixture
     extends IngestTrackerFixtures
     with IngestGenerators
+    with IntegrationPatience
     with Akka {
 
   val trackerUri = "http://localhost:8080"
@@ -114,7 +114,7 @@ trait IngestsTrackerApiFixture
   )(testWith: TestWith[HttpResponse, R]): R =
     withActorSystem { implicit actorSystem =>
       val request = Http().singleRequest(r)
-      whenReady(request) { response: HttpResponse =>
+      whenReady(request, timeout = PatienceConfiguration.Timeout(Span.apply(5, Seconds))) { response: HttpResponse =>
         testWith(response)
       }
     }
