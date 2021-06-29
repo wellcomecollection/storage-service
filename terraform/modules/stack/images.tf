4062088e7e33a4bf1c67577463fbaa9fa19f0e9b
Alex Chan <a.chan@wellcome.org>
2021-06-29 16:44:39 +0100

    The ECR repos live in the storage account, so hard-code the URL
    In particular, this will allow the module to be consumed in other accounts
    and read the images from our ECR repositories.

--- terraform/modules/stack/images.tf
+++ terraform/modules/stack/images.tf
@@ -21,12 +21,10 @@ locals {
   ]
 }
 
-data "aws_ecr_repository" "service" {
-  count = length(local.services)
-  name  = "uk.ac.wellcome/${local.services[count.index]}"
-}
-
 locals {
-  repo_urls = [for repo_url in data.aws_ecr_repository.service.*.repository_url : "${repo_url}:env.${var.release_label}"]
+  repo_urls = [
+    for name in local.services :
+    "975596993436.dkr.ecr.eu-west-1.amazonaws.com/uk.ac.wellcome/${name}:env.${var.release_label}"
+  ]
   image_ids = zipmap(local.services, local.repo_urls)
 }
