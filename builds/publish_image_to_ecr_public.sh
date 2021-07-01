c0e0ae70dae4adf8a3f8c48be3a054510fa5fdfc
Alex Chan <a.chan@wellcome.org>
2021-07-01 21:34:50 +0100

    tweak the ecr public + password

--- builds/publish_image_to_ecr_public.sh
+++ builds/publish_image_to_ecr_public.sh
@@ -12,14 +12,12 @@ CURRENT_COMMIT=$(git rev-parse HEAD)
 
 if [[ "${BUILDKITE:-}" = "true" ]]
 then
-  AWS_PROFILE=""
+  PASSWORD=$(aws ecr-public get-login-password --region us-east-1)
 else
-  AWS_PROFILE="storage-dev"
+  PASSWORD=$(AWS_PROFILE=storage-dev aws ecr-public get-login-password --region us-east-1)
 fi
 
-AWS_PROFILE="$AWS_PROFILE" \
-  aws ecr-public get-login-password --region us-east-1 | \
-  docker login --username AWS --password-stdin public.ecr.aws
+docker login --username AWS --password "$PASSWORD" public.ecr.aws
 
 docker tag "$PROJECT:$CURRENT_COMMIT" "$ECR_REGISTRY/$PROJECT:ref.$CURRENT_COMMIT"
 docker push "$ECR_REGISTRY/$PROJECT:ref.$CURRENT_COMMIT"
