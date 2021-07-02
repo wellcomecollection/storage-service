29a82e4732ff5005f50d03a5f3d9b04b3969ebd9
Alex Chan <a.chan@wellcome.org>
2021-07-02 08:55:40 +0100

    try using the storage-ci role to publish images

--- builds/publish_image_to_ecr_public.sh
+++ builds/publish_image_to_ecr_public.sh
@@ -17,7 +17,19 @@ pip3 install --user --upgrade awscli
 
 if [[ "${BUILDKITE:-}" = "true" ]]
 then
-  PASSWORD=$(/var/lib/buildkite-agent/.local/bin/aws ecr-public get-login-password --region us-east-1)
+  # We can only push to our ECR Public repo using the storage-ci role.
+  # At some point Terraform will support ECR Public Repository policies,
+  # at which point we can allow platform-ci to push to these repos and
+  # ditch these slightly hacky workaround.
+  #
+  # See https://github.com/hashicorp/terraform-provider-aws/issues/16540
+  echo "" >> ~/.aws/credentials
+  echo "[storage-ci-$BUILDKITE_BUILD_ID]"                   >> ~/.aws/credentials
+  echo "source_profile=default"                             >> ~/.aws/credentials
+  echo "role_arn=arn:aws:iam::975596993436:role/storage-ci" >> ~/.aws/credentials
+  echo "region=eu-west-1"                                   >> ~/.aws/credentials
+
+  PASSWORD=$(AWS_PROFILE="storage-ci-$BUILDKITE_BUILD_ID" /var/lib/buildkite-agent/.local/bin/aws ecr-public get-login-password --region us-east-1)
 else
   PASSWORD=$(AWS_PROFILE=storage-dev /var/lib/buildkite-agent/.local/bin/aws ecr-public get-login-password --region us-east-1)
 fi
