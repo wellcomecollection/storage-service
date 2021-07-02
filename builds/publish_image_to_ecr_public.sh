6657edb340570d04edad2498983cddf0c5636bbf
Alex Chan <a.chan@wellcome.org>
2021-07-02 09:13:47 +0100

    don't muck about with role arns

--- builds/publish_image_to_ecr_public.sh
+++ builds/publish_image_to_ecr_public.sh
@@ -12,26 +12,14 @@ CURRENT_COMMIT=$(git rev-parse HEAD)
 
 # The AWS CLI installed by default doesn't include the "ecr-public" commands,
 # so go ahead and grab the latest version, then use that.
-pip3 install --user --upgrade awscli
+pip3 install --user --upgrade boto3
 /var/lib/buildkite-agent/.local/bin/aws help --version
 
 if [[ "${BUILDKITE:-}" = "true" ]]
 then
-  # We can only push to our ECR Public repo using the storage-ci role.
-  # At some point Terraform will support ECR Public Repository policies,
-  # at which point we can allow platform-ci to push to these repos and
-  # ditch these slightly hacky workaround.
-  #
-  # See https://github.com/hashicorp/terraform-provider-aws/issues/16540
-  echo "" >> ~/.aws/credentials
-  echo "[storage-ci-$BUILDKITE_BUILD_ID]"                   >> ~/.aws/credentials
-  echo "source_profile=default"                             >> ~/.aws/credentials
-  echo "role_arn=arn:aws:iam::975596993436:role/storage-ci" >> ~/.aws/credentials
-  echo "region=eu-west-1"                                   >> ~/.aws/credentials
-
-  PASSWORD=$(AWS_PROFILE="storage-ci-$BUILDKITE_BUILD_ID" /var/lib/buildkite-agent/.local/bin/aws ecr-public get-login-password --region us-east-1)
+  PASSWORD=$(/var/lib/buildkite-agent/.local/bin/aws ecr-public get-login-password --region us-east-1)
 else
-  PASSWORD=$(AWS_PROFILE=storage-dev /var/lib/buildkite-agent/.local/bin/aws ecr-public get-login-password --region us-east-1)
+  PASSWORD=$(AWS_PROFILE=storage-dev aws ecr-public get-login-password --region us-east-1)
 fi
 
 docker login --username AWS --password "$PASSWORD" public.ecr.aws
