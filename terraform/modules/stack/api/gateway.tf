a8e916153a0045a0a172ac751168c8ffd97d6781
Alex Chan <a.chan@wellcome.org>
2021-07-06 15:11:34 +0100

    Don't create the AWS API Gateway stage explicitly
    The deployment will automatically create a stage for us, and this avoids
    us getting an error from Terraform when spinning up a new stack:
    
        Error creating API Gateway Stage: ConflictException: Stage already exists

--- terraform/modules/stack/api/gateway.tf
+++ terraform/modules/stack/api/gateway.tf
@@ -49,18 +49,6 @@ resource "aws_api_gateway_deployment" "v1" {
   }
 }
 
-resource "aws_api_gateway_stage" "v1" {
-  stage_name    = "v1"
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  deployment_id = aws_api_gateway_deployment.v1.id
-
-  variables = {
-    bags_port        = local.bags_listener_port
-    ingests_port     = local.ingests_listener_port,
-    integration_uris = local.integration_uri_variable
-  }
-}
-
 # Resources
 
 module "bags" {
