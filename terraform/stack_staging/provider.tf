42edddf23e0ba74bf2b5e91bf38906693d0f7771
Alex Chan <a.chan@wellcome.org>
2021-07-07 14:02:47 +0100

    fix tags in the staging satck

--- terraform/stack_staging/provider.tf
+++ terraform/stack_staging/provider.tf
@@ -1,6 +1,6 @@
 locals {
   default_tags = {
-    TerraformConfigurationURL = "https://github.com/wellcomecollection/storage-service/tree/main/terraform/stack_prod"
+    TerraformConfigurationURL = "https://github.com/wellcomecollection/storage-service/tree/main/terraform/stack_staging"
   }
 }
 
