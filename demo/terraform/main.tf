e78ef5d5f71e31c55e2f6bb40e6b10a36fd6ba0e
Alex Chan <a.chan@wellcome.org>
2021-07-13 21:07:50 +0100

    Bump module references in the demo stack README to 302cb69

--- demo/terraform/main.tf
+++ demo/terraform/main.tf
@@ -1,5 +1,5 @@
 module "demo_stack" {
-  source = "github.com/wellcomecollection/storage-service.git//demo/terraform/demo_stack?ref=06213c4"
+  source = "github.com/wellcomecollection/storage-service.git//demo/terraform/demo_stack?ref=302cb69"
 
   namespace       = "weco-dams-prototype"
   short_namespace = "weco"
