6d55162065239b75d043e554d7204e4aeccb7f2a
Alex Chan <a.chan@wellcome.org>
2021-07-14 12:09:38 +0100

    Bump module references in the demo stack to caab5b2

--- demo/terraform/demo_stack/metadata_stores.tf
+++ demo/terraform/demo_stack/metadata_stores.tf
@@ -1,5 +1,5 @@
 module "metadata_stores" {
-  source = "github.com/wellcomecollection/storage-service.git//terraform/modules/critical/metadata_stores?ref=82377ba"
+  source = "github.com/wellcomecollection/storage-service.git//terraform/modules/critical/metadata_stores?ref=caab5b2"
 
   namespace = var.namespace
 
--- demo/terraform/demo_stack/stack.tf
+++ demo/terraform/demo_stack/stack.tf
@@ -1,5 +1,5 @@
 module "stack" {
-  source = "github.com/wellcomecollection/storage-service.git//terraform/modules/stack?ref=82377ba"
+  source = "github.com/wellcomecollection/storage-service.git//terraform/modules/stack?ref=caab5b2"
 
   namespace = var.short_namespace
 
