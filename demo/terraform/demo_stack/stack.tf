a0979ee161fa8b5abbde4f58aa8f7d1deac8cc8b
Alex Chan <a.chan@wellcome.org>
2021-07-15 08:58:38 +0100

    Add min/max capacity arguments

--- demo/terraform/demo_stack/stack.tf
+++ demo/terraform/demo_stack/stack.tf
@@ -3,8 +3,8 @@ module "stack" {
 
   namespace = var.short_namespace
 
-  min_capacity = 0
-  max_capacity = 1
+  min_capacity = var.min_capacity
+  max_capacity = var.max_capacity
 
   vpc_id = module.vpc.vpc_id
 
