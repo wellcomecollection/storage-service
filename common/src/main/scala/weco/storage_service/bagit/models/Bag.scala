ef5edf440744536aefd59e84fa079567abd543b3
Alex Chan <a.chan@wellcome.org>
2021-07-21 15:05:59 +0100

    Add an assertion around consistent use of manifest algorithms

--- common/src/main/scala/weco/storage_service/bagit/models/Bag.scala
+++ common/src/main/scala/weco/storage_service/bagit/models/Bag.scala
@@ -25,6 +25,22 @@ case class Bag(
           path -> multiChecksum.sha256.get
       }
     )
+
+  // Quoting RFC 8493 § 2.2.1 (https://datatracker.ietf.org/doc/html/rfc8493#section-2.2.1):
+  //
+  //      Tag manifests SHOULD use the same algorithms as the payload manifests
+  //      that are present in the bag.
+  //
+  // This should already have been checked by the BagReader before creating
+  // an instance of this class, so we can display useful errors to users.
+  // This assertion is meant to prevent against programmer error in the
+  // storage service code, not malformed bags uploaded by users.
+  //
+  require(
+    (newManifest.entries.values ++ newTagManifest.entries.values)
+      .map(_.definedAlgorithms).toSeq.distinct.size == 1,
+    "Payload and tag manifests use different algorithms!"
+  )
 }
 
 case object Bag {
