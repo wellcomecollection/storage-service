14d7998561fb6de073e02f67a69459c8550579c4
Alex Chan <a.chan@wellcome.org>
2021-07-23 14:20:47 +0100

    Add a human-readable description to MismatchedChecksum

--- common/src/main/scala/weco/storage_service/checksum/MismatchedChecksum.scala
+++ common/src/main/scala/weco/storage_service/checksum/MismatchedChecksum.scala
@@ -4,4 +4,9 @@ case class MismatchedChecksum(
   algorithm: ChecksumAlgorithm,
   expected: ChecksumValue,
   actual: ChecksumValue
-)
+) {
+  require(expected != actual)
+
+  def description: String =
+    s"Expected ${algorithm.pathRepr}:$expected, saw ${algorithm.pathRepr}:$actual"
+}
