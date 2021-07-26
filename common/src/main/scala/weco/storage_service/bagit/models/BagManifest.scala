67b8cd87356811a77674a2e70890d3874b8edcee
Alex Chan <a.chan@wellcome.org>
2021-07-26 15:06:03 +0100

    Remove the now-unused BagManifest models

--- common/src/main/scala/weco/storage_service/bagit/models/BagManifest.scala
+++ common/src/main/scala/weco/storage_service/bagit/models/BagManifest.scala
@@ -2,24 +2,8 @@ package weco.storage_service.bagit.models
 
 import weco.storage_service.checksum.{
   ChecksumAlgorithm,
-  ChecksumValue,
   MultiManifestChecksum
 }
-
-sealed trait BagManifest {
-  val checksumAlgorithm: ChecksumAlgorithm
-  val entries: Map[BagPath, ChecksumValue]
-}
-
-case class PayloadManifest(
-  checksumAlgorithm: ChecksumAlgorithm,
-  entries: Map[BagPath, ChecksumValue]
-) extends BagManifest
-
-case class TagManifest(
-  checksumAlgorithm: ChecksumAlgorithm,
-  entries: Map[BagPath, ChecksumValue]
-) extends BagManifest
 
 sealed trait NewBagManifest {
   val algorithms: Set[ChecksumAlgorithm]
