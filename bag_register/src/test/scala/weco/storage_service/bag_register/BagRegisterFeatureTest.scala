1e5599ab5fb0a74ad6884bbfcad301de79999704
Alex Chan <a.chan@wellcome.org>
2021-09-08 19:27:28 +0100

    only run the affected tests

--- bag_register/src/test/scala/weco/storage_service/bag_register/BagRegisterFeatureTest.scala
+++ bag_register/src/test/scala/weco/storage_service/bag_register/BagRegisterFeatureTest.scala
@@ -30,82 +30,6 @@ class BagRegisterFeatureTest
     with PayloadGenerators
     with Eventually
     with IntegrationPatience {
-
-  it("sends an update if it registers a bag") {
-    val ingests = new MemoryMessageSender()
-
-    val storageManifestDao = createStorageManifestDao()
-
-    val createdAfterDate = Instant.now()
-    val space = createStorageSpace
-    val version = createBagVersion
-    val payloadFileCount = randomInt(1, 15)
-
-    withLocalS3Bucket { implicit bucket =>
-      val (bagRoot, bagInfo) = storeS3BagWith(
-        space = space,
-        version = version,
-        payloadFileCount = payloadFileCount
-      )
-
-      val bagId = BagId(
-        space = space,
-        externalIdentifier = bagInfo.externalIdentifier
-      )
-
-      val primaryLocation = PrimaryS3ReplicaLocation(
-        prefix = bagRoot
-      )
-
-      val knownReplicas = KnownReplicas(
-        location = primaryLocation,
-        replicas = List.empty
-      )
-
-      val payload = createKnownReplicasPayloadWith(
-        context = createPipelineContextWith(
-          storageSpace = space
-        ),
-        version = version,
-        knownReplicas = knownReplicas
-      )
-
-      withLocalSqsQueue() { queue =>
-        withBagRegisterWorker(
-          queue = queue,
-          ingests = ingests,
-          storageManifestDao = storageManifestDao
-        ) { _ =>
-          sendNotificationToSQS(queue, payload)
-
-          eventually {
-            val storageManifest: StorageManifest =
-              storageManifestDao.getLatest(bagId).value
-
-            storageManifest.space shouldBe bagId.space
-            storageManifest.info shouldBe bagInfo
-            storageManifest.manifest.files should have size payloadFileCount
-
-            storageManifest.location shouldBe PrimaryS3StorageLocation(
-              prefix = bagRoot
-                .copy(
-                  keyPrefix = bagRoot.keyPrefix.stripSuffix(s"/$version")
-                )
-            )
-
-            storageManifest.replicaLocations shouldBe empty
-
-            storageManifest.createdDate.isAfter(createdAfterDate) shouldBe true
-
-            assertBagRegisterSucceeded(ingests)
-
-            assertQueueEmpty(queue)
-          }
-        }
-      }
-    }
-  }
-
   it("can receive the same update twice") {
     val ingests = new MemoryMessageSender()
 
@@ -167,29 +91,6 @@ class BagRegisterFeatureTest
             }
           }
       }
-    }
-  }
-
-  it("handles a failed registration") {
-    val ingests = new MemoryMessageSender()
-
-    // This registration will fail because when the register tries to read the
-    // bag from the store, it won't find anything at the primary location
-    // in this payload.
-    val payload = createKnownReplicasPayload
-
-    withLocalSqsQueuePair() {
-      case QueuePair(queue, dlq) =>
-        withBagRegisterWorker(queue = queue, ingests = ingests) { _ =>
-          sendNotificationToSQS(queue, payload)
-
-          eventually {
-            assertBagRegisterFailed(ingests)
-
-            assertQueueEmpty(queue)
-            assertQueueEmpty(dlq)
-          }
-        }
     }
   }
 }
