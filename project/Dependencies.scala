a478fdbefdeaa83122c194f29bf40751ad6f5196
BuildKite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2022-01-26 09:37:53 +0000

    Bump scala-libs to v32.9.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object WellcomeDependencies {
-  val defaultVersion = "32.9.0" // This is automatically bumped by the scala-libs release process, do not edit this line manually
+  val defaultVersion = "32.9.1" // This is automatically bumped by the scala-libs release process, do not edit this line manually
 
   lazy val versions = new {
     val fixtures = defaultVersion
