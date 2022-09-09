f2ff48602f0fe1de743d8527693f7e3dcaeb560d
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2022-09-09 11:22:02 +0000

    Apply auto-formatting rules

--- indexer/reindexing/chunked_diff.py
+++ indexer/reindexing/chunked_diff.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 def chunked_diff(diff_for_chunk, all_entries):
     def _chunks(big_list, chunk_length):
         for i in range(0, len(big_list), chunk_length):
-            yield big_list[i: i + chunk_length]
+            yield big_list[i : i + chunk_length]
 
     chunk_length = 500
     chunk_count = math.ceil(len(all_entries) / chunk_length)
