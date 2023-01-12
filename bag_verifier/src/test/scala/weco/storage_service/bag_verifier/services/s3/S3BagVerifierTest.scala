2e7a0dd9f429d1c36b9a2439e424def37e01d673
Alex Chan <alex@alexwlchan.net>
2023-01-12 15:41:40 +0000

    remove another old V1 SDK bit

--- bag_verifier/src/test/scala/weco/storage_service/bag_verifier/services/s3/S3BagVerifierTest.scala
+++ bag_verifier/src/test/scala/weco/storage_service/bag_verifier/services/s3/S3BagVerifierTest.scala
@@ -1,8 +1,5 @@
 package weco.storage_service.bag_verifier.services.s3
 
-import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
-import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration
-import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
 import weco.fixtures.TestWith
 import weco.storage_service.bag_verifier.models.{BagVerifyContext, ReplicatedBagVerifyContext, StandaloneBagVerifyContext}
 import weco.storage_service.bag_verifier.services._
@@ -55,14 +52,6 @@ trait S3BagVerifierTests[Verifier <: BagVerifier[
   override val bagBuilder
     : BagBuilder[S3ObjectLocation, S3ObjectLocationPrefix, Bucket] =
     new S3BagBuilder {}
-
-  implicit val amazonS3: AmazonS3 =
-    AmazonS3ClientBuilder.standard()
-      .withCredentials(new AWSStaticCredentialsProvider(
-        new BasicAWSCredentials("accessKey1", "verySecretKey1")))
-      .withPathStyleAccessEnabled(true)
-      .withEndpointConfiguration(new EndpointConfiguration("http://localhost:33333", "localhost"))
-      .build()
 }
 
 class S3ReplicatedBagVerifierTest
