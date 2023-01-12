691e14c7adbe9ad1b2866ff7b298098ae1c173ce
Alex Chan <alex@alexwlchan.net>
2023-01-12 15:16:31 +0000

    Get rid of the V1 SDK when using S3Locatable

--- bag_verifier/src/main/scala/weco/storage_service/bag_verifier/storage/s3/S3Locatable.scala
+++ bag_verifier/src/main/scala/weco/storage_service/bag_verifier/storage/s3/S3Locatable.scala
@@ -1,32 +1,34 @@
 package weco.storage_service.bag_verifier.storage.s3
 
-import java.net.URI
-
-import com.amazonaws.services.s3.AmazonS3URI
-import weco.storage_service.bag_verifier.storage.{
-  Locatable,
-  LocateFailure,
-  LocationParsingError
-}
 import weco.storage.s3.{S3ObjectLocation, S3ObjectLocationPrefix}
+import weco.storage_service.bag_verifier.storage.{Locatable, LocateFailure, LocationParsingError}
 
-import scala.util.{Failure, Success, Try}
+import java.net.URI
 
 object S3Locatable {
   implicit val s3UriLocatable
-    : Locatable[S3ObjectLocation, S3ObjectLocationPrefix, URI] =
+  : Locatable[S3ObjectLocation, S3ObjectLocationPrefix, URI] =
     new Locatable[S3ObjectLocation, S3ObjectLocationPrefix, URI] {
       override def locate(uri: URI)(
         maybeRoot: Option[S3ObjectLocationPrefix]
       ): Either[LocateFailure[URI], S3ObjectLocation] =
-        Try { new AmazonS3URI(uri) } match {
-          case Success(s3Uri) =>
-            Right(
-              S3ObjectLocation(bucket = s3Uri.getBucket, key = s3Uri.getKey)
-            )
+        uri.getScheme match {
+          case "s3" => {
+            val bucket = uri.getAuthority
 
-          // We are not running in AWS - manually parse URL
-          case Failure(_) if uri.getHost == "localhost" =>
+            val path = uri.getPath
+            val key =
+              if (path.length <= 1) { // s3://bucket or s3://bucket/
+                ""
+              } else { // s3://bucket/key
+                // Remove the leading '/'.
+                uri.getPath.substring(1)
+              }
+
+            Right(S3ObjectLocation(bucket, key))
+          }
+
+          case "http" if uri.getHost == "localhost" =>
             uri.getPath.split("/").toList match {
               case _ :: head :: tail =>
                 Right(S3ObjectLocation(bucket = head, key = tail.mkString("/")))
@@ -40,14 +42,7 @@ object S3Locatable {
                 )
             }
 
-          // We are running in AWS - fail as usual.
-          case Failure(e) =>
-            Left(
-              LocationParsingError(
-                uri,
-                s"Failed to parse S3 URI: ${e.getMessage}"
-              )
-            )
+          case _ => Left(LocationParsingError(uri, "Failed to parse S3 URI"))
         }
     }
 }
