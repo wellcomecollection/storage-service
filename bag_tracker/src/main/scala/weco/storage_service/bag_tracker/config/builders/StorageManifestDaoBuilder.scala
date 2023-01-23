d5766ef309869efee8afa6cf710f46cd1080e34b
Alex Chan <alex@alexwlchan.net>
2023-01-23 09:52:56 +0000

    remove another old-style builder

--- bag_tracker/src/main/scala/weco/storage_service/bag_tracker/config/builders/StorageManifestDaoBuilder.scala
+++ bag_tracker/src/main/scala/weco/storage_service/bag_tracker/config/builders/StorageManifestDaoBuilder.scala
@@ -3,14 +3,14 @@ package weco.storage_service.bag_tracker.config.builders
 import com.typesafe.config.Config
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 import software.amazon.awssdk.services.s3.S3Client
+import weco.storage.typesafe.{DynamoBuilder, S3Builder}
 import weco.storage_service.bag_tracker.storage.StorageManifestDao
 import weco.storage_service.bag_tracker.storage.dynamo.DynamoStorageManifestDao
-import weco.storage.typesafe.{DynamoBuilder, S3Builder}
 
 object StorageManifestDaoBuilder {
   def build(config: Config): StorageManifestDao = {
     implicit val dynamoClient: DynamoDbClient =
-      DynamoBuilder.buildDynamoClient
+      DynamoDbClient.builder().build()
 
     implicit val s3Client: S3Client =
       S3Client.builder().build()
