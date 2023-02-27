08625fda22427b8f50df8d6bc59deab5b6040492
jamie <j.parkinson@wellcome.ac.uk>
2023-02-27 11:09:25 +0000

    Pin fluentbit container tags to the latest version

--- terraform/stack_staging/main.tf
+++ terraform/stack_staging/main.tf
@@ -93,7 +93,7 @@ module "stack_staging" {
   logging_container = {
     container_registry = "760097843905.dkr.ecr.eu-west-1.amazonaws.com/uk.ac.wellcome"
     container_name     = "fluentbit"
-    container_tag      = "0fe256a94441ceff15629344d4225fbb64457fdd"
+    container_tag      = "ac9e48d11d76f3d3abd64e4a9440462539da2e7a"
   }
 
   nginx_container = {
