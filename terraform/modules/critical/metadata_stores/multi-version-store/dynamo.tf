24c92a628bf1acb6e39713126ceab48ae54e8b66
Alex Chan <alex@alexwlchan.net>
2023-05-18 10:12:55 +0100

    tidy up a few other VHS bits

--- terraform/modules/critical/metadata_stores/multi-version-store/dynamo.tf
+++ terraform/modules/critical/metadata_stores/multi-version-store/dynamo.tf
@@ -2,8 +2,6 @@ resource "aws_dynamodb_table" "table" {
   name             = var.table_name
   hash_key         = "id"
   range_key        = "version"
-  stream_enabled   = true
-  stream_view_type = "NEW_AND_OLD_IMAGES"
 
   billing_mode = "PAY_PER_REQUEST"
 
--- terraform/modules/critical/metadata_stores/multi-version-store/s3.tf
+++ terraform/modules/critical/metadata_stores/multi-version-store/s3.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "bucket" {
-  bucket = var.bucket_name
+  bucket = aws_s3_bucket.bucket.id
 
   rule {
     id     = "objects_to_standard_ia"
