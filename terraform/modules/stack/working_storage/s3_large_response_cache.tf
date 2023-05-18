7338f5970a504df037652ba7a86cb134fa73f6bc
Alex Chan <alex@alexwlchan.net>
2023-05-18 11:18:50 +0100

    update the stack module to the newest terraform provider

--- terraform/modules/stack/working_storage/s3_large_response_cache.tf
+++ terraform/modules/stack/working_storage/s3_large_response_cache.tf
@@ -1,9 +1,18 @@
 resource "aws_s3_bucket" "large_response_cache" {
   bucket = "${var.bucket_name_prefix}${var.namespace}-large-response-cache"
-  acl    = "private"
+}
 
-  lifecycle_rule {
-    enabled = true
+resource "aws_s3_bucket_acl" "large_response_cache" {
+  bucket = aws_s3_bucket.large_response_cache.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "large_response_cache" {
+  bucket = aws_s3_bucket.large_response_cache.id
+
+  rule {
+    id           = "transition-to-standard-id"
+    status = "Enabled"
 
     transition {
       days          = 30
@@ -11,4 +20,3 @@ resource "aws_s3_bucket" "large_response_cache" {
     }
   }
 }
-
--- terraform/modules/stack/working_storage/s3_unpacked_bags.tf
+++ terraform/modules/stack/working_storage/s3_unpacked_bags.tf
@@ -1,13 +1,21 @@
 resource "aws_s3_bucket" "unpacked_bags" {
   bucket = "${var.bucket_name_prefix}${var.namespace}-unpacked-bags"
-  acl    = "private"
-
-  lifecycle_rule {
-    expiration {
-      days = "30"
-    }
-
-    enabled = true
-  }
 }
 
+resource "aws_s3_bucket_acl" "unpacked_bags" {
+  bucket = aws_s3_bucket.unpacked_bags.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "unpacked_bags" {
+  bucket = aws_s3_bucket.unpacked_bags.id
+
+  rule {
+    id           = "expire-after-30-days"
+    status = "Enabled"
+
+    expiration {
+      days          = 30
+    }
+  }
+}
