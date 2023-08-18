2828b6d880a132df7b4ca38962a9e424ae4e34b5
Alex Chan <alex@alexwlchan.net>
2023-08-18 10:48:41 +0100

    Use the new Lambda module for our monitoring Lambdas

--- terraform/modules/lambda/lambda_function.tf
+++ terraform/modules/lambda/lambda_function.tf
@@ -3,40 +3,45 @@ data "aws_s3_object" "package" {
   key    = var.s3_key
 }
 
-resource "aws_lambda_function" "lambda_function" {
-  description   = var.description
-  function_name = var.name
+moved {
+  from = aws_lambda_function.lambda_function
+  to   = module.lambda.aws_lambda_function.main
+}
+
+moved {
+  from = aws_iam_role.iam_role
+  to   = module.lambda.aws_iam_role.lambda
+}
+
+moved {
+  from = aws_cloudwatch_metric_alarm.lambda
+  to   = module.lambda.aws_cloudwatch_metric_alarm.lambda_errors["arn:aws:sns:eu-west-1:975596993436:storage_lambda_error_alarm"]
+}
+
+moved {
+  from = aws_cloudwatch_log_group.cloudwatch_log_group
+  to   = module.lambda.aws_cloudwatch_log_group.lambda
+}
+
+module "lambda" {
+  source = "github.com/wellcomecollection/terraform-aws-lambda.git?ref=v1.2.0"
+
+  name        = var.name
+  description = var.description
+
+  error_alarm_topic_arn = var.lambda_error_alerts_topic_arn
 
   s3_bucket         = var.s3_bucket
   s3_key            = var.s3_key
   s3_object_version = data.aws_s3_object.package.version_id
 
-  role    = aws_iam_role.iam_role.arn
   handler = var.module_name == "" ? "${var.name}.main" : "${var.module_name}.main"
   runtime = "python3.8"
   timeout = var.timeout
 
   memory_size = var.memory_size
 
-  environment {
+  environment = {
     variables = var.environment
   }
-}
-
-resource "aws_cloudwatch_metric_alarm" "lambda" {
-  alarm_name          = "lambda-${var.name}-errors"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "1"
-  metric_name         = "Errors"
-  namespace           = "AWS/Lambda"
-  period              = "60"
-  statistic           = "Sum"
-  threshold           = "1"
-
-  dimensions = {
-    FunctionName = var.name
-  }
-
-  alarm_description = "This metric monitors Lambda errors in the function ${var.name}"
-  alarm_actions     = [var.lambda_error_alerts_topic_arn]
 }
