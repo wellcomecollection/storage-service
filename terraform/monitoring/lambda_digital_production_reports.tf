59ca2178ce55dfba38d10808564e097ca7b21fb1
Alex Chan <alex@alexwlchan.net>
2023-08-18 10:53:04 +0100

    we can remove a layer of indirection

--- terraform/monitoring/lambda_digital_production_reports.tf
+++ terraform/monitoring/lambda_digital_production_reports.tf
@@ -11,18 +11,28 @@ resource "aws_s3_object" "digital_production_report_lambda" {
   etag   = data.archive_file.digital_production_report_lambda.output_md5
 }
 
-module "digital_production_report_lambda" {
-  source = "../modules/lambda"
+moved {
+  from = module.digital_production_report_lambda.module.lambda
+  to   = module.digital_production_report_lambda2
+}
+
+module "digital_production_report_lambda2" {
+  source = "github.com/wellcomecollection/terraform-aws-lambda.git?ref=v1.2.0"
 
   name        = "digital_production_report"
   description = "Create monthly/quarterly reports for the Digital Production team"
 
-  s3_bucket = aws_s3_object.digital_production_report_lambda.bucket
-  s3_key    = aws_s3_object.digital_production_report_lambda.key
+  error_alarm_topic_arn = local.lambda_error_alerts_topic_arn
 
+  s3_bucket         = aws_s3_object.digital_production_report_lambda.bucket
+  s3_key            = aws_s3_object.digital_production_report_lambda.key
+  s3_object_version = aws_s3_object.digital_production_report_lambda.version_id
+
+  handler = "digital_production_report.main"
+  runtime = "python3.8"
   timeout = 300
 
-  lambda_error_alerts_topic_arn = local.lambda_error_alerts_topic_arn
+  memory_size = 128
 }
 
 data "aws_iam_policy_document" "read_reporting_secrets" {
@@ -39,7 +49,7 @@ data "aws_iam_policy_document" "read_reporting_secrets" {
 }
 
 resource "aws_iam_role_policy" "allow_production_report_to_read_secrets" {
-  role   = module.digital_production_report_lambda.role_name
+  role   = module.digital_production_report_lambda2.lambda_role.name
   policy = data.aws_iam_policy_document.read_reporting_secrets.json
 }
 
@@ -52,12 +62,12 @@ resource "aws_cloudwatch_event_rule" "first_of_month_at_7am" {
 
 resource "aws_lambda_permission" "allow_production_report_cloudwatch_trigger" {
   action        = "lambda:InvokeFunction"
-  function_name = module.digital_production_report_lambda.function_name
+  function_name = module.digital_production_report_lambda2.lambda.function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.first_of_month_at_7am.arn
 }
 
 resource "aws_cloudwatch_event_target" "first_of_month_at_7am" {
   rule = aws_cloudwatch_event_rule.first_of_month_at_7am.name
-  arn  = module.digital_production_report_lambda.arn
+  arn  = module.digital_production_report_lambda2.lambda.arn
 }
