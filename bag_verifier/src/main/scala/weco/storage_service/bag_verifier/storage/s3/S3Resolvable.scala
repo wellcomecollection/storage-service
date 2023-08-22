d0fffed89170d64b83d63eb350d9985de21b6810
Alex Chan <alex@alexwlchan.net>
2023-08-22 21:43:44 +0100

    okay but actually use a class we have

--- bag_verifier/src/main/scala/weco/storage_service/bag_verifier/storage/s3/S3Resolvable.scala
+++ bag_verifier/src/main/scala/weco/storage_service/bag_verifier/storage/s3/S3Resolvable.scala
@@ -1,13 +1,13 @@
 package weco.storage_service.bag_verifier.storage.s3
 
-import java.net.URI
+import java.net.{URI, URLEncoder}
 
 import weco.storage_service.bag_verifier.storage.Resolvable
 import weco.storage.providers.s3.S3ObjectLocation
 
 class S3Resolvable extends Resolvable[S3ObjectLocation] {
   override def resolve(location: S3ObjectLocation): URI = {
-    val encodedKey = UriUtils.encodePath(location.key, "UTF-8")
+    val encodedKey = URLEncoder.encode(location.key, "UTF-8")
     new URI(s"s3://${location.bucket}/$encodedKey")
   }
 }
