6a131af055686ba256843f3e9a0db1fcf46057cf
Alex Chan <alex@alexwlchan.net>
2023-08-22 22:24:38 +0100

    Fix the encoding of spaces

--- bag_verifier/src/main/scala/weco/storage_service/bag_verifier/storage/s3/S3Resolvable.scala
+++ bag_verifier/src/main/scala/weco/storage_service/bag_verifier/storage/s3/S3Resolvable.scala
@@ -7,7 +7,7 @@ import weco.storage.providers.s3.S3ObjectLocation
 
 class S3Resolvable extends Resolvable[S3ObjectLocation] {
   override def resolve(location: S3ObjectLocation): URI = {
-    val encodedKey = URLEncoder.encode(location.key, "UTF-8")
+    val encodedKey = URLEncoder.encode(location.key, "UTF-8").replace("+", "%20")
     new URI(s"s3://${location.bucket}/$encodedKey")
   }
 }
