c3c9c20da923279e03ce22cb19b1cf92d0b485e7
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2023-08-22 21:30:06 +0000

    Apply auto-formatting rules

--- bag_verifier/src/main/scala/weco/storage_service/bag_verifier/storage/s3/S3Resolvable.scala
+++ bag_verifier/src/main/scala/weco/storage_service/bag_verifier/storage/s3/S3Resolvable.scala
@@ -7,7 +7,8 @@ import weco.storage.providers.s3.S3ObjectLocation
 
 class S3Resolvable extends Resolvable[S3ObjectLocation] {
   override def resolve(location: S3ObjectLocation): URI = {
-    val encodedKey = URLEncoder.encode(location.key, "UTF-8").replace("+", "%20")
+    val encodedKey =
+      URLEncoder.encode(location.key, "UTF-8").replace("+", "%20")
     new URI(s"s3://${location.bucket}/$encodedKey")
   }
 }
