0b13e6df071dd66ed29e3f490f4bb139813afbc1
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2023-08-29 13:24:22 +0000

    Apply auto-formatting rules

--- indexer/reindexing/chunked_diff.py
+++ indexer/reindexing/chunked_diff.py
@@ -11,6 +11,9 @@ def chunked_diff(diff_for_chunk, all_entries):
     chunk_length = 500
     chunk_count = math.ceil(len(all_entries) / chunk_length)
 
-    diff_list = (diff_for_chunk(chunk) for chunk in tqdm(_chunks(all_entries, chunk_length), total=chunk_count))
+    diff_list = (
+        diff_for_chunk(chunk)
+        for chunk in tqdm(_chunks(all_entries, chunk_length), total=chunk_count)
+    )
 
     return [item for sublist in diff_list for item in sublist]
