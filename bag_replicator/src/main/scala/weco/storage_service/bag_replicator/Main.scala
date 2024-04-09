e77dbe11ec484eb452bbc1c3f7f51eaa90c46893
Robert Kenny <r.kenny@wellcome.org>
2024-04-09 10:15:49 +0100

    use crt libs

--- bag_replicator/src/main/scala/weco/storage_service/bag_replicator/Main.scala
+++ bag_replicator/src/main/scala/weco/storage_service/bag_replicator/Main.scala
@@ -3,7 +3,7 @@ package weco.storage_service.bag_replicator
 import akka.actor.ActorSystem
 import com.azure.storage.blob.{BlobServiceClient, BlobServiceClientBuilder}
 import com.typesafe.config.Config
-import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.{S3AsyncClient, S3Client}
 import software.amazon.awssdk.services.s3.presigner.S3Presigner
 import software.amazon.awssdk.services.sqs.SqsAsyncClient
 import software.amazon.awssdk.transfer.s3.S3TransferManager
@@ -24,16 +24,8 @@ import weco.storage_service.bag_replicator.replicator.azure.AzureReplicator
 import weco.storage_service.bag_replicator.replicator.models.ReplicationSummary
 import weco.storage_service.bag_replicator.replicator.s3.S3Replicator
 import weco.storage_service.bag_replicator.services.BagReplicatorWorker
-import weco.storage_service.config.builders.{
-  IngestUpdaterBuilder,
-  OperationNameBuilder,
-  OutgoingPublisherBuilder
-}
-import weco.storage_service.ingests.models.{
-  AmazonS3StorageProvider,
-  AzureBlobStorageProvider,
-  StorageProvider
-}
+import weco.storage_service.config.builders.{IngestUpdaterBuilder, OperationNameBuilder, OutgoingPublisherBuilder}
+import weco.storage_service.ingests.models.{AmazonS3StorageProvider, AzureBlobStorageProvider, StorageProvider}
 import weco.storage_service.storage.models.IngestStepResult
 import weco.typesafe.WellcomeTypesafeApp
 import weco.typesafe.config.builders.EnrichConfig._
@@ -52,8 +44,13 @@ object Main extends WellcomeTypesafeApp {
 
     implicit val s3Client: S3Client =
       S3Client.builder().build()
+
+    implicit val s3AsyncClient: S3AsyncClient =
+      S3AsyncClient.crtBuilder().build()
+
     implicit val s3TransferManager: S3TransferManager =
-      S3TransferManager.builder().build()
+      S3TransferManager.builder().s3Client(s3AsyncClient).build()
+
     implicit val s3Presigner: S3Presigner =
       S3Presigner.builder().build()
 
