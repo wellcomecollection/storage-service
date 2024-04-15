238369ac64fc54328d8c9826d4aa1a6c149c6b7a
Agnes Garoux <a.garoux@wellcome.ac.uk>
2024-04-15 15:46:13 +0100

    use S3AsyncClient.crtBuilder()

--- bag_replicator/src/test/scala/weco/storage/transfer/fixtures/S3TransferManagerFixtures.scala
+++ bag_replicator/src/test/scala/weco/storage/transfer/fixtures/S3TransferManagerFixtures.scala
@@ -1,7 +1,6 @@
 package weco.storage.transfer.fixtures
 
-//import software.amazon.awssdk.services.s3.S3AsyncClient
-import software.amazon.awssdk.services.s3.internal.crt.S3CrtAsyncClient
+import software.amazon.awssdk.services.s3.S3AsyncClient
 import software.amazon.awssdk.transfer.s3.S3TransferManager
 import weco.storage.fixtures.S3Fixtures
 
@@ -10,7 +9,7 @@ import java.net.URI
 trait S3TransferManagerFixtures extends S3Fixtures {
   def createS3TransferManagerWithEndpoint(endpoint: String): S3TransferManager = {
     val s3AsyncClient =
-      S3CrtAsyncClient.builder()
+      S3AsyncClient.crtBuilder()
         .credentialsProvider(s3Credentials)
         .forcePathStyle(true)
         .endpointOverride(new URI(endpoint))
