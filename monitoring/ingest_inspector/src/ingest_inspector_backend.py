db36d8b0e21761fb06f5ed2287a9c5fc45bd867f
Štěpán Brychta <stepan.brychta@me.com>
2024-06-19 14:08:05 +0100

    Create backend lambda (#1130)

---
+++ monitoring/ingest_inspector/src/ingest_inspector_backend.py
@@ -0,0 +1,61 @@
+import json
+import functools
+
+import boto3
+from wellcome_storage_service import RequestsOAuthStorageServiceClient, IngestNotFound
+
+
+STAGING_URL = "https://api-stage.wellcomecollection.org/storage/v1"
+PRODUCTION_URL = "https://api.wellcomecollection.org/storage/v1"
+
+
+def _client_from_environment(api_url):
+    secretsmanager = boto3.Session().client("secretsmanager")
+
+    def _get_secretsmanager_value(secret_id: str):
+        return secretsmanager.get_secret_value(SecretId=secret_id)["SecretString"]
+
+    client_id = _get_secretsmanager_value("ingest-inspector/cognito-client-id")
+    client_secret = _get_secretsmanager_value("ingest-inspector/cognito-client-secret")
+
+    return RequestsOAuthStorageServiceClient(
+        api_url=api_url,
+        client_id=client_id,
+        client_secret=client_secret,
+        token_url="https://auth.wellcomecollection.org/oauth2/token"
+    )
+
+
+@functools.lru_cache()
+def get_prod_client():
+    return _client_from_environment(PRODUCTION_URL)
+
+
+@functools.lru_cache()
+def get_staging_client():
+    return _client_from_environment(STAGING_URL)
+
+
+def lambda_handler(event, context):
+    print(f"Starting lambda_handler, got event: {event}")
+    
+    ingest_id = event["pathParameters"]["ingest_id"]
+    
+    try:
+        ingest = get_prod_client().get_ingest(ingest_id=ingest_id)
+        environment = "production"
+    except IngestNotFound:
+        ingest = get_staging_client().get_ingest(ingest_id=ingest_id)
+        environment = "staging"
+
+    response = {
+        "environment": environment,
+        "ingest": ingest
+    }
+
+    return {"status": 200, "body": json.dumps(response)}
+
+
+if __name__ == "__main__":
+    event = {"pathParameters": {"ingest_id": "bd62a401-11c6-46c9-bdc3-346b01f1d05b"}}
+    print(lambda_handler(event, None))
---
+++ terraform/monitoring/ingest_inspector_lambda.tf
@@ -0,0 +1,79 @@
+data "aws_s3_object" "ingest_inspector" {
+  bucket = "wellcomecollection-storage-infra"
+  key    = "lambdas/monitoring/ingest_inspector_backend.zip"
+}
+
+module "ingest_inspector_lambda" {
+  source = "git@github.com:wellcomecollection/terraform-aws-lambda?ref=v1.2.0"
+
+  name        = "ingest_inspector_backend"
+  description = "API endpoint for the Ingest Inspector app."
+  runtime     = "python3.10"
+
+  s3_bucket         = data.aws_s3_object.ingest_inspector.bucket
+  s3_key            = data.aws_s3_object.ingest_inspector.key
+  s3_object_version = data.aws_s3_object.ingest_inspector.version_id
+
+  handler     = "ingest_inspector_backend.lambda_handler"
+  memory_size = 512
+  timeout     = 60 // 1 minute
+
+  environment = {
+    variables = {
+      "HOME" = "dummy_value"
+    }
+  }
+
+#  error_alarm_topic_arn = local.lambda_error_alerts_topic_arn
+}
+
+data "aws_iam_policy_document" "allow_secret_read" {
+  statement {
+    actions = ["secretsmanager:GetSecretValue"]
+    resources = [
+      aws_secretsmanager_secret.ingest_inspector_cognito_client_id.arn,
+      aws_secretsmanager_secret.ingest_inspector_cognito_client_secret.arn
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "read_secrets_policy" {
+  role   = module.ingest_inspector_lambda.lambda_role.name
+  policy = data.aws_iam_policy_document.allow_secret_read.json
+}
+
+resource "aws_apigatewayv2_api" "ingest_inspector_api" {
+  name          = "Ingest Inspector API"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "ingest_inspector_api_v1" {
+  api_id = aws_apigatewayv2_api.ingest_inspector_api.id
+
+  name        = "v1"
+  auto_deploy = true
+}
+
+resource "aws_apigatewayv2_integration" "ingest_inspector_api_lambda_integration" {
+  api_id = aws_apigatewayv2_api.ingest_inspector_api.id
+
+  integration_uri    = module.ingest_inspector_lambda.lambda.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "get_ingest_endpoint" {
+  api_id = aws_apigatewayv2_api.ingest_inspector_api.id
+
+  route_key = "GET /ingest/{ingest_id}"
+  target    = "integrations/${aws_apigatewayv2_integration.ingest_inspector_api_lambda_integration.id}"
+}
+
+resource "aws_lambda_permission" "allow_api_gateway_to_trigger_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = module.ingest_inspector_lambda.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.ingest_inspector_api.execution_arn}/*/*"
+}
