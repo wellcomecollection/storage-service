950affe4488e4f73d1fad990260a4b85176d499c
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2024-07-03 13:35:28 +0000

    Apply auto-formatting rules

--- terraform/monitoring/ingest_inspector/certificate.tf
+++ terraform/monitoring/ingest_inspector/certificate.tf
@@ -2,7 +2,7 @@ module "ingest_inspector_certificate" {
   source = "github.com/wellcomecollection/terraform-aws-acm-certificate?ref=v1.0.0"
 
   domain_name = var.domain_name
-  zone_id = data.aws_route53_zone.weco_zone.id
+  zone_id     = data.aws_route53_zone.weco_zone.id
 
   providers = {
     # The ACM certificate must be created in the us-east-1 region to work with CloudFront
