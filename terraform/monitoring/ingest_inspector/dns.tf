0d69500410ab2c4ef6434fbfc82a1bc61ea7d0eb
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2024-07-03 10:32:30 +0000

    Apply auto-formatting rules

--- terraform/monitoring/ingest_inspector/dns.tf
+++ terraform/monitoring/ingest_inspector/dns.tf
@@ -2,9 +2,9 @@
 # to the CloudFront distribution
 resource "aws_route53_record" "cdn" {
   provider = aws.dns
-  zone_id = data.aws_route53_zone.weco_zone.id
-  name    = var.domain_name
-  type    = "A"
+  zone_id  = data.aws_route53_zone.weco_zone.id
+  name     = var.domain_name
+  type     = "A"
 
   alias {
     name                   = aws_cloudfront_distribution.ingest_inspector_cloudfront_distribution.domain_name
--- terraform/monitoring/ingest_inspector/provider.tf
+++ terraform/monitoring/ingest_inspector/provider.tf
@@ -13,5 +13,5 @@ provider "aws" {
   }
 
   region = "us-east-1"
-  alias = "us-east-1"
+  alias  = "us-east-1"
 }
