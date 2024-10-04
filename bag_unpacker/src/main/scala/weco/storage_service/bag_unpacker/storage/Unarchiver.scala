ac76976ef8f6e597ee812f782baa519fd53524d0
Github on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2024-10-04 14:02:22 +0000

    Apply auto-formatting rules

--- bag_unpacker/src/main/scala/weco/storage_service/bag_unpacker/storage/Unarchiver.scala
+++ bag_unpacker/src/main/scala/weco/storage_service/bag_unpacker/storage/Unarchiver.scala
@@ -41,8 +41,8 @@ import scala.util.{Failure, Success, Try}
   */
 object Unarchiver {
   def open(
-            inputStream: InputStream
-          ): Either[UnarchiverError, Iterator[(ArchiveEntry, InputStream)]] =
+    inputStream: InputStream
+  ): Either[UnarchiverError, Iterator[(ArchiveEntry, InputStream)]] =
     for {
       uncompressedStream <- uncompress(inputStream)
       archiveInputStream <- extract(uncompressedStream)
@@ -50,8 +50,8 @@ object Unarchiver {
     } yield iterator
 
   private def createIterator(
-                              archiveInputStream: ArchiveInputStream
-                            ): Iterator[(ArchiveEntry, InputStream)] =
+    archiveInputStream: ArchiveInputStream
+  ): Iterator[(ArchiveEntry, InputStream)] =
     new Iterator[(ArchiveEntry, InputStream)] {
       private var latest: ArchiveEntry = _
       private var seen: Set[ArchiveEntry] = Set.empty
@@ -84,9 +84,9 @@ object Unarchiver {
       new CompressorStreamFactory()
         .createCompressorInputStream(new BufferedInputStream(compressedStream))
     } match {
-      case Success(stream) => Right(stream)
+      case Success(stream)                   => Right(stream)
       case Failure(err: CompressorException) => Left(CompressorError(err))
-      case Failure(err) => Left(UnexpectedUnarchiverError(err))
+      case Failure(err)                      => Left(UnexpectedUnarchiverError(err))
     }
   private def extract(
     inputStream: InputStream
