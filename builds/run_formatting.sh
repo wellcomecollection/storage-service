76cc8c89c880e85e9e405ae46ac120cd168852ac
Robert Kenny <r.kenny@wellcome.org>
2026-05-11 14:56:52 +0100

    use mirror of scalafmt

--- builds/run_formatting.sh
+++ builds/run_formatting.sh
@@ -10,12 +10,12 @@ ROOT=$(git rev-parse --show-toplevel)
 docker run --tty --rm \
 	--volume "$ROOT:/repo" \
 	--workdir /repo \
-	"public.ecr.aws/hashicorp/terraform:light" fmt -recursive
+	"$ECR_REGISTRY/terraform:light" fmt -recursive
 
 ./builds/run_sbt_task_in_docker.sh "scalafmt"
 
 docker run --tty --rm \
 	--volume "$ROOT:/repo" \
-  --workdir /repo \
-	"$ECR_REGISTRY/pyfound/black" \
-  black --exclude ".lambda_zips/|.terraform/|target/" .
+  	--workdir /repo \
+	"$ECR_REGISTRY/pyfound/black" black \
+  	--exclude ".lambda_zips/|.terraform/|target/" .
