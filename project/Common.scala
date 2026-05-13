4f9763639c2d5f4a7a76cc20bbb22a746eeba180
Robert Kenny <r.kenny@wellcome.org>
2026-05-13 08:49:35 +0100

    use aws codeartifact repo if the auth env var is present

--- project/Common.scala
+++ project/Common.scala
@@ -2,9 +2,25 @@ import sbt.Keys._
 import sbt._
 
 object Common {
+  private val codeArtifactToken = sys.env.get("CODEARTIFACT_AUTH_TOKEN").filter(_.nonEmpty)
+
   val settings: Seq[Def.Setting[_]] = Seq(
     scalaVersion := "2.12.16",
     organization := "weco",
+    externalResolvers := {
+      val codeArtifact = codeArtifactToken.map(_ =>
+        "CodeArtifact" at "https://wellcomecollection-maven-mirror-760097843905.d.codeartifact.eu-west-1.amazonaws.com/maven/wellcomecollection-maven-mirror/"
+      ).toSeq
+      Seq(Resolver.defaultLocal) ++ codeArtifact ++ Seq(Resolver.DefaultMavenRepository)
+    },
+    credentials ++= codeArtifactToken.map(token =>
+      Credentials(
+        "wellcomecollection-maven-mirror/wellcomecollection-maven-mirror",
+        "wellcomecollection-maven-mirror-760097843905.d.codeartifact.eu-west-1.amazonaws.com",
+        "aws",
+        token
+      )
+    ).toSeq,
     scalacOptions ++= Seq(
       "-deprecation",
       "-unchecked",
